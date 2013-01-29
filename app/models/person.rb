29deaacc87765df7481eddc54fe70a47cdad4caa
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-01-30 02:34:12 +0530

    validations + tests for the person model.

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,4 @@
 class Person < ActiveRecord::Base
   attr_accessible :name
+  validates :name, :presence => true
 end
--- test/unit/person_test.rb
+++ test/unit/person_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class PersonTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "cannot create a person without a name" do
+    ps = Person.new
+    ps.name = ""
+    assert !ps.save
+  end
 end
