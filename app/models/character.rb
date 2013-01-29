d7509cbf67b27f7ef4b4a04b8844f36fac5ad7c3
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-01-30 02:18:18 +0530

    validations + tests for the character model.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,4 +1,6 @@
 class Character < ActiveRecord::Base
   attr_accessible :description, :name, :voice_actor_id
   belongs_to :voice_actor, :class_name => "Person"
+
+  validates :name, :presence => true
 end
--- test/unit/character_test.rb
+++ test/unit/character_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class CharacterTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "cannot create a character without a name" do
+    c = Character.new
+    c.name = ""
+    c.voice_actor = people(:kirito)
+    c.description = "TBD"
+    assert !c.save
+  end
+
+  test "can create a character without a voice actor" do
+    c = Character.new
+    c.name = "kirito"
+    c.description = "TBD"
+    assert c.save
+  end
+
+  test "can create a character without a description" do
+    c = Character.new
+    c.name = "kirito"
+    c.voice_actor = people(:kirito)
+    assert c.save
+  end
 end
