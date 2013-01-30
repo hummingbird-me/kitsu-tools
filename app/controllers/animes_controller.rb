e6a4f02b374be28b4abf903fed20480c7b0f8430
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-01-31 02:38:40 +0530

    there's no need to respond to requests for json.

--- app/controllers/animes_controller.rb
+++ app/controllers/animes_controller.rb
@@ -4,7 +4,6 @@ class AnimesController < ApplicationController
 
     respond_to do |format|
       format.html { render :show }
-      format.json { render :text => @anime.to_json }
     end
   end
 end
