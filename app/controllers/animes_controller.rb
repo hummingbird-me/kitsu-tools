5f06ce945594630eafc55df0f449e004be7bb8f9
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-01-30 17:54:03 +0530

    added a simple anime view.

--- app/controllers/animes_controller.rb
+++ app/controllers/animes_controller.rb
@@ -3,6 +3,7 @@ class AnimesController < ApplicationController
     @anime = Anime.find(params[:id])
 
     respond_to do |format|
+      format.html { render :show }
       format.json { render :text => @anime.to_json }
     end
   end
