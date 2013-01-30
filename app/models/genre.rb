e24077e5e3d31b2020b0b1921c5252a74b0972e7
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-01-31 04:03:58 +0530

    added tests for the genre model.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,4 +1,6 @@
 class Genre < ActiveRecord::Base
   attr_accessible :name
   has_and_belongs_to_many :animes
+
+  validates :name, :presence => true, :uniqueness => true
 end
--- test/unit/genre_test.rb
+++ test/unit/genre_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class GenreTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "cannot create genre without name" do
+    g = Genre.new
+    g.name = ""
+    assert !g.save
+  end
+
+  test "name must be unique" do
+    g = Genre.new
+    g.name = "Fantasy"
+    assert !g.save
+  end
 end
