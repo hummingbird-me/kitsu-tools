1dc26400d6167afb3f89f4bd2535fe761e6d2ed9
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-02-01 02:46:09 +0530

    added user model with devise.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Hummingbird::Application.routes.draw do
+  devise_for :users
+
   root :to => "home#index"
 
   resources :animes do
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130131125336) do
+ActiveRecord::Schema.define(:version => 20130131211433) do
 
   create_table "animes", :force => true do |t|
     t.string   "age_rating"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(:version => 20130131125336) do
     t.string   "title"
     t.string   "slug"
     t.string   "youtube_video_id"
+    t.string   "cover_image_url"
   end
 
   add_index "animes", ["slug"], :name => "index_animes_on_slug", :unique => true
@@ -81,5 +82,23 @@ ActiveRecord::Schema.define(:version => 20130131125336) do
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
   end
+
+  create_table "users", :force => true do |t|
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
