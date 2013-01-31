1747e2c31e9b42d07e035a10b0911503404ab00b
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-02-01 01:09:23 +0530

    fixed some n+1 query issues.

--- app/controllers/animes_controller.rb
+++ app/controllers/animes_controller.rb
@@ -1,6 +1,10 @@
 class AnimesController < ApplicationController
   def show
     @anime = Anime.find(params[:id])
+    @genres = @anime.genres
+    @producers = @anime.producers
+    @quotes = @anime.quotes.limit(4).includes(:character)
+    @castings = @anime.castings.includes(:character, :voice_actor)
 
     respond_to do |format|
       format.html { render :show }
