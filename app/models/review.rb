8892c4fc318fa8b2d0e8515009763f24506d3a4c
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-02-01 19:43:19 +0530

    defined unit tests for the review model.

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,10 @@ class Review < ActiveRecord::Base
   belongs_to :user
   belongs_to :anime
   attr_accessible :content, :positive
+
+  validates :user, :anime, :content, :positive, :presence => true
+
+  def negative?
+    not positive?
+  end
 end
--- test/unit/review_test.rb
+++ test/unit/review_test.rb
@@ -1,7 +1,35 @@
 require 'test_helper'
 
 class ReviewTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "cannot create a review without a user" do
+    r = Review.new
+    r.anime = animes(:sword_art_online)
+    r.content = "test"
+    r.positive = true
+    assert !r.save
+  end
+
+  test "cannot create a review without an anime" do
+    r = Review.new
+    r.user = users(:one)
+    r.content = "test"
+    r.positive = false
+    assert !r.save
+  end
+
+  test "cannot create a review without content" do
+    r = Review.new
+    r.user = users(:one)
+    r.anime = animes(:sword_art_online)
+    r.positive = true
+    assert !r.save
+  end
+
+  test "cannot create a review without specifying positiveness" do
+    r = Review.new
+    r.user = users(:one)
+    r.anime = animes(:sword_art_online)
+    r.content = "test"
+    assert !r.save
+  end
 end
