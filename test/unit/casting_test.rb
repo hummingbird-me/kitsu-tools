179041160b4345794974e47054da995087ee2028
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-02-01 20:00:41 +0530

    added unit tests for castings.

--- test/unit/casting_test.rb
+++ test/unit/casting_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class CastingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "cannot create casting without anime" do
+    c = Casting.new
+    c.character = characters(:kirito)
+    c.voice_actor = people(:asuna)
+    assert !c.save
+  end
+
+  test "cannot create casting without character" do
+    c = Casting.new
+    c.anime = animes(:sword_art_online)
+    c.voice_actor = people(:asuna)
+    assert !c.save
+  end
+
+  test "can create casting without voice actor" do
+    c = Casting.new
+    c.anime = animes(:sword_art_online)
+    c.character = characters(:kirito)
+    c.voice_actor = people(:asuna)
+    assert c.save
+  end
 end
