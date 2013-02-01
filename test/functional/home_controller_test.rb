5a02b47a1f90b52dee2c3a4d94dfacd06764a231
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-02-01 19:29:35 +0530

    get functional tests to work.

--- test/functional/home_controller_test.rb
+++ test/functional/home_controller_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
+  include Devise::TestHelpers
+
   test "should get index" do
     get :index
     assert_response :success
