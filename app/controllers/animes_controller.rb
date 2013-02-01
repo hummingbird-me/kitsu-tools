1afd29a015b7ad504fd6dac97116ed1a98542599
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-02-01 20:56:58 +0530

    add reviews to view.

--- app/controllers/animes_controller.rb
+++ app/controllers/animes_controller.rb
@@ -5,6 +5,7 @@ class AnimesController < ApplicationController
     @producers = @anime.producers
     @quotes = @anime.quotes.limit(4).includes(:character)
     @castings = @anime.castings.includes(:character, :voice_actor)
+    @reviews = @anime.reviews.includes(:user)
 
     respond_to do |format|
       format.html { render :show }
