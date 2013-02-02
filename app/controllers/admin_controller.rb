ba6c13054671689f5284b5c4b79e459aa4a98f02
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-02-02 18:50:00 +0530

    added quote moderation functionality.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,6 +2,19 @@ class AdminController < ApplicationController
   http_basic_authenticate_with :name => "change", :password => "thislater"
   
   def index
-    @quotes_pending_moderation = Quote.where("visible IS NULL OR visible = ?", false)
+    @quotes_pending_moderation = Quote.where("visible IS NULL OR visible = ?", false).includes(:anime, :creator)
+  end
+
+  def approve_quote
+    @quote = Quote.find(params[:id])
+    @quote.visible = true
+    @quote.save
+    redirect_to admin_panel_path
+  end
+
+  def delete_quote
+    @quote = Quote.find(params[:id])
+    @quote.delete
+    redirect_to admin_panel_path
   end
 end
