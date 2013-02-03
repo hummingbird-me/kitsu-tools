6f075a82b7b3f234d57663dccffadaa45e7f8b6f
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-02-03 19:04:45 +0530

    some primitive tests for the watchlist model.

--- app/models/watchlist.rb
+++ app/models/watchlist.rb
@@ -2,4 +2,17 @@ class Watchlist < ActiveRecord::Base
   belongs_to :user
   belongs_to :anime
   attr_accessible :positive, :status
+
+  validates_uniqueness_of :user_id, :scope => :anime_id
+
+  def status
+    s = read_attribute(:status)
+    if s.nil?
+      # TODO later, check the number of episodes watched and return
+      #       "Finished Watching" instead if appropriate.
+      return "Currently Watching"
+    else
+      return s
+    end
+  end
 end
--- test/unit/watchlist_test.rb
+++ test/unit/watchlist_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class WatchlistTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "cannot create duplicates" do
+    w = Watchlist.new
+    w.user = users(:vikhyat)
+    w.anime = anime(:sword_art_online)
+    assert w.save
+    w = Watchlist.new
+    w.user = users(:vikhyat)
+    w.anime = anime(:sword_art_online)
+    assert !w.save
+  end
 end
