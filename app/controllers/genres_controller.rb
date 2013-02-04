3a316a60362215fe9cdda7a3307d7df134ae797a
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-02-05 02:22:24 +0530

    Don't create a huge querystring when the user wants all the genres.

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,9 +1,14 @@
 class GenresController < ApplicationController
   def index
-    @genres = params.keys.select {|x| params[x] == ".#{x}" }
+    @genres = params.keys.select {|x| params[x] == ".#{x}" }.uniq
                     .map {|x| x.gsub(/^g_/, "") }
+
     url = URI.parse request.env["HTTP_REFERER"]
-    url.query = "genres=#{@genres*'+'}"
+    if @genres.length == Genre.count
+      url.query = ""
+    else
+      url.query = "genres=#{@genres*'+'}"
+    end
     redirect_to url.to_s
   end
 
