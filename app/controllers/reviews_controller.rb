d187a7e6c020a4ac2f36ec53192e785c39bbf7ce
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-02-06 21:05:14 +0530

    Give the user feedback that their vote on the review went through.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,6 +2,12 @@ class ReviewsController < ApplicationController
   def index
     @anime = Anime.find(params[:anime_id])
     @reviews = Review.includes(:user).find_with_reputation(:votes, :all, {:conditions => ["anime_id = ?", @anime.id], :order => 'votes DESC'})
+    @user_evaluations = Hash.new(false)
+    if user_signed_in?
+      @reviews.map {|x| x.evaluations.where(source_id: current_user.id).first }.each do |evaluation|
+        @user_evaluations[evaluation.target_id] = evaluation if evaluation
+      end
+    end
   end
   
   def vote
