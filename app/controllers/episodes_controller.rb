d9845b958c09090a71bd0d8794f9dcb8bd4c9e33
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-02-11 22:47:29 +0530

    Update the progress bar when an episode is viewed.

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -9,14 +9,17 @@ class EpisodesController < ApplicationController
     
     @anime = Anime.find(params[:anime_id])
     @episode = Episode.find(params[:id])
+    @watchlist = Watchlist.find_or_create_by_anime_id_and_user_id(@anime.id, current_user.id)
 
     if params[:watched] == "true"
       ev = EpisodeView.where(anime_id: @anime.id, user_id: current_user.id, episode_id: @episode.id).first
       if ev.nil?
         EpisodeView.create(anime: @anime, user: current_user, episode: @episode)
+        @watchlist.episodes_watched += 1; @watchlist.save
       end
-    else
+    elsif params[:watched] == "false"
       EpisodeView.where(anime_id: @anime.id, user_id: current_user.id, episode_id: @episode.id).delete_all
+      @watchlist.episodes_watched -= 1; @watchlist.save
     end
 
     redirect_to :back
