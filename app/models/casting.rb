828a76e32504cad97e2367813afefd60c02f526c
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-02-12 22:58:49 +0530

    remove outdated casting model.

--- app/models/casting.rb
+++
@@ -1,8 +0,0 @@
-class Casting < ActiveRecord::Base
-  attr_accessible :anime_id, :character_id, :voice_actor_id
-  belongs_to :anime
-  belongs_to :character
-  belongs_to :voice_actor, :class_name => "Person"
-
-  validates :anime, :character, :presence => true
-end
