08543724e745cfa6e84dd3d09ead0102fa6263c2
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-02-14 07:50:01 +0530

    added empty views for different lists.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,20 @@ class UsersController < ApplicationController
   def watchlist
     @user = User.find(params[:user_id])
     @active_tab = :watchlist
+    if params[:list]
+      if params[:list] == "plan_to_watch"
+        render "watchlist_plan_to_watch"
+      elsif params[:list] == "completed"
+        render "watchlist_completed"
+      elsif params[:list] == "on_hold"
+        render "watchlist_on_hold"
+      elsif params[:list] == "dropped"
+        render "dropped"
+      else
+        # Currently watchlist
+        render "watchlist"
+      end
+    end
   end
   
   def reviews
