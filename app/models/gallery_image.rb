f943a5fbd48f43fcf3c56c379c7d01de63c51739
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-02-17 18:59:23 +0530

    Get the gallery model to play well with rails_admin.

--- app/models/gallery_image.rb
+++ app/models/gallery_image.rb
@@ -1,4 +1,7 @@
 class GalleryImage < ActiveRecord::Base
   belongs_to :anime
-  attr_accessible :description
+  attr_accessible :anime_id, :description, :image
+  has_attached_file :image
+
+  validates :anime, presence: true
 end
