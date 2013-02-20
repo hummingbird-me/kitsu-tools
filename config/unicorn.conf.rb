d767cc0d841eeed9d3bceecc567c17c55c731cc8
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-02-20 22:31:55 +0530

    Zero downtime deploy configuration.

--- config/unicorn.conf.rb
+++ config/unicorn.conf.rb
@@ -1,37 +1,21 @@
-# Sample verbose configuration file for Unicorn (not Rack)
-#
-# This configuration file documents many features of Unicorn
-# that may not be needed for some applications. See
-# http://unicorn.bogomips.org/examples/unicorn.conf.minimal.rb
-# for a much simpler configuration file.
-#
-# See http://unicorn.bogomips.org/Unicorn/Configurator.html for complete
-# documentation.
-
-# Use at least one worker per core if you're on a dedicated server,
-# more will usually help for _short_ waits on databases/caches.
 worker_processes 4
-
-# Since Unicorn is never exposed to outside clients, it does not need to
-# run on the standard HTTP port (80), there is no reason to start Unicorn
-# as root unless it's from system init scripts.
-# If running the master process as root and the workers as an unprivileged
-# user, do this to switch euid/egid in the workers (also chowns logs):
-# user "unprivileged_user", "unprivileged_group"
-
-# Help ensure your application will always spawn in the symlinked
-# "current" directory that Capistrano sets up.
 working_directory "/u/apps/hummingbird/current"
 
-# listen on both a Unix domain socket and a TCP port,
-# we use a shorter backlog for quicker failover when busy
-listen "/tmp/unicorn.sock", :backlog => 64
+preload_app true
+# Memory savings when we switch to Ruby 2.0
+if GC.respond_to?(:copy_on_write_friendly=)
+  GC.copy_on_write_friendly = true
+end
+
+# Listen on a Unix domain socket. Backlog setting needs tweaking.
+listen "/tmp/unicorn.sock", backlog: 1024
+# Uncomment to listen on a TCP socket.
 # listen 8080, :tcp_nopush => true
 
-# nuke workers after 30 seconds instead of 60 seconds (the default)
+# Nuke workers after 30 seconds.
 timeout 30
 
-# feel free to point this anywhere accessible on the filesystem
+# PID file.
 pid "/u/apps/hummingbird/shared/pids/unicorn.pid"
 
 # By default, the Unicorn logger will write to stderr.
@@ -39,11 +23,6 @@ pid "/u/apps/hummingbird/shared/pids/unicorn.pid"
 # so prevent them from going to /dev/null when daemonized here:
 stderr_path "/u/apps/hummingbird/shared/log/unicorn.stderr.log"
 stdout_path "/u/apps/hummingbird/shared/log/unicorn.stdout.log"
-
-# combine Ruby 2.0.0dev or REE with "preload_app true" for memory savings
-# http://rubyenterpriseedition.com/faq.html#adapt_apps_for_cow
-preload_app true
-GC.respond_to?(:copy_on_write_friendly=) and GC.copy_on_write_friendly = true
 
 # Enable this flag to have unicorn test client connections by writing the
 # beginning of the HTTP headers before calling the application.  This
@@ -56,32 +35,20 @@ check_client_connection false
 before_fork do |server, worker|
   # the following is highly recomended for Rails + "preload_app true"
   # as there's no need for the master process to hold a connection
-  defined?(ActiveRecord::Base) and
+  if defined?(ActiveRecord::Base)
     ActiveRecord::Base.connection.disconnect!
+  end
 
-  # The following is only recommended for memory/DB-constrained
-  # installations.  It is not needed if your system can house
-  # twice as many worker_processes as you have configured.
+  # Zero downtime deploys.
   #
-  # # This allows a new master process to incrementally
-  # # phase out the old master process with SIGTTOU to avoid a
-  # # thundering herd (especially in the "preload_app false" case)
-  # # when doing a transparent upgrade.  The last worker spawned
-  # # will then kill off the old master process with a SIGQUIT.
-  # old_pid = "#{server.config[:pid]}.oldbin"
-  # if old_pid != server.pid
-  #   begin
-  #     sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
-  #     Process.kill(sig, File.read(old_pid).to_i)
-  #   rescue Errno::ENOENT, Errno::ESRCH
-  #   end
-  # end
-  #
-  # Throttle the master from forking too quickly by sleeping.  Due
-  # to the implementation of standard Unix signal handlers, this
-  # helps (but does not completely) prevent identical, repeated signals
-  # from being lost when the receiving process is busy.
-  # sleep 1
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if File.exists?(old_pid) and server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # Someone else did our job for us.
+    end
+  end
 end
 
 after_fork do |server, worker|
@@ -90,8 +57,9 @@ after_fork do |server, worker|
   # server.listen(addr, :tries => -1, :delay => 5, :tcp_nopush => true)
 
   # the following is *required* for Rails + "preload_app true",
-  defined?(ActiveRecord::Base) and
+  if defined?(ActiveRecord::Base)
     ActiveRecord::Base.establish_connection
+  end
 
   # if preload_app is true, then you may also want to check and
   # restart any other shared sockets/descriptors such as Memcached,
