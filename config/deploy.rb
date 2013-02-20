445198a2cdb9a95317ebac7f5aa6163951ace03c
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-02-20 12:50:52 +0530

    Deploy preparation commit #1.

---
+++ config/deploy.rb
@@ -0,0 +1,29 @@
+require 'bundler/capistrano'
+
+set :application, "hummingbird"
+set :repository,  "git@github.com:vikhyat/hummingbird.git"
+
+set :scm, :git
+set :deploy_via, :remote_cache
+
+default_run_options[:pty] = true
+
+role :web, "hakanai" # Your HTTP server, Apache/etc
+role :app, "hakanai" # This may be the same as your `Web` server
+role :db,  "hakanai", :primary => true # This is where Rails migrations will run
+#role :db,  "your slave db-server here"
+
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
