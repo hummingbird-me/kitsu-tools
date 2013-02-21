86dd3cf4f8429a04bae90d812700c6fdd3067daf
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-02-21 15:31:08 +0530

    Switch to SQL schema.

--- config/application.rb
+++ config/application.rb
@@ -45,7 +45,7 @@ module Hummingbird
     # Use SQL instead of Active Record's schema dumper when creating the database.
     # This is necessary if your schema can't be completely dumped by the schema dumper,
     # like if you have constraints or database-specific column types
-    # config.active_record.schema_format = :sql
+    config.active_record.schema_format = :sql
 
     # Enforce whitelist mode for mass assignment.
     # This will create an empty whitelist of attributes available for mass-assignment for all models
