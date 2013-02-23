61e61ff9e99ac7b49e13ba70611329e830c355aa
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-02-23 12:05:08 +0530

    Move status transition logic out of the model, so that we can make decisions based on the context of the user's actions.

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -17,11 +17,22 @@ class EpisodesController < ApplicationController
       ev = EpisodeView.where(anime_id: @anime.id, user_id: current_user.id, episode_id: @episode.id).first
       if ev.nil?
         EpisodeView.create(anime: @anime, user: current_user, episode: @episode)
-        @watchlist.episodes_watched += 1; @watchlist.save
+        @watchlist.episodes_watched += 1
+        # If this is the last episode, set the status to completed.
+        if @anime.episode_count == @watchlist.episodes_watched
+          @watchlist.status = "Completed"
+        end
+        @watchlist.save
       end
     elsif params[:watched] == "false"
       EpisodeView.where(anime_id: @anime.id, user_id: current_user.id, episode_id: @episode.id).delete_all
-      @watchlist.episodes_watched -= 1; @watchlist.save
+      @watchlist.episodes_watched -= 1
+      # If the user removes an episode from a completed show, move it into the
+      # "Currently Watching" list.
+      if @watchlist.status = "Completed"
+        @watchlist.status = "Currently Watching"
+      end
+      @watchlist.save
     end
 
     respond_to do |format|
