04556a227150720817f8ec8954fe60bf4c4b6ade
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-02-23 11:47:43 +0530

    An excellent start on implementing the profile watchlists.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,20 +7,26 @@ class UsersController < ApplicationController
   def watchlist
     @user = User.find(params[:user_id])
     @active_tab = :watchlist
-    if params[:list]
-      if params[:list] == "plan_to_watch"
-        render "watchlist_plan_to_watch"
-      elsif params[:list] == "completed"
-        render "watchlist_completed"
-      elsif params[:list] == "on_hold"
-        render "watchlist_on_hold"
-      elsif params[:list] == "dropped"
-        render "dropped"
-      else
+    @sub_tab = params[:list] || "currently_watching"
+    
+    if @sub_tab == "currently_watching"
+      @watchlist = @user.watchlists.where(:status => "Currently Watching").order('updated_at DESC')
+    end
+
+#    if params[:list]
+#      if params[:list] == "plan_to_watch"
+#        render "watchlist_plan_to_watch"
+#      elsif params[:list] == "completed"
+#        render "watchlist_completed"
+#      elsif params[:list] == "on_hold"
+#        render "watchlist_on_hold"
+#      elsif params[:list] == "dropped"
+#        render "dropped"
+#      else
         # Currently watchlist
         render "watchlist"
-      end
-    end
+#      end
+#    end
   end
   
   def reviews
