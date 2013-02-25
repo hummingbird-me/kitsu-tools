9afa8c2afb9d0fdd9da429a499413c0bbcbbabc9
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-02-25 22:52:41 +0530

    Swap search order for faster searches.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -22,9 +22,9 @@ class SearchController < ApplicationController
       @watchlist = Hash.new(false)
     end
     
-    @anime_list = @anime.fuzzy_search_by_title(params[:query])
+    @anime_list = @anime.simple_search_by_title(params[:query])
     if @anime_list.length == 0
-      @anime_list = @anime.simple_search_by_title(params[:query])
+      @anime_list = @anime.fuzzy_search_by_title(params[:query])
     end
     @anime = @anime_list
 
