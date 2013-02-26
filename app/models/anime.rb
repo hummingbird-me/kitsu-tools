8cac15fe49653a037736d9d3aea9a19203459e01
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-02-26 23:24:32 +0530

    Allow adding YouTube videos.

--- app/models/anime.rb
+++ app/models/anime.rb
@@ -8,7 +8,7 @@ class Anime < ActiveRecord::Base
   extend FriendlyId
   friendly_id :title, :use => [:slugged]
 
-  attr_accessible :title, :age_rating, :episode_count, :episode_length, :mal_id, :status, :synopsis, :cover_image
+  attr_accessible :title, :age_rating, :episode_count, :episode_length, :mal_id, :status, :synopsis, :cover_image, :youtube_video_id
 
   has_attached_file :cover_image, 
     :styles => { :thumb => "225x335!", :"thumb@2x" => "450x670!" },
