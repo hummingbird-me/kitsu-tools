03772a165cd5da82d3e8d0ea8d2b74e137a5fbb1
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-03-02 14:02:23 +0530

    Fetch the Watchlist on the review and quote listings.

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,6 +1,9 @@
 class QuotesController < ApplicationController
   def index
     @anime = Anime.find(params[:anime_id])
+    if user_signed_in?
+      @watchlist = current_user.watchlists.where(anime_id: @anime).first
+    end
     @quotes = Quote.includes(:user).find_with_reputation(:votes, :all, {:conditions => ["anime_id = ?", @anime.id], :order => "votes DESC"})
   end
 
