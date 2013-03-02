d62086cf4a81c20a088be61bcdb484bae7c8c337
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-03-02 18:52:16 +0530

    Wired together a bit of the home page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
   def index
     @hide_cover_image = true
+    @latest_reviews = Review.order('created_at DESC').limit(2)
+    @popular_anime = Anime.order('wilson_ci DESC').limit(4)
   end
   
   def dashboard
