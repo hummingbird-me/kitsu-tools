736efd05de980b13ed54edc578e0b8896bb5ec6b
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-03-03 17:42:47 +0530

    Added beta sign up email.

--- app/mailers/beta_mailer.rb
+++ app/mailers/beta_mailer.rb
@@ -1,3 +1,7 @@
 class BetaMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "vikhyat@hummingbird.ly"
+  
+  def beta_sign_up(beta_invite)
+    mail(to: beta_invite.email, subject: "Ready for your new, favorite anime?")
+  end
 end
