8ef2cdecd886f513701e9ba7e081d42d115a0754
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-03-09 23:49:16 +0530

    Re-add forem migrations.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
+
+  def forem_user
+    current_user
+  end
+  helper_method :forem_user
+
   protect_from_forgery
 
   def mixpanel
---
+++ config/initializers/forem.rb
@@ -0,0 +1,16 @@
+Forem.user_class = "User"
+Forem.email_from_address = "please-change-me@example.com"
+# If you do not want to use gravatar for avatars then specify the method to use here:
+# Forem.avatar_user_method = :custom_avatar_url
+Forem.per_page = 20
+
+
+# Rails.application.config.to_prepare do
+#   If you want to change the layout that Forem uses, uncomment and customize the next line:
+#   Forem::ApplicationController.layout "forem"
+#
+#   If you want to add your own cancan Abilities to Forem, uncomment and customize the next line:
+#   Forem::Ability.register_ability(Ability)
+# end
+#
+# By default, these lines will use the layout located at app/views/layouts/forem.html.erb in your application.
