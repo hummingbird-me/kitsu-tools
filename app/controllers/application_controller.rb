07bf89e66203ec5996dbdbbb38734e038397836d
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-03-10 00:03:46 +0530

    Hook up forem.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,5 @@
 class ApplicationController < ActionController::Base
-
-  def forem_user
-    current_user
-  end
+  def forem_user; current_user; end
   helper_method :forem_user
 
   protect_from_forgery
