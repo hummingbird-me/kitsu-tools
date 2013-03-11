6e368d5c1f3b7cc3d07b9da3bb4b33ba1a41d9dc
Josh Fabian <contact@joshfabian.com>
2013-03-11 07:47:43 -0500

    Update twitter button with review summary

---
+++ app/assets/javascripts/googleplus.js
@@ -0,0 +1,5 @@
+(function() {
+  var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+  po.src = 'https://apis.google.com/js/plusone.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+})();
