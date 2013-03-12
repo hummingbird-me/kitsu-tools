75bc0fe824c522695bd5fd4611330ac7e26ab1ce
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-03-12 15:06:34 +0530

    (hopefully) redirect to the previous page on sign in.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    "/dashboard"
+    stored_location_for(resource) || "/dashboard"
   end
   
   def after_sign_out_path_for(resource)
