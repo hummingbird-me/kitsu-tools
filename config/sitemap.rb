6cd70a9928ee6fde9c71d22f3664aac5ee9ed491
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-03-17 01:44:49 +0530

    Added sitemap config.

---
+++ config/sitemap.rb
@@ -0,0 +1,28 @@
+# Set the host name for URL creation
+SitemapGenerator::Sitemap.default_host = "http://hummingbird.ly"
+
+SitemapGenerator::Sitemap.create do
+  # Users
+  User.find_each do |user|
+    add user_path(user), lastmod: user.updated_at
+  end
+  
+  # Anime
+  add '/anime'
+  Anime.find_each do |anime|
+    add anime_path(anime), lastmod: anime.updated_at
+    add anime_quotes_path(anime), lastmod: anime.quotes.map {|x| x.updated_at }.max
+    
+    # Reviews
+    anime.reviews.each do |review|
+      add anime_review_path(anime, review), lastmod: review.updated_at
+    end
+  end
+  
+  # TODO: Genres
+  #
+  # Genre.find_each do |genre|
+  #   add genre_path(genre)
+  # end
+  
+end
