b860352bbb7ec2f6813d78553ed6e87a8814c426
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-03-17 18:15:04 +0530

    Make the alt title editable.

--- app/models/anime.rb
+++ app/models/anime.rb
@@ -8,7 +8,7 @@ class Anime < ActiveRecord::Base
   extend FriendlyId
   friendly_id :title, :use => [:slugged]
 
-  attr_accessible :title, :age_rating, :episode_count, :episode_length, :mal_id, :status, :synopsis, :cover_image, :youtube_video_id
+  attr_accessible :title, :age_rating, :episode_count, :episode_length, :mal_id, :status, :synopsis, :cover_image, :youtube_video_id, :alt_title
 
   has_attached_file :cover_image, 
     :styles => { :thumb => "225x335!", :"thumb@2x" => "450x670!" },
