2edc99373edab8cd738e0acc336cb158505bf9b1
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-03-22 15:32:43 +0530

    Revert "Remember users by default."
    This reverts commit 6f40cacb407a19a7b7b154bdfa1b50df9b1ec4db.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :recoverable, :rememberable,
          :trackable, :validatable, :omniauthable, :confirmable, :async
 
-  # Remember me by default.
-  def remember_me; true; end
-
   # Setup accessible (or protected) attributes for your model
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me,
     :watchlist_hash, :recommendations_up_to_date, :avatar, :facebook_id, :bio,
