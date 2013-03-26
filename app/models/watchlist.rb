c7c8dabf65f0471af463c6a469e142efd4c43395
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-03-26 15:00:20 +0530

    Slightly faster watchlist saves.

--- app/models/watchlist.rb
+++ app/models/watchlist.rb
@@ -48,8 +48,11 @@ class Watchlist < ActiveRecord::Base
     self.episodes_watched = self.episodes.length
     
     if self.status == "Completed"
-      self.user.update_life_spent_on_anime((self.anime.episodes - self.episodes).map {|x| x.length }.sum)
-      self.episodes = self.anime.episodes
+      if self.episodes_watched != self.anime.episodes.length and self.anime.episodes.length > 0
+        self.user.update_life_spent_on_anime((self.anime.episodes - self.episodes).map {|x| x.length }.sum)
+        self.episodes = self.anime.episodes
+        self.last_watched = Time.now
+      end
     elsif self.episodes_watched == self.anime.episodes.length and self.episodes_watched > 0
       self.status = "Completed"
     end
