139f105b5af4753e71fec92f90a1a7563ffae8b3
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-03-26 13:11:07 +0530

    More idiomatic validation.

--- app/models/watchlist.rb
+++ app/models/watchlist.rb
@@ -4,7 +4,7 @@ class Watchlist < ActiveRecord::Base
   attr_accessible :user, :anime, :status, :episodes_watched, 
     :updated_at, :last_watched, :imported, :rating
 
-  validates_uniqueness_of :user_id, :scope => :anime_id
+  validates :user_id, :uniqueness => {:scope => :anime_id}
 
   has_and_belongs_to_many :episodes, :uniq => true
 
