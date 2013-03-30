4c5553da0b5e08eb92fa9fac93070dcbe85a528b
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-03-30 23:42:07 +0530

    Remove slug from attr_accessible.

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me,
     :watchlist_hash, :recommendations_up_to_date, :avatar, :facebook_id, :bio,
-    :forem_admin, :slug, :about, :cover_image, :sfw_filter, :star_rating
+    :forem_admin, :about, :cover_image, :sfw_filter, :star_rating
 
   has_attached_file :avatar, :styles => {:thumb => "200x200"},
     :default_url => "http://placekitten.com/g/200/200"
