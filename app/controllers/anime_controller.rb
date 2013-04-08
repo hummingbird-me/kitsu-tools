d286e3bf0fe3738e7b18dbee77e58b30a4329e59
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-04-08 12:44:02 +0530

    Bugfix

--- app/controllers/anime_controller.rb
+++ app/controllers/anime_controller.rb
@@ -65,7 +65,7 @@ class AnimeController < ApplicationController
   def get_metadata_from_mal
     @anime = Anime.find(params[:anime_id])
     authorize! :update, @anime
-    meta = MALImport.series_metadata(@anime.mal_id)
+    meta = MalImport.series_metadata(@anime.mal_id)
     @anime.episode_count = meta[:episode_count]
     @anime.episode_length = meta[:episode_length]
     @anime.status = meta[:status]
