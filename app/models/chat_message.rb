368d7fd7fd54ee3f6dbedabd240e632d5df8af98
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-04-18 23:56:11 +0530

    Chat message model update.

--- app/models/chat_message.rb
+++ app/models/chat_message.rb
@@ -1,7 +1,8 @@
 class ChatMessage
   include Mongoid::Document
 
-  field :user_id, Integer
-  field :message_type, String
-  field :message, String
+  field :user_id, type: Integer
+  field :message_type, type: String
+  field :message, type: String
+  field :created_at, type: Time, default: ->{ Time.now }
 end
