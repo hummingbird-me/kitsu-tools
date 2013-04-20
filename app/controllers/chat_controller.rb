134faa339c95bde010fb893cb3388c466254c69f
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-04-21 03:47:47 +0530

    Reduce the amount of querying being done.

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -2,7 +2,6 @@ class ChatController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-
   end
   
   # Update the current user's last seen time in Redis, and return a list of
@@ -23,17 +22,25 @@ class ChatController < ApplicationController
       messages = []
       
       if params[:since] == nil
-        messages = ChatMessage.desc(:created_at).limit(100).map {|x| {id: x[:"_id"], user_id: x[:user_id], type: x[:message_type], message: x[:message], created_at: x[:created_at]} }
+        messages = ChatMessage.desc(:created_at).limit(30)
+      else
+        messages = ChatMessage.desc(:created_at).where(:created_at.gte => DateTime.parse(params[:since])).limit(100)
       end
       
+      # Convert Mongo model into hash.
+      messages = messages.map {|x| {id: x[:"_id"], user_id: x[:user_id], type: x[:message_type], message: x[:message], created_at: x[:created_at]} }
+
+      # Fetch user data for each message.
       users = {}
       messages.each do |x|
         users[x[:user_id]] ||= User.find(x[:user_id])
         x[:user] = {name: users[x[:user_id]].name, url: user_url(users[x[:user_id]]), avatar: users[x[:user_id]].avatar.url(:thumb)}
       end
-      messages = messages.sort_by {|x| x[:created_at] }
-      render :json => messages
       
+      # Sort the messages.
+      messages = messages.sort_by {|x| x[:created_at] }
+
+      render :json => messages
     end
   end
 end
