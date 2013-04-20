fca4b3bf1e7799dc111123067d86019f1263c302
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-04-20 22:42:44 +0530

    Show actual chat messages.

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -12,4 +12,19 @@ class ChatController < ApplicationController
     user_ids = $redis.zrangebyscore("chat_last_seen", Time.now.to_i - 30, Time.now.to_i + 5)
     render :json => User.where(id: user_ids).map {|x| {name: x.name, url: user_url(x)} }
   end
+  
+  def messages
+    messages = []
+    if params[:since] == nil
+      messages = ChatMessage.desc(:created_at).limit(100).map {|x| {user: x[:user_id], type: x[:message_type], message: x[:message], created_at: x[:created_at]} }
+    end
+    
+    users = {}
+    messages.each do |x|
+      users[x] ||= User.find(x[:user])
+      x[:user] = {name: users[x].name, url: user_url(users[x]), avatar: users[x].avatar.url(:thumb)}
+    end
+    messages = messages.sort_by {|x| x[:created_at] }.reverse
+    render :json => messages
+  end
 end
