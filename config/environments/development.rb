29dccd4a87607449c05e651ea75b505e100090e0
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-05-07 01:51:05 +0530

    Don't send email in development mode. :|

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,6 +17,7 @@ Hummingbird::Application.configure do
   config.action_mailer.raise_delivery_errors = false
   
   # Other mailer options
+  config.action_mailer.perform_deliveries = false
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
