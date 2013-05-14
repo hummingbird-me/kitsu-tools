5081cc21adbf4b61dd0c7125b24ae642c0b2e2ab
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-05-14 23:41:41 +0530

    Correct sitemap URL.

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,5 +1,5 @@
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "http://hummingbird.ly"
+SitemapGenerator::Sitemap.default_host = "http://hummingbird.me"
 
 SitemapGenerator::Sitemap.create do
   # Users
