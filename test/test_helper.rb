c31b0dd21b434345feed9c16e555705cb574d824
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-05-16 20:04:49 +0530

    Stub Sunspot session for tests.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,13 @@
-require 'rubygems'
 require 'simplecov'
-
 SimpleCov.start 'rails'
 
 ENV["RAILS_ENV"] = "test"
+
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+
 class ActiveSupport::TestCase
   fixtures :all
 end
+
+Sunspot.session = Sunspot::Rails::StubSessionProxy.new(Sunspot.session)
