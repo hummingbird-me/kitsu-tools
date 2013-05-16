79438078973f3aba4f18439e52441a6a96e70b43
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-05-16 22:07:38 +0530

    Failing test for the follower count.

--- test/unit/follow_test.rb
+++ test/unit/follow_test.rb
@@ -5,10 +5,33 @@ class FollowTest < ActiveSupport::TestCase
     u1 = FactoryGirl.create(:user)
     u2 = FactoryGirl.create(:user)
     
-    # Counts should be zero initially.
-    assert_equal u1.followers_count, 0
-    assert_equal u2.followers_count, 0
-    assert_equal u1.following_count, 0
-    assert_equal u2.following_count, 0
+    assert_equal 0, u1.followers_count
+    assert_equal 0, u2.followers_count
+    assert_equal 0, u1.following_count
+    assert_equal 0, u2.following_count
+
+    u1.followers.push u2; u1.reload; u2.reload
+    assert_equal 1, u1.followers_count
+    assert_equal 1, u2.following_count
+    assert_equal 0, u1.following_count
+    assert_equal 0, u2.followers_count
+
+    u2.followers.push u1; u1.reload; u2.reload
+    assert_equal 1, u1.followers_count
+    assert_equal 1, u2.following_count
+    assert_equal 1, u1.following_count
+    assert_equal 1, u2.followers_count
+    
+    u1.followers.delete u2; u1.reload; u2.reload
+    assert_equal 0, u1.followers_count
+    assert_equal 0, u2.following_count
+    assert_equal 1, u1.following_count
+    assert_equal 1, u2.followers_count
+    
+    u1.following.delete u1; u1.reload; u2.reload
+    assert_equal 0, u1.followers_count
+    assert_equal 0, u2.following_count
+    assert_equal 0, u1.following_count
+    assert_equal 0, u2.followers_count
   end
 end
