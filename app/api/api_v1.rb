42898df72e5990755e17c4f9f8bb5a38816adb0b
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-05-24 02:30:43 +0530

    Request processing time -> latency.

--- app/api/api_v1.rb
+++ app/api/api_v1.rb
@@ -1,10 +1,22 @@
 class API_v1 < Grape::API
   version 'v1', using: :path, format: :json, vendor: 'hummingbird'
+
+  before do
+    @start_time = Time.now
+  end
+  
+  after do
+    @end_time = Time.now
+    $riemann << {
+      service: "api req",
+      metric: @end_time - @start_time,
+      state: "ok"
+    }
+  end
   
   resource :system do
     desc "Returns pong."
     get :ping do
-      $riemann << {service: 'api request', metric: 1}
       {ping: 'pong'}
     end
   end
