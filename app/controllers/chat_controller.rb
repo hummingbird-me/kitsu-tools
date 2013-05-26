88c2af66bc9d0b07e99c5c4f134e84944ef8d669
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-05-26 16:58:05 +0530

    Remove obsoleted chat message post endpoint.

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -69,44 +69,37 @@ class ChatController < ApplicationController
   end
   
   def messages
-    # If a message was posted, save it.
-    if params[:message] and params[:message].strip.length > 0
-      sleep 30 if current_user.id == 951
-      render :json => true
-    else
+    messages = []
     
-      messages = []
-      
-      if params[:since]
-        messages = ChatMessage.desc(:created_at).where(:created_at.gte => DateTime.parse(params[:since])).limit(100)
-      elsif params[:until]
-        messages = ChatMessage.desc(:created_at).where(:created_at.lte => DateTime.parse(params[:until])).limit(100)
-      else
-        messages = ChatMessage.desc(:created_at).limit(100)
-      end
-      
-      # Format any unformatted messages.
-      messages.each do |x|
-        if x.formatted_message.nil?
-          x.formatted_message = format_message x[:message]
-          x.save
-        end
-      end
-      
-      # Convert Mongo model into hash.
-      messages = messages.map {|x| {id: x[:"_id"], user_id: x[:user_id], type: x[:message_type], message: x.formatted_message, raw_message: x[:message], created_at: x[:created_at]} }
-
-      # Fetch user data for each message.
-      users = {}
-      messages.each do |x|
-        users[x[:user_id]] ||= User.find(x[:user_id])
-        x[:user] = {name: users[x[:user_id]].name, url: user_url(users[x[:user_id]]), avatar: users[x[:user_id]].avatar.url(:thumb)}
-      end
-      
-      # Sort the messages.
-      messages = messages.sort_by {|x| x[:created_at] }
-
-      render :json => messages
+    if params[:since]
+      messages = ChatMessage.desc(:created_at).where(:created_at.gte => DateTime.parse(params[:since])).limit(100)
+    elsif params[:until]
+      messages = ChatMessage.desc(:created_at).where(:created_at.lte => DateTime.parse(params[:until])).limit(100)
+    else
+      messages = ChatMessage.desc(:created_at).limit(100)
     end
+    
+    # Format any unformatted messages.
+    messages.each do |x|
+      if x.formatted_message.nil?
+        x.formatted_message = format_message x[:message]
+        x.save
+      end
+    end
+    
+    # Convert Mongo model into hash.
+    messages = messages.map {|x| {id: x[:"_id"], user_id: x[:user_id], type: x[:message_type], message: x.formatted_message, raw_message: x[:message], created_at: x[:created_at]} }
+
+    # Fetch user data for each message.
+    users = {}
+    messages.each do |x|
+      users[x[:user_id]] ||= User.find(x[:user_id])
+      x[:user] = {name: users[x[:user_id]].name, url: user_url(users[x[:user_id]]), avatar: users[x[:user_id]].avatar.url(:thumb)}
+    end
+    
+    # Sort the messages.
+    messages = messages.sort_by {|x| x[:created_at] }
+
+    render :json => messages
   end
 end
