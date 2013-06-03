b1068e5caa59cf816d63d19bb67129d86b07bef9
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-06-04 00:59:30 +0530

    Fuck off, Lisa.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :fuck_off_lisa
+  def fuck_off_lisa
+    sleep 24 if user_signed_in? and current_user.id == 951
+  end
+
   def forem_user; current_user; end
   helper_method :forem_user
 
