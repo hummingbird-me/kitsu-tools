5fd45f5c59407015c39f3273df9d8d233f2857a9
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-06-06 10:24:15 +0530

    Old homepage for normal users.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,17 @@ class HomeController < ApplicationController
   #caches_action :index, layout: false, :if => lambda { not user_signed_in? }
 
   def index
-    if user_signed_in?
+    if user_signed_in? and current_user.id == 1
+
       @forum_topics = Forem::Topic.by_most_recent_post.limit(10)
+      
+    elsif user_signed_in?
+
+      @recent_anime_users = User.joins(:watchlists).where('watchlists.episodes_watched > 0').order('MAX(watchlists.last_watched) DESC').group('users.id').limit(8)
+      @recent_anime = @recent_anime_users.map {|x| x.watchlists.where("EXISTS (SELECT 1 FROM anime WHERE anime.id = anime_id AND age_rating <> 'R18+')").order('updated_at DESC').limit(1).first }.sort_by {|x| x.last_watched || x.updated_at }.reverse
+      @latest_reviews = Review.order('created_at DESC').limit(2)
+
+      render :old_index
 
     else
       render :guest_index
