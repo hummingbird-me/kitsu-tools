4e7754ca7fc348f54d7e419f95af16ff6bb70fdb
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-06-06 18:49:35 +0530

    Bugfix.

--- app/api/api_v1.rb
+++ app/api/api_v1.rb
@@ -26,6 +26,9 @@ class API_v1 < Grape::API
     def current_user
       warden.user
     end
+    def user_signed_in?
+      not current_user.nil?
+    end
     def current_ability
       @current_ability ||= Ability.new(current_user)
     end
