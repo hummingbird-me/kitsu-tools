fe603f7563421a3b043296d32558eeed758f57e0
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-06-20 01:18:44 +0530

    aggregate(story, [story]) -> [story]

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
   #caches_action :index, layout: false, :if => lambda { not user_signed_in? }
 
   def index
-    if user_signed_in? and (current_user.id == 1 or current_user.id == 4075)
+    if user_signed_in? and ([1, 2, 4075].include? current_user.id)
 
       respond_to do |format|
         format.html do
