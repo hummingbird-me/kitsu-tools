72c96330f291f49bae98284599a7d242fecfb374
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-06-21 13:18:37 +0530

    Add invite code "THEFAN".

--- app/controllers/beta_invites_controller.rb
+++ app/controllers/beta_invites_controller.rb
@@ -11,7 +11,7 @@ class BetaInvitesController < ApplicationController
   end
 
   def invite_code
-    valid_codes = %w[ACEN2013 ANICHARTNET]
+    valid_codes = %w[ACEN2013 ANICHARTNET THEFAN]
     email = params[:email].strip.downcase
     code = params[:invite_code].strip.upcase
     
