613c6a1b7a7e869eed8eef5206705f9ef8b0b46b
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-06-27 20:54:01 +0530

    Fix tooltip styling.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,6 @@
-#require 'rvm/capistrano'
 require 'sidekiq/capistrano'
 require 'bundler/capistrano'
-#require 'new_relic/recipes'
+require 'new_relic/recipes'
 
 set :application, "hummingbird"
 set :repository,  "git@github.com:vikhyat/hummingbird.git"
@@ -10,59 +9,18 @@ set :scm, :git
 set :user, "vikhyat"
 set :git_enable_submodules, 1
 set :deploy_via, :remote_cache
-#set :rvm_type, :user
 
 default_run_options[:pty] = true
 default_run_options[:shell] = '/bin/bash --login'
 
-if ENV['RAILS_ENV'] == "development"
-  server "192.168.33.10", :app, :web, :db, primary: true
-elsif ENV['RAILS_ENV'] == "staging"
-  server "192.241.199.146", :app, :web, :db, primary: true
-else
-  server "sheska", :app, :web, :db, primary: true
-end
+server "sheska", :app, :web, :db, primary: true
 
 #role :db,  "your slave db-server here"
-
-#
-# CUSTOM TASKS
-#
 
 namespace :deploy do
   desc "stub task to get cap to prompt for the root password"
   task :sudo_prompt, roles: :web do
     run "#{sudo} ls"
-  end
-
-  desc "symlink nginx configuration"
-  task :nginx_symlink, roles: :web do
-    run "#{sudo} ln -nfs #{release_path}/config/nginx.conf /etc/nginx/sites-enabled/hummingbird"
-  end
-
-  desc "symlink riemann configuration"
-  task :riemann_symlink, roles: :web do
-    run "#{sudo} ln -nfs #{release_path}/config/riemann.config /home/vikhyat/Riemann/etc/riemann.config"
-  end
-  
-  desc "restart riemann"
-  task :riemann_restart, roles: :web do
-    run "#{sudo} monit restart riemann"
-  end
-  
-  desc "reload nginx configuration"
-  task :nginx_reload, roles: :web do
-    run "#{sudo} service nginx reload"
-  end
-  
-  desc "symlink monit configuration"
-  task :monit_symlink, roles: :web do
-    run "#{sudo} ln -nfs #{release_path}/config/monit.conf /etc/monit/conf.d/hummingbird.conf"
-  end
-  
-  desc "reload monit configuration"
-  task :monit_reload, roles: :web do
-    run "#{sudo} monit reload"
   end
   
   desc "start the app"
@@ -90,20 +48,27 @@ namespace :deploy do
     run "#{sudo} monit monitor sidekiq"
   end
   
-  #namespace :assets do
-  #  task :precompile, roles: :web, except: {no_release: true} do
-  #    from = source.next_revision(current_revision)
-  #    if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
-  #      run <<-CMD.compact
-  #        cd -- #{latest_release} &&
-  #        #{rake} RAILS_ENV=#{rails_env.to_s.shellescape} #{asset_env} assets:precompile &&
-  #        cp -- #{shared_path.shellescape}/assets/manifest.yml #{current_release.shellescape}/assets_manifest.yml
-  #      CMD
-  #    else
-  #      logger.info "Skipping asset pre-compilation because there were no asset changes"
-  #    end
-  #  end
-  #end
+  namespace :assets do
+    task :precompile, roles: :web, except: {no_release: true} do
+      changes = 0
+      begin
+        from = source.next_revision(current_revision)
+        changes = capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i
+      rescue
+        changes = 1
+      end
+
+      if changes > 0
+        run <<-CMD.compact
+          cd -- #{latest_release} &&
+          #{rake} RAILS_ENV=#{rails_env.to_s.shellescape} #{asset_env} assets:precompile &&
+          cp -- #{shared_path.shellescape}/assets/manifest.yml #{current_release.shellescape}/assets_manifest.yml
+        CMD
+      else
+        logger.info "Skipping asset pre-compilation because there were no asset changes"
+      end
+    end
+  end
   
   task :copy_old_sitemap do
     run "if [ -e #{previous_release}/public/sitemap_index.xml.gz ]; then cp #{previous_release}/public/sitemap* #{current_release}/public/; fi"
@@ -118,19 +83,15 @@ after "deploy:update_code", "deploy:copy_old_sitemap"
 
 before "deploy", "deploy:sudo_prompt"
 
-#after "deploy:finalize_update",
-#  "deploy:nginx_symlink", "deploy:monit_symlink", "deploy:riemann_symlink"
-
 after "deploy:restart",
   "deploy:reload_unicorn"
-#  "deploy:nginx_reload", "deploy:monit_reload", "deploy:riemann_restart"
 
 before "sidekiq:restart", "deploy:stop_monit_sidekiq"
 after "sidekiq:restart", "deploy:start_monit_sidekiq"
 
 after "deploy:migrate", "deploy:reload_unicorn"
 
-#after "deploy:update", "newrelic:notice_deployment"
+after "deploy:update", "newrelic:notice_deployment"
 
 # To keep only the last 5 releases:
 # (Default is `set :keep_releases, 5`)
