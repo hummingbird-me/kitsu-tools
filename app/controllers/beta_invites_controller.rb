0bdbe58be5100df525c9f6b335814ea79150da22
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-06-28 09:09:48 +0530

    Add ANIMESUGGEST invite code.

--- app/controllers/beta_invites_controller.rb
+++ app/controllers/beta_invites_controller.rb
@@ -11,7 +11,7 @@ class BetaInvitesController < ApplicationController
   end
 
   def invite_code
-    valid_codes = %w[ACEN2013 ANICHARTNET THEFAN]
+    valid_codes = %w[ACEN2013 ANICHARTNET THEFAN ANIMESUGGEST]
     email = params[:email].strip.downcase
     code = params[:invite_code].strip.upcase
     
