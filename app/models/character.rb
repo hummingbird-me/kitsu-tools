6d8ba7c88b72f3a42c5213831868235831fd3e72
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-06-30 21:05:38 +0530

    Default avatar!

--- app/models/character.rb
+++ app/models/character.rb
@@ -2,7 +2,7 @@ class Character < ActiveRecord::Base
   attr_accessible :description, :name, :mal_id
   validates :name, :presence => true
   has_attached_file :image, :styles => {:thumb_small => "30x47", :thumb => "225x350"}, 
-    :default_url => "http://placekitten.com/225/350"
+    :default_url => "/assets/default-avatar.jpg"
 
   has_many :castings, dependent: :destroy
 end
