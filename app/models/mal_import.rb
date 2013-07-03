993b162e8bce33831d5bf54ee70f967745ddfc6c
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-07-04 02:10:21 +0530

    More string surgery to make the MAL XML valid.

--- app/models/mal_import.rb
+++ app/models/mal_import.rb
@@ -146,10 +146,23 @@ class MalImport
     
     return meta
   end
+  
+  VALID_XML_CHARS = /^(
+      [\x09\x0A\x0D\x20-\x7E] # ASCII
+    | [\xC2-\xDF][\x80-\xBF] # non-overlong 2-byte
+    | \xE0[\xA0-\xBF][\x80-\xBF] # excluding overlongs
+    | [\xE1-\xEC\xEE][\x80-\xBF]{2} # straight 3-byte
+    | \xEF[\x80-\xBE]{2} #
+    | \xEF\xBF[\x80-\xBD] # excluding U+fffe and U+ffff
+    | \xED[\x80-\x9F][\x80-\xBF] # excluding surrogates
+    | \xF0[\x90-\xBF][\x80-\xBF]{2} # planes 1-3
+    | [\xF1-\xF3][\x80-\xBF]{3} # planes 4-15
+    | \xF4[\x80-\x8F][\x80-\xBF]{2} # plane 16
+  )*$/nx;
 
   def self.fetch_watchlist_from_remote(username)
     watchlist = []
-    animelist = Hash.from_xml(open("http://myanimelist.net/malappinfo.php?status=all&type=anime&u=#{username}").read.encode('UTF-16', invalid: :replace, replace: '').encode('UTF-8'))
+    animelist = Hash.from_xml(open("http://myanimelist.net/malappinfo.php?status=all&type=anime&u=#{username}").read.encode('ASCII-8BIT', invalid: :replace, undef: :replace, replace: '').split('').select {|x| x =~ VALID_XML_CHARS }.join.encode('UTF-8'))
     status_map = {
       "1"           => "Currently Watching",
       "watching"    => "Currently Watching",
