af11e5198eba9f96b48bbb9975bed7d8fde8c67b
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-07-03 09:44:33 +0530

    Fix post count and last post avatar.

--- app/decorators/controllers/forem/forums_controller_decorator.rb
+++ app/decorators/controllers/forem/forums_controller_decorator.rb
@@ -1,4 +1,17 @@
 Forem::ForumsController.class_eval do
+  def last_post_and_count(topics)
+    last_posts = {}
+    topics.each do |topic|
+      if user_signed_in? and (current_user.ninja_banned? or current_user.admin?)
+        posts = topic.posts
+      else
+        posts = topic.posts.joins(:user).where('NOT users.ninja_banned')
+      end
+      last_posts[topic.id] = [posts.last, posts.count]
+    end
+    last_posts
+  end
+  
   def index
     @forums = Forem::Forum.order(:name)
     if user_signed_in? and (current_user.ninja_banned? or current_user.admin?)
@@ -6,6 +19,7 @@ Forem::ForumsController.class_eval do
     else
       @topics = Forem::Topic.by_most_recent_post.joins(:user).where('NOT users.ninja_banned').page(params[:page]).per(Forem.per_page)
     end
+    @last_post_and_count = last_post_and_count(@topics)
   end
   
   def show
@@ -22,6 +36,8 @@ Forem::ForumsController.class_eval do
     else
       @topics = @topics.by_pinned_or_most_recent_post.joins(:user).where('NOT users.ninja_banned').page(params[:page]).per(Forem.per_page)
     end
+    
+    @last_post_and_count = last_post_and_count(@topics)
     
     respond_to do |format|
       format.html
