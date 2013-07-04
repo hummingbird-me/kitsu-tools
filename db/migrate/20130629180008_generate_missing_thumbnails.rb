c398586e66e61bfe63785f58d143d019ea4e0f61
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-07-04 06:36:52 +0530

    Don't fail the migration if we can't generate a thumbnail.

--- db/migrate/20130629180008_generate_missing_thumbnails.rb
+++ db/migrate/20130629180008_generate_missing_thumbnails.rb
@@ -1,10 +1,16 @@
 class GenerateMissingThumbnails < ActiveRecord::Migration
   def up
     Character.find_each do |c|
-      c.image.reprocess! :thumb_small
+      begin
+        c.image.reprocess! :thumb_small
+      rescue
+      end
     end
     Person.find_each do |c|
-      c.image.reprocess! :thumb_small
+      begin
+        c.image.reprocess! :thumb_small
+      rescue
+      end
     end
   end
 
