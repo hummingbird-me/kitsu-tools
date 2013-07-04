e06b72a719e0ef136c204e71f96b3a91b177d0d5
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-07-04 11:59:07 +0530

    Fuck it.

--- db/migrate/20130629180008_generate_missing_thumbnails.rb
+++ db/migrate/20130629180008_generate_missing_thumbnails.rb
@@ -1,17 +1,17 @@
 class GenerateMissingThumbnails < ActiveRecord::Migration
   def up
-    Character.find_each do |c|
-      begin
-        c.image.reprocess! :thumb_small
-      rescue
-      end
-    end
-    Person.find_each do |c|
-      begin
-        c.image.reprocess! :thumb_small
-      rescue
-      end
-    end
+#    Character.find_each do |c|
+#      begin
+#        c.image.reprocess! :thumb_small
+#      rescue
+#      end
+#    end
+#    Person.find_each do |c|
+#      begin
+#        c.image.reprocess! :thumb_small
+#      rescue
+#      end
+#    end
   end
 
   def down
