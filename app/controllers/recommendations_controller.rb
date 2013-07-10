60e57bc547fed1c0dd54ee47b62f8cd869013183
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-07-10 07:49:48 +0530

    Catch errors for more robust recommendations.

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -28,15 +28,15 @@ class RecommendationsController < ApplicationController
         @status_categories = ["currently_watching", "plan_to_watch", "completed"]
         @recommendations = {}
         @status_categories.each do |cat|
-          @recommendations[cat] = r ? JSON.parse(r.recommendations["by_status"])[cat].map {|x| Anime.find(x) } : []
+          @recommendations[cat] = JSON.parse(r.recommendations["by_status"])[cat].map {|x| Anime.find(x) } rescue []
         end
 
         @genre_recommendations = {}
         current_user.favorite_genres.each do |genre|
-          @genre_recommendations[ genre.slug ] = r ? JSON.parse(r.recommendations["by_genre"])[genre.slug].map {|x| Anime.find(x) } : []
+          @genre_recommendations[ genre.slug ] = JSON.parse(r.recommendations["by_genre"])[genre.slug].map {|x| Anime.find(x) } rescue []
         end
 
-        @neon_alley = r ? JSON.parse(r.recommendations["by_service"])["neon_alley"].map {|x| Anime.find(x) } : []
+        @neon_alley = JSON.parse(r.recommendations["by_service"])["neon_alley"].map {|x| Anime.find(x) } rescue []
         
         # View convenience variables. Move to translations later.
         @word_before = {
