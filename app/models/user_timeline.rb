b5fa6bf3e90793b67b2a6aaf7700d788e0142d29
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-07-11 16:11:59 +0530

    Added documentation on how the feed should work.

--- app/models/user_timeline.rb
+++ app/models/user_timeline.rb
@@ -1,4 +1,32 @@
-# This implements the feed users see on their homepages.
+# Implementation of the timeline shown on user homepages.
+#
+# Definitions:
+#
+# * For the purposes of the user timeline, an active user is defined as a user who
+#   has generated a story in the last INACTIVE_DAYS days.
+#
+# Parameters:
+# * INACTIVE_DAYS: Number of days before a user is considered to be inactive.
+# * CACHE_SIZE: Number of stories shown in a user's timeline.
+# * UPDATE_FREQ: For how much time is a cached timeline considered active.
+#
+# What needs to be stored in Redis for the timelines:
+#
+# * [ ] Last story update time for all users, in a sorted set.
+# * [ ] Last timeline update time for all active users.
+# * [ ] Set of followed users, for every user.
+#
+# How it should works:
+#
+# 1. Timelines for all active users are cached in Redis.
+# 2. Each cached timeline will hold a maximum of CACHE_SIZE stories.
+# 3. When the timeline is fetched, if it is more than UPDATE_FREQ seconds old, 
+#    update it.
+# 4. Updating the timeline involves finding the last CACHE_SIZE active users 
+#
+# Current Implementation Status:
+#
+#   Nothing to see here. >_<
 
 class UserTimeline
   def self.fetch(user, options={})
