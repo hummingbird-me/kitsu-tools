3b8f9a9e3da2ee43f388de5bca5d641ba4e41e83
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-07-21 14:07:19 +0530

    Add instrumentation to debug Sidekiq memory leak in production.

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,29 @@
+require 'json'
+
+class ObjectSpaceSidekiqMiddleware
+  def call(worker, job, queue)
+    @@logger ||= Logger.new '/tmp/sidekiq_mem.log'
+    
+    GC.start
+    initial_memory = `ps -o rss= -p #{Process.pid}`.to_i
+    
+    yield
+    
+    GC.start
+    final_memory = `ps -o rss= -p #{Process.pid}`.to_i
+
+    report = {}
+    report[:memory_delta] = final_memory - initial_memory
+    report[:class]        = job["class"]
+    report[:args]         = job["args"]
+    report[:time]         = Time.now
+
+    @@logger.info report.to_json
+  end
+end
+
+Sidekiq.configure_server do |config|
+  config.server_middleware do |chain|
+    chain.add ObjectSpaceSidekiqMiddleware
+  end
+end
