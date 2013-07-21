f6b33448afd51ca6c7f117ab8e436db5f6392ba1
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-07-21 15:07:15 +0530

    Turn off Sidekiq profiling.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,29 +1,31 @@
-require 'json'
+if false
+  require 'json'
 
-class ObjectSpaceSidekiqMiddleware
-  def call(worker, job, queue)
-    @@logger ||= Logger.new '/tmp/sidekiq_mem.log'
-    
-    GC.start
-    initial_memory = `ps -o rss= -p #{Process.pid}`.to_i
-    
-    yield
-    
-    GC.start
-    final_memory = `ps -o rss= -p #{Process.pid}`.to_i
+  class ObjectSpaceSidekiqMiddleware
+    def call(worker, job, queue)
+      @@logger ||= Logger.new '/tmp/sidekiq_mem.log'
+      
+      GC.start
+      initial_memory = `ps -o rss= -p #{Process.pid}`.to_i
+      
+      yield
+      
+      GC.start
+      final_memory = `ps -o rss= -p #{Process.pid}`.to_i
 
-    report = {}
-    report[:memory_delta] = final_memory - initial_memory
-    report[:class]        = job["class"]
-    report[:args]         = job["args"]
-    report[:time]         = Time.now
+      report = {}
+      report[:memory_delta] = final_memory - initial_memory
+      report[:class]        = job["class"]
+      report[:args]         = job["args"]
+      report[:time]         = Time.now
 
-    @@logger.info report.to_json
+      @@logger.info report.to_json
+    end
   end
-end
 
-Sidekiq.configure_server do |config|
-  config.server_middleware do |chain|
-    chain.add ObjectSpaceSidekiqMiddleware
+  Sidekiq.configure_server do |config|
+    config.server_middleware do |chain|
+      chain.add ObjectSpaceSidekiqMiddleware
+    end
   end
 end
