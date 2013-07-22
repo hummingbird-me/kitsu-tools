63819d64e008b208b6c20fe57b66df77db773bf6
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-07-22 23:02:17 +0530

    Hoom up that forum widget.

--- app/controllers/anime_controller.rb
+++ app/controllers/anime_controller.rb
@@ -101,6 +101,7 @@ class AnimeController < ApplicationController
       @trending_anime = TrendingAnime.get(6).map {|x| Anime.find(x) }
       @recent_reviews = Review.order('created_at DESC').limit(12).includes(:anime)
       @trending_reviews = TrendingReview.get(6).map {|x| Review.find(x) }
+      @forum_topics = Forem::Forum.find("anime-manga").topics.by_most_recent_post.joins(:user).where('NOT users.ninja_banned').limit(10)
     end
 
     return
