ebe2d7eab47ab3a9c962bfceec367c15849866df
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-07-24 00:10:22 +0530

    Unique user for trending anime fix.

--- app/controllers/anime_controller.rb
+++ app/controllers/anime_controller.rb
@@ -155,7 +155,15 @@ class AnimeController < ApplicationController
     else
       @trending_anime = TrendingAnime.get(6).map {|x| Anime.find(x) }
       @recent_reviews = Review.order('created_at DESC').limit(12).includes(:anime)
-      @trending_reviews = Review.select("DISTINCT ON (wilson_score, user_id) *").where("created_at >= ?", 30.days.ago).order("wilson_score DESC").limit(6)
+      trending_review_candidates = Review.where("created_at >= ?", 30.days.ago).order("wilson_score DESC").limit(30)
+      @trending_reviews = []
+      trending_review_candidates.each do |candidate|
+        unless @trending_reviews.any? {|r| r.user_id == candidate.user_id }
+          @trending_reviews.push candidate
+        end
+        break if @trending_reviews.length == 6
+      end
+      
       @forum_topics = Forem::Forum.find("anime-manga").topics.by_most_recent_post.joins(:user).where('NOT users.ninja_banned').limit(10)
     end
 
