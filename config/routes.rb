5eff4116c35afec01ec41953240f7b940fd70f8d
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-07-25 09:55:10 +0530

    404 the chat.

--- config/routes.rb
+++ config/routes.rb
@@ -19,10 +19,10 @@ Hummingbird::Application.routes.draw do
   match '/recommendations/plan_to_watch' => 'recommendations#plan_to_watch'
 
   # Chat
-  match '/chat' => 'chat#index'
-  match '/chat/ping' => 'chat#ping'
-  match '/chat/messages' => 'chat#messages'
-  match '/chat/new_message' => 'chat#new_message'
+  #match '/chat' => 'chat#index'
+  #match '/chat/ping' => 'chat#ping'
+  #match '/chat/messages' => 'chat#messages'
+  #match '/chat/new_message' => 'chat#new_message'
 
   match "/beta_invites/resend_invite" => "beta_invites#resend_invite", as: :resend_beta_invite
   match "/beta_invites/invite_code" => "beta_invites#invite_code"
