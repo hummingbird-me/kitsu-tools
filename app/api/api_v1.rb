c79fa5caa5b88f2e293424b6e010e8a6d6173e04
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-07-28 02:29:37 +0530

    Attempt to reduce memory leakage from Grape.

--- app/api/api_v1.rb
+++ app/api/api_v1.rb
@@ -2,23 +2,6 @@ require_relative 'entities.rb'
 
 class API_v1 < Grape::API
   version 'v1', using: :path, format: :json, vendor: 'hummingbird'
-
-  #
-  # Log response time to Riemann.
-  #
-  before do
-    @start_time = Time.now
-  end
-  
-  after do
-    @end_time = Time.now
-    lat = (@end_time - @start_time).to_f
-    $riemann << {
-      service: "api req",
-      metric: lat,
-      state: "ok"
-    }
-  end
   
   helpers do
     def warden; env['warden']; end
@@ -64,17 +47,17 @@ class API_v1 < Grape::API
       requires :password, type: String
     end
     post '/authenticate' do
-      @user = nil
+      user = nil
       if params[:username]
-        @user = User.find_by_name params[:username]
+        user = User.find_by_name params[:username]
       elsif params[:email]
-        @user = User.find_by_email params[:email]
+        user = User.find_by_email params[:email]
       end
-      if @user.nil? or (not @user.valid_password? params[:password])
+      if user.nil? or (not user.valid_password? params[:password])
         error!("Invalid credentials", 401)
       end
-      @user.reset_authentication_token! if @user.authentication_token.nil?
-      return @user.authentication_token
+      user.reset_authentication_token! if user.authentication_token.nil?
+      return user.authentication_token
     end
     
     desc "Return the entries in a user's library under a specific status.", {
@@ -87,13 +70,13 @@ class API_v1 < Grape::API
       optional :title_language_preference, type: String
     end
     get ':user_id/library' do
-      @user = find_user(params[:user_id])
+      user = find_user(params[:user_id])
       status = Watchlist.status_parameter_to_status(params[:status])
 
-      if @user == current_user
-        watchlists = @user.watchlists.where(status: status).order(status == "Currently Watching" ? "last_watched DESC" : "created_at DESC").includes(:anime)
+      if user == current_user
+        watchlists = user.watchlists.where(status: status).order(status == "Currently Watching" ? "last_watched DESC" : "created_at DESC").includes(:anime)
       else
-        watchlists = @user.watchlists.accessible_by(current_ability).where(status: status).order(status == "Currently Watching" ? 'last_watched DESC' : 'created_at DESC').includes(:anime)
+        watchlists = user.watchlists.accessible_by(current_ability).where(status: status).order(status == "Currently Watching" ? 'last_watched DESC' : 'created_at DESC').includes(:anime)
       end
       watchlists = watchlists.page(params[:page]).per(400)
       
@@ -112,10 +95,10 @@ class API_v1 < Grape::API
       optional :page, type: Integer
     end
     get ":user_id/feed" do
-      @user = find_user(params[:user_id])
-      @stories = @user.stories.accessible_by(current_ability).order('updated_at DESC').includes(:substories).page(params[:page]).per(20)
+      user = find_user(params[:user_id])
+      stories = user.stories.accessible_by(current_ability).order('updated_at DESC').includes(:substories).page(params[:page]).per(20)
       
-      present @stories, with: Entities::Story, current_ability: current_ability, title_language_preference: (user_signed_in? ? current_user.title_language_preference : "canonical")
+      present stories, with: Entities::Story, current_ability: current_ability, title_language_preference: (user_signed_in? ? current_user.title_language_preference : "canonical")
     end
     
     desc "Delete a substory from the user's feed."
@@ -146,67 +129,67 @@ class API_v1 < Grape::API
     post ':anime_slug' do
       authenticate_user!
 
-      @anime = Anime.find(params["anime_slug"])
-      @watchlist = Watchlist.find_or_create_by_anime_id_and_user_id(@anime.id, current_user.id)
+      anime = Anime.find(params["anime_slug"])
+      watchlist = Watchlist.find_or_create_by_anime_id_and_user_id(anime.id, current_user.id)
         
       # Update status.
       if params[:status]
         status = Watchlist.status_parameter_to_status(params[:status])
-        if @watchlist.status != status
+        if watchlist.status != status
           # Create an action if the status was changed.
           Substory.from_action({
             user_id: current_user.id,
             action_type: "watchlist_status_update",
-            anime_id: @anime.slug,
-            old_status: @watchlist.status,
+            anime_id: anime.slug,
+            old_status: watchlist.status,
             new_status: status,
             time: Time.now
           })
         end
-        @watchlist.status = status if Watchlist.valid_statuses.include? status
+        watchlist.status = status if Watchlist.valid_statuses.include? status
         if status == "Completed"
           # Mark all episodes as viewed when the show is "Completed".
-          @watchlist.update_episode_count (@watchlist.anime.episode_count || 0)
+          watchlist.update_episode_count (watchlist.anime.episode_count || 0)
         end
       end
       
       # Update privacy.
       if params[:privacy]
         if params[:privacy] == "private"
-          @watchlist.private = true
+          watchlist.private = true
         elsif params[:privacy] == "public"
-          @watchlist.private = false
+          watchlist.private = false
         end
       end
 
       # Update rating.
       if params[:rating]
-        if @watchlist.rating == params[:rating].to_i
-          @watchlist.rating = nil
+        if watchlist.rating == params[:rating].to_i
+          watchlist.rating = nil
         else
-          @watchlist.rating = [ [1, params[:rating].to_i].max, 5 ].min
+          watchlist.rating = [ [1, params[:rating].to_i].max, 5 ].min
         end
       end
 
       # Update rewatched_times.
       if params[:rewatched_times]
-        @watchlist.update_rewatched_times params[:rewatched_times]
+        watchlist.update_rewatched_times params[:rewatched_times]
       end
 
       # Update notes.
       if params[:notes]
-        @watchlist.notes = params[:notes]
+        watchlist.notes = params[:notes]
       end
       
       # Update episode count.
       if params[:episodes_watched]
-        @watchlist.update_episode_count params[:episodes_watched]
+        watchlist.update_episode_count params[:episodes_watched]
       end
 
       if params[:increment_episodes]
-        @watchlist.status = "Currently Watching"
-        @watchlist.update_episode_count((@watchlist.episodes_watched||0)+1)
-        if current_user.neon_alley_integration? and Anime.neon_alley_ids.include? @anime.id
+        watchlist.status = "Currently Watching"
+        watchlist.update_episode_count((watchlist.episodes_watched||0)+1)
+        if current_user.neon_alley_integration? and Anime.neon_alley_ids.include? anime.id
           service = "neon_alley"
         else
           service = nil
@@ -214,15 +197,15 @@ class API_v1 < Grape::API
         Substory.from_action({
           user_id: current_user.id,
           action_type: "watched_episode",
-          anime_id: @anime.slug,
-          episode_number: @watchlist.episodes_watched,
+          anime_id: anime.slug,
+          episode_number: watchlist.episodes_watched,
           service: service
         })
-        if @watchlist.status == "Completed"
+        if watchlist.status == "Completed"
           Substory.from_action({
             user_id: current_user.id,
             action_type: "watchlist_status_update",
-            anime_id: @anime.slug,
+            anime_id: anime.slug,
             old_status: "Currently Watching",
             new_status: "Completed",
             time: Time.now + 5.seconds
@@ -236,8 +219,8 @@ class API_v1 < Grape::API
       end
       title_language_preference ||= "canonical"
       
-      if @watchlist.save
-        present @watchlist, with: Entities::Watchlist, title_language_preference: title_language_preference
+      if watchlist.save
+        present watchlist, with: Entities::Watchlist, title_language_preference: title_language_preference
       else
         return false
       end
