c84ce279f25eae423bf858a37bb37e5e4cbf9828
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-07-29 22:47:58 +0530

    dk

--- app/models/casting.rb
+++ app/models/casting.rb
@@ -2,7 +2,7 @@ class Casting < ActiveRecord::Base
   belongs_to :anime
   belongs_to :person
   belongs_to :character
-  attr_accessible :role, :type, :anime_id, :person_id, :character_id, :featured
+  attr_accessible :role, :type, :anime_id, :person_id, :character_id, :featured, :voice_actor
 
   def name
     "#{character.try(:name)} ... #{person.try(:name)}"
