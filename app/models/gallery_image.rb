41a0096dca1acbef6ad559c3f90035ae12fd11a6
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-07-31 04:35:52 +0530

    Gallery Images on S3.

--- app/models/gallery_image.rb
+++ app/models/gallery_image.rb
@@ -1,6 +1,16 @@
 class GalleryImage < ActiveRecord::Base
   belongs_to :anime
   attr_accessible :anime_id, :description, :image
-  has_attached_file :image, styles: {thumb: '380x214'}
   validates :anime, :image, presence: true
+
+  has_attached_file :image, 
+    styles: {thumb: '265x144'},
+    storage: :s3,
+    s3_credentials: {
+      bucket: ENV['AWS_BUCKET'],
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+    },
+    url: ':s3_domain_url',
+    path: '/:class/:attachment/:id_partition/:style/:filename'
 end
