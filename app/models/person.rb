0a6b57d0176d3bdbaf8b3a8b13af44309dc90fed
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-07-31 03:40:41 +0530

    S3 Domain URL.

--- app/models/person.rb
+++ app/models/person.rb
@@ -12,5 +12,6 @@ class Person < ActiveRecord::Base
       bucket: ENV['AWS_BUCKET'],
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
-    }
+    },
+    url: ':s3_domain_url'
 end
