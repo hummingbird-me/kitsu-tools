b8be1dc0a00988a8844a56ba5a4f611bb43cf5d2
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-07-31 04:09:05 +0530

    Use S3 for characters as well.

--- app/models/character.rb
+++ app/models/character.rb
@@ -6,5 +6,13 @@ class Character < ActiveRecord::Base
   has_attached_file :image, 
     styles: {thumb_small: "30x39#"}, 
     default_url: "/assets/default-avatar.jpg",
-    convert_options: {all: "-unsharp 2x0.5+1+0"}
+    convert_options: {all: "-unsharp 2x0.5+1+0"},
+    storage: :s3,
+    s3_credentials: {
+      bucket: ENV['AWS_BUCKET'],
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+    },
+    url: ':s3_domain_url',
+    path: '/:class/:attachment/:id_partition/:style/:filename'
 end
