037b54c5fa1d49d6c32399ad587095c91b08d4c2
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-07-31 23:28:36 +0530

    Get your shit together, Sidekiq.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,9 @@
+require 'uri/redis'
+
 Sidekiq.configure_server do |config|
-  config.redis = { :url => "#{ENV['REDIS_HOST']}:6379/1" }
+  config.redis = { :url => "redis://#{ENV['REDIS_HOST']}:6379/1" }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { :url => "#{ENV['REDIS_HOST']}:6379/1" }
+  config.redis = { :url => "redis://#{ENV['REDIS_HOST']}:6379/1" }
 end
