84420a1fde1812c53a227644eb37b6a77045ea92
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-07-31 23:20:30 +0530

    Attempt to fix Sidekiq.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { :url => "redis://#{ENV['REDIS_HOST']}:6379/1" }
+  config.redis = { :url => "#{ENV['REDIS_HOST']}:6379/1" }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { :url => "redis://#{ENV['REDIS_HOST']}:6379/1" }
+  config.redis = { :url => "#{ENV['REDIS_HOST']}:6379/1" }
 end
