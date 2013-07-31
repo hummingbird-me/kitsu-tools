abd86a1d47b158f49101b4e6cce99e2aec4716c4
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-07-31 06:15:05 +0530

    Anime: set s3 protocol.

--- app/models/anime.rb
+++ app/models/anime.rb
@@ -22,6 +22,7 @@ class Anime < ActiveRecord::Base
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
     },
+    s3_protocol: 'http',
     url: ':s3_domain_url',
     path: '/:class/:attachment/:id_partition/:style/:filename'
     
