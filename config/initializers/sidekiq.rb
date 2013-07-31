7c4f676ba8284bba1852c67c43f73fb9389665e4
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-07-31 23:35:28 +0530

    Pls work ;_;

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,9 @@
 Sidekiq.configure_server do |config|
-  config.redis = { :url => "tcp://#{ENV['REDIS_HOST']}:6379/1" }
+  #config.redis = { :url => "tcp://#{ENV['REDIS_HOST']}:6379/1" }
+  config.redis = { :url => "tcp://localhost:6379/1" }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { :url => "tcp://#{ENV['REDIS_HOST']}:6379/1" }
+  #config.redis = { :url => "tcp://#{ENV['REDIS_HOST']}:6379/1" }
+  config.redis = { :url => "tcp://localhost:6379/1" }
 end
