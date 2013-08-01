bef2315bd65764e27db3c0a381a28a7ff1197a13
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-08-01 13:49:38 +0530

    Onboarding after creating an account via Facebook.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,10 +4,12 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     
     if @user.persisted?
       flash[:notice] = "Successfully signed in using Facebook."
+      sign_in_and_redirect @user, :event => :authentication
     else
       flash[:notice] = "Successfully created a new account using Facebook."
+      sign_in @user
+      redirect_to "/?signup_tour=true"
     end
 
-    sign_in_and_redirect @user, :event => :authentication
   end
 end
