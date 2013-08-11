2b4af0a9a7f927c4b1236b61a6ef21b0e7d46e57
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-08-11 09:27:19 +0530

    Underscore.js > jQuery.ba-throttle-debounce.js. Tiny reduction in JS size.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,7 +23,6 @@
 //= require typeahead.js
 //= require offcanvas.js
 //= require jquery.easy-pie-chart.js
-//= require jquery.ba-throttle-debounce.js
 //= require jquery.icheck.js
 //= require vendor/custom.modernizr
 //= require googleplus.js
