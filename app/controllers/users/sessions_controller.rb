6516ef6fe261f78ea6f2093f595eef7c6ca531cb
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-08-13 16:25:32 +0530

    Return user to the same page after modal login.

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,12 +1,8 @@
 class Users::SessionsController < Devise::SessionsController
   def new
-    session["user_return_to"] = request.referer
+    session["user_return_to"] = params[:return_to] || request.referer
     self.resource = build_resource(nil, :unsafe => true)
     clean_up_passwords(resource)
-    if params[:return_to]
-      redirect_to params[:return_to]
-    else
-      respond_with(resource, serialize_options(resource))
-    end
+    respond_with(resource, serialize_options(resource))
   end
 end
