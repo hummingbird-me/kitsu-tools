581ff854e91b8abaf7d40713cbcad2596e52f4bc
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-08-14 22:30:43 +0530

    Change notification model a bit.

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,4 +1,6 @@
 class Notification < ActiveRecord::Base
   belongs_to :user
+  belongs_to :source
   attr_accessible :data
+  serialize :data, ActiveRecord::Coders::Hstore
 end
--- db/migrate/20130814165800_create_notifications.rb
+++ db/migrate/20130814165800_create_notifications.rb
@@ -2,10 +2,13 @@ class CreateNotifications < ActiveRecord::Migration
   def change
     create_table :notifications do |t|
       t.references :user
+      t.references :source, polymorphic: true
       t.hstore :data
 
       t.timestamps
     end
+
     add_index :notifications, :user_id
+    add_index :notifications, :source_id
   end
 end
