e4b50a6d1173762efd307966367c95211f04fab3
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-08-24 17:19:05 +0530

    Fix typo.

--- app/api/entities.rb
+++ app/api/entities.rb
@@ -26,7 +26,7 @@ module Entities
     expose :about
     expose :bio
     expose(:karma) {|user, options| user.reputation_for(:karma) }
-    expose :life_spend_on_anime
+    expose :life_spent_on_anime
     expose(:show_adult_content) {|user, options| !user.sfw_filter }
     expose :title_language_preference
     expose :last_library_update
