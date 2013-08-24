00bedd97b07d7820bf9c0548ff946f2e4608cdca
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-08-24 14:14:47 +0530

    Revert "Change email sender."
    This reverts commit 0503bbb5574d76a0a1bcb5ee53daf3125dbaf85f.

--- config/initializers/forem.rb
+++ config/initializers/forem.rb
@@ -1,5 +1,5 @@
 Forem.user_class = "User"
-Forem.email_from_address = "josh@hummingbird.me"
+Forem.email_from_address = "vikhyat@hummingbird.me"
 # If you do not want to use gravatar for avatars then specify the method to use here:
 # Forem.avatar_user_method = :custom_avatar_url
 Forem.per_page = 20
