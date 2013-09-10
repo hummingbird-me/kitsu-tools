e8ff1c79f373d82acd8f58fd9920965959abf4e2
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-09-10 12:17:41 +0530

    Allow async email sending.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,8 @@
 class UserMailer < ActionMailer::Base
   default from: "josh@hummingbird.me"
 
-  def newsletter(user)
-    @user = user
+  def newsletter(user_id)
+    @user = User.find user_id
     if @user.subscribed_to_newsletter?
       @subject = "Wait, it's that time of the month already?"
       mail(to: user.email, subject: @subject)
