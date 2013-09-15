f6fadafed3264974146b81d7f867dd2af4231a14
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-09-15 20:15:40 +0530

    Admin authentication.

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -9,11 +9,13 @@ Doorkeeper.configure do
   end
 
   # If you want to restrict access to the web interface for adding oauth authorized applications, you need to declare the block below.
-  # admin_authenticator do
-  #   # Put your admin authentication logic here.
-  #   # Example implementation:
-  #   Admin.find_by_id(session[:admin_id]) || redirect_to(new_admin_session_url)
-  # end
+  admin_authenticator do
+    # Put your admin authentication logic here.
+    # Example implementation:
+    unless user_signed_in? and current_user.admin?
+      redirect_to '/'
+    end
+  end
 
   # Authorization Code expiration time (default 10 minutes).
   # authorization_code_expires_in 10.minutes
