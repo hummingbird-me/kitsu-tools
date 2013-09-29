9891dc9842b76724f5f7e97fc2d8c0b67688cd5f
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-09-29 13:14:33 +0530

    Bring back image embedding in the feeds.

--- lib/message_formatter.rb
+++ lib/message_formatter.rb
@@ -1,7 +1,7 @@
 class MessageFormatter
   def self.format_message(message)
     formatted = Rinku.auto_link(ERB::Util.html_escape(message), :all, 'target="_blank"')
-    
+
     # Link @usernames.
     formatted = formatted.gsub(/@[-_A-Za-z0-9]+/) do |x|
       u = User.find_by_name(x[1..-1])
@@ -11,22 +11,22 @@ class MessageFormatter
         x
       end
     end
-    
+
     noko = Nokogiri::HTML.parse formatted
     links = noko.css('a').map {|link| link['href'] }
     if links.length > 0
       link = links[-1]
 
       # Embed images.
-      #if link =~ /\.(gif|jpe?g|png)$/i
-      #  begin
-      #    if open(link).size <= 1024*1024*2
-      #      formatted += "<br><img class='autoembed' src='#{link}' style='max-height: 200px; width: auto; max-width: 600' />"
-      #    end
-      #  rescue
-      #  end
-      #end
-      
+      if link =~ /\.(gif|jpe?g|png)$/i
+        begin
+          if open(link).size <= 1024*1024*2
+            formatted += "<br><img class='autoembed' src='#{link}' style='max-height: 200px; width: auto; max-width: 600' />"
+          end
+        rescue
+        end
+      end
+
       # Embed YouTube videos.
       if link =~ /(?:http:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)\/(?:watch\?v=)?(.+)/
         begin
@@ -36,7 +36,7 @@ class MessageFormatter
         end
       end
     end
-    
+
     formatted
   end
 end
