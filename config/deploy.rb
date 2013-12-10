94de8c11fca74a0e48052537d12968e4c2d23564
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-12-10 19:06:53 +0530

    Faster deploys, don't re-pre-compile assets unless needed.

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,17 +24,17 @@ namespace :deploy do
   task :sudo_prompt, roles: :web do
     run "#{sudo} ls"
   end
-  
+
   desc "start the app"
   task :start, roles: :web do
     run "#{sudo} monit -g hummingbird start"
   end
-  
+
   desc "stop the app"
   task :stop, roles: :web do
     run "#{sudo} monit -g hummingbird stop"
   end
-  
+
   desc "reload unicorn"
   task :reload_unicorn, roles: :web do
     run "kill -USR2 `cat /u/apps/hummingbird/shared/pids/unicorn.pid`"
@@ -44,33 +44,35 @@ namespace :deploy do
   task :stop_monit_sidekiq, roles: :sidekiq do
     run "#{sudo} monit unmonitor sidekiq"
   end
-  
+
   desc "resume monit sidekiq monitoring "
   task :start_monit_sidekiq, roles: :sidekiq do
     run "#{sudo} monit monitor sidekiq"
   end
-  
-  #namespace :assets do
-  #    changes = 0
-  #    begin
-  #      from = source.next_revision(current_revision)
-  #      changes = capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i
-  #    rescue
-  #      changes = 1
-  #    end
-  #
-  #    if changes > 0
-  #      run <<-CMD.compact
-  #        cd -- #{latest_release} &&
-  #        #{rake} RAILS_ENV=#{rails_env.to_s.shellescape} #{asset_env} assets:precompile &&
-  #        cp -- #{shared_path.shellescape}/assets/manifest.yml #{current_release.shellescape}/assets_manifest.yml
-  #      CMD
-  #    else
-  #      logger.info "Skipping asset pre-compilation because there were no asset changes"
-  #    end
-  #  end
-  #end
-  
+
+  namespace :assets do
+    desc "asset precompilation"
+    task :precompile do
+      changes = 0
+      begin
+        from = source.next_revision(current_revision)
+        changes = capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i
+      rescue
+        changes = 1
+      end
+
+      if changes > 0
+        run <<-CMD.compact
+          cd -- #{latest_release} &&
+          #{rake} RAILS_ENV=#{rails_env.to_s.shellescape} #{asset_env} assets:precompile &&
+          cp -- #{shared_path.shellescape}/assets/manifest.yml #{current_release.shellescape}/assets_manifest.yml
+        CMD
+      else
+        logger.info "Skipping asset pre-compilation because there were no asset changes"
+      end
+    end
+  end
+
   task :copy_old_sitemap do
     run "if [ -e #{previous_release}/public/sitemap_index.xml.gz ]; then cp #{previous_release}/public/sitemap* #{current_release}/public/; fi"
   end
