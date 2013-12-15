349a1dcf096602f600cf905dc39aa8be11aebc3b
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-12-15 17:11:14 +0530

    Add "season" to list of stop words.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,16 +1,20 @@
 class SearchController < ApplicationController
+  STOP_WORDS = /season/i
+
   def basic
     if true # user_signed_in? and (current_user.email == "c@vikhyat.net" or current_user.email == "josh@hummingbird.ly")
 
       # What are we searching for?
       @search_type = params[:type] || "anime"
-      
+
       if @search_type == "anime"
 
+        query = (params[:query] || "").gsub(STOP_WORDS, '')
+
         @anime = Anime.accessible_by(current_ability).page(params[:page]).per(20)
-        @anime_list = @anime.simple_search_by_title(params[:query])
+        @anime_list = @anime.simple_search_by_title(query)
         if @anime_list.length == 0
-          @anime_list = @anime.fuzzy_search_by_title(params[:query])
+          @anime_list = @anime.fuzzy_search_by_title(query)
         end
         @results = @anime_list
 
@@ -20,7 +24,7 @@ class SearchController < ApplicationController
             @watchlist_status[w.anime_id] = w.status
           end
         end
-        
+
         respond_to do |format|
           format.html { render "anime" }
           format.json { render :json => @results.map {|x| [x.title, x.alt_title] }.flatten.compact }
@@ -32,22 +36,22 @@ class SearchController < ApplicationController
           fulltext params[:query]
           paginate :page => (params[:page] || 1)
         end.results
-        
+
         render "forum_posts"
-        
+
       elsif @search_type == "users"
 
         @results = User.search do
           fulltext params[:query]
           paginate :page => (params[:page] || 1)
         end.results
-        
+
         render "users"
-        
+
       end
-      
+
     else
-      
+
       @anime = Anime.accessible_by(current_ability).page(params[:page]).per(18)
 
       @all_genres = Genre.order(:name)
@@ -69,7 +73,7 @@ class SearchController < ApplicationController
       else
         @watchlist = Hash.new(false)
       end
-      
+
       @anime_list = @anime.simple_search_by_title(params[:query])
       if @anime_list.length == 0
         @anime_list = @anime.fuzzy_search_by_title(params[:query])
