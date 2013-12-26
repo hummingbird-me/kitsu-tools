ed5f7251ff15599b19799082c5af3284903cd2c4
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-12-26 06:18:55 +0530

    Add manga search.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,84 +2,58 @@ class SearchController < ApplicationController
   STOP_WORDS = /season/i
 
   def basic
-    if true # user_signed_in? and (current_user.email == "c@vikhyat.net" or current_user.email == "josh@hummingbird.ly")
 
-      # What are we searching for?
-      @search_type = params[:type] || "anime"
+    # What are we searching for?
+    @search_type = params[:type] || "anime"
 
-      if @search_type == "anime"
+    if @search_type == "anime"
 
-        query = (params[:query] || "").gsub(STOP_WORDS, '')
+      query = (params[:query] || "").gsub(STOP_WORDS, '')
 
-        @anime = Anime.accessible_by(current_ability).page(params[:page]).per(20)
-        @anime_list = @anime.simple_search_by_title(query)
-        if @anime_list.length == 0
-          @anime_list = @anime.fuzzy_search_by_title(query)
-        end
-        @results = @anime_list
-
-        @watchlist_status = {}
-        if user_signed_in?
-          current_user.watchlists.where(anime_id: @results.map {|x| x.id }).each do |w|
-            @watchlist_status[w.anime_id] = w.status
-          end
-        end
-
-        respond_to do |format|
-          format.html { render "anime" }
-          format.json { render :json => @results.map {|x| [x.title, x.alt_title] }.flatten.compact }
-        end
-
-      elsif @search_type == "users"
-
-        @results = User.search do
-          fulltext params[:query]
-          paginate :page => (params[:page] || 1)
-        end.results
-
-        render "users"
-
-      end
-
-    else
-
-      @anime = Anime.accessible_by(current_ability).page(params[:page]).per(18)
-
-      @all_genres = Genre.order(:name)
-
-      # Filter by genre if needed.
-      if params[:genres] and params[:genres].length > 0
-        @genre_slugs  = params[:genres].split.uniq 
-        if @all_genres.count > @genre_slugs.length
-          @genre_filter = Genre.where("slug IN (?)", @genre_slugs)
-          @anime = @anime.joins(:genres)
-                         .where("genres.id IN (?)", @genre_filter.map(&:id))
-        end
-      end
-      @genre_filter ||= @all_genres
-
-      # Fetch the user's watchlist.
-      if user_signed_in?
-        @watchlist = current_user.watchlist_table
-      else
-        @watchlist = Hash.new(false)
-      end
-
-      @anime_list = @anime.simple_search_by_title(params[:query])
+      @anime = Anime.accessible_by(current_ability).page(params[:page]).per(20)
+      @anime_list = @anime.simple_search_by_title(query)
       if @anime_list.length == 0
-        @anime_list = @anime.fuzzy_search_by_title(params[:query])
+        @anime_list = @anime.fuzzy_search_by_title(query)
       end
-      @anime = @anime_list
+      @results = @anime_list
+
+      @watchlist_status = {}
+      if user_signed_in?
+        current_user.watchlists.where(anime_id: @results.map {|x| x.id }).each do |w|
+          @watchlist_status[w.anime_id] = w.status
+        end
+      end
 
       respond_to do |format|
-        format.html {
-          @collection = @anime.map {|x| [x, @watchlist[x.id]] }
-          render "anime/index", layout: "layouts/application"
-        }
-        format.json {
-          render :json => @anime.map {|x| [x.title, x.alt_title] }.flatten.compact
-        }
+        format.html { render "anime" }
+        format.json { render :json => @results.map {|x| [x.title, x.alt_title] }.flatten.compact }
       end
+
+    elsif @search_type == "manga"
+
+      query = (params[:query] || "").gsub(STOP_WORDS, '')
+
+      @manga = Manga.page(params[:page]).per(20)
+      @manga_list = @manga.simple_search_by_title(query)
+      if @manga_list.count == 0
+        @manga_list = @manga.fuzzy_search_by_title(query)
+      end
+      @results = @manga_list
+
+      respond_to do |format|
+        format.html { render :manga }
+      end
+
+    elsif @search_type == "users"
+
+      @results = User.search do
+        fulltext params[:query]
+        paginate :page => (params[:page] || 1)
+      end.results
+
+      render "users"
+
     end
+
   end
 end
