d72b82e27762a8d1b5ede59cf16431c038f8af9e
Vikhyat Korrapati <vikhyatk@gmail.com>
2014-01-04 11:58:27 +0530

    404 if topic not found.

--- app/controllers/community/topics_controller.rb
+++ app/controllers/community/topics_controller.rb
@@ -4,6 +4,7 @@ module Community
 
     def show
       @topic = Community::Topic.find_by_slug params[:id]
+      not_found! if @topic.nil?
       @first_post = @topic.posts.order(:created_at).first
       @posts = @topic.posts.order(:created_at).page(params[:page]).per(20)
     end
