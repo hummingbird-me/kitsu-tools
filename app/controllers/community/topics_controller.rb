0b883946dc4b8f4ed9177eb923d345180c6dcaa5
Vikhyat Korrapati <vikhyatk@gmail.com>
2014-01-19 03:40:06 +0530

    Fix accidental error instead of 404.

--- app/controllers/community/topics_controller.rb
+++ app/controllers/community/topics_controller.rb
@@ -4,7 +4,7 @@ module Community
 
     def show
       @topic = Community::Topic.find_by_slug params[:id]
-      not_found! if @topic.nil?
+      raise ActionController::RoutingError.new('Not Found') if @topic.nil?
       @first_post = @topic.posts.order(:created_at).first
       @posts = @topic.posts.order(:created_at).page(params[:page]).per(20)
     end
