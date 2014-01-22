f16a6762d4a890b8188f19c3586aefbf63a3a47c
Vikhyat Korrapati <vikhyatk@gmail.com>
2014-01-22 19:59:02 +0530

    temporarily add back quote reputation def.

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -12,6 +12,10 @@
 #
 
 class Quote < ActiveRecord::Base
+  has_reputation :votes, source: :user, source_of: [
+    {reputation: :quote_votes, of: :user}
+  ]
+
   attr_accessible :anime_id, :character_name, :content
 
   belongs_to :anime
