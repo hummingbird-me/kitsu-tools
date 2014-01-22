6e8c9cb6f4717a1916ee1ea6f2f77942667f8ac2
Vikhyat Korrapati <vikhyatk@gmail.com>
2014-01-22 20:52:21 +0530

    Remove caching from the notifications.

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -26,22 +26,22 @@ class Notification < ActiveRecord::Base
 
   def self.unseen_count(user_id)
     user_id = user_id.id unless user_id.is_a? Fixnum
-    Rails.cache.fetch(:"#{user_id}_unseen_notifications", expires_in: 60.minutes) do
-      Notification.where(user_id: user_id, seen: false).count
-    end
+    #Rails.cache.fetch(:"#{user_id}_unseen_notifications", expires_in: 60.minutes) do
+    Notification.where(user_id: user_id, seen: false).count
+    #end
   end
 
   def self.recent_notifications(user_id)
     user_id = user_id.id unless user_id.is_a? Fixnum
-    Rails.cache.fetch(:"#{user_id}_recent_notifications", expires_in: 60.minutes) do
-      Notification.where(user_id: user_id).order('CASE WHEN seen THEN 1 ELSE 0 END, created_at DESC').limit(3).dup
-    end
+    #Rails.cache.fetch(:"#{user_id}_recent_notifications", expires_in: 60.minutes) do
+    Notification.where(user_id: user_id).order('CASE WHEN seen THEN 1 ELSE 0 END, created_at DESC').limit(3).dup
+    #end
   end
 
   def self.uncache_notification_cache(user_id)
     user_id = user_id.id unless user_id.is_a? Fixnum
-    Rails.cache.delete(:"#{user_id}_unseen_notifications")
-    Rails.cache.delete(:"#{user_id}_recent_notifications")
+    #Rails.cache.delete(:"#{user_id}_unseen_notifications")
+    #Rails.cache.delete(:"#{user_id}_recent_notifications")
   end
 
   after_create do
