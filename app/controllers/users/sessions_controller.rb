f58236035932c9a4f68caddb2d242d2f6ff71b20
Vikhyat Korrapati <vikhyatk@gmail.com>
2014-01-22 20:55:01 +0530

    Fix sign in.

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,7 +1,7 @@
 class Users::SessionsController < Devise::SessionsController
   def new
     session["user_return_to"] = params[:return_to] || request.referer
-    self.resource = build_resource(nil, :unsafe => true)
+    self.resource = resource_class.new(sign_in_params)
     clean_up_passwords(resource)
     respond_with(resource, serialize_options(resource))
   end
