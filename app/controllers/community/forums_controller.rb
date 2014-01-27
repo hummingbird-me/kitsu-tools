c0c1dca982cfe12875509182b0f8d04cbe58ac99
Vikhyat Korrapati <vikhyatk@gmail.com>
2014-01-27 15:56:29 +0530

    Fix undefined method `topics' for nil:NilClass.

--- app/controllers/community/forums_controller.rb
+++ app/controllers/community/forums_controller.rb
@@ -8,6 +8,7 @@ module Community
 
     def show
       @forum = Community::Forum.where(slug: params[:id]).first
+      raise ActionController::RoutingError.new('Not Found') if @forum.nil?
       @topics = @forum.topics.by_pinned_or_most_recent_post.page(params[:page]).per(20)
     end
   end
