639dd38047dffd9ce4144a53febca624f6436e48
Vikhyat Korrapati <vikhyatk@gmail.com>
2014-01-31 09:10:18 +0530

    Change newsletter subject.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,7 +4,7 @@ class UserMailer < ActionMailer::Base
   def newsletter(user_id)
     @user = User.find user_id
     if @user.subscribed_to_newsletter?
-      @subject = "The Perfect Halloween Newsletter"
+      @subject = "The Newsletter That Leapt Through Time"
       mail(to: @user.email, subject: @subject)
     end
   end
