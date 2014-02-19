f3099619c8a9cd943f3c8c3077edc654a739b62d
Vikhyat Korrapati <vikhyatk@gmail.com>
2014-02-19 23:21:32 +0530

    oops...

--- app/models/library_entry.rb
+++ app/models/library_entry.rb
@@ -99,6 +99,7 @@ class LibraryEntry < ActiveRecord::Base
         "rating_frequencies = COALESCE(rating_frequencies, hstore(ARRAY[]::text[])) || hstore('#{nkey}', ((COALESCE((rating_frequencies -> '#{nkey}'), '0'))::integer + 1)::text)"
       )
     end
+  end
 
   after_save do
     # Vote for the show to appear on the trending list.
