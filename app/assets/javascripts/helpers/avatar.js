c50664dcb59b6258b0f932b8daf2e96853bea5ca
Vikhyat Korrapati <vikhyatk@gmail.com>
2014-03-15 19:38:39 +0530

    Fix small avatar JPG issue in the Ember app.

--- app/assets/javascripts/helpers/avatar.js
+++ app/assets/javascripts/helpers/avatar.js
@@ -1,4 +1,10 @@
 Ember.Handlebars.helper('avatar', function(user, size) {
+
   var avatarUrl = user.get("avatarTemplate").replace('{size}', size);
+  if (size !== "thumb") {
+    avatarUrl = avatarUrl.replace(/\.[a-z]+\?/, '.jpg?');
+  }
+
   return new Handlebars.SafeString('<img class="responsive-image" src="' + avatarUrl + '" alt="' + user.get("username") + '">')
+
 }, 'avatarTemplate', 'username');
