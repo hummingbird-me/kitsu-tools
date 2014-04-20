290561b85575bf9c5b3ac94779ea6202fc1c9978
Josh Fabian <contact@joshfabian.com>
2014-04-19 22:31:52 -0500

    embedded images are a bit too large. Decrease max height.

--- lib/message_formatter.rb
+++ lib/message_formatter.rb
@@ -28,7 +28,7 @@ class MessageFormatter
         begin
           Timeout::timeout(5) do
             if open(link).size <= 1024*1024*2
-              formatted += "<br><a href='#{link}'><img class='autoembed' src='#{link}' style='max-height: 800px; width: auto; max-width: 600' /></a>"
+              formatted += "<br><a href='#{link}'><img class='autoembed' src='#{link}' style='max-height: 500px; width: auto;' /></a>"
             end
           end
         rescue
