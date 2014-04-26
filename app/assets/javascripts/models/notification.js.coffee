2ed335b61a5cc9a37348755be789bfa2866637bd
CybroX <marc@cybrox.eu>
2014-04-27 00:20:53 +0200

    Fixed notification model attribute type, added id's

--- app/assets/javascripts/models/notification.js.coffee
+++ app/assets/javascripts/models/notification.js.coffee
@@ -2,5 +2,5 @@ Hummingbird.Notification = DS.Model.extend
   source_type: DS.attr('string')
   source_user: DS.attr('string')
   created_at: DS.attr('date')
-  notification_type: DS.attr('profile_comment')
+  notification_type: DS.attr('string')
   seen: DS.attr('boolean')
--- app/serializers/notification_serializer.rb
+++ app/serializers/notification_serializer.rb
@@ -1,6 +1,6 @@
 class NotificationSerializer < ActiveModel::Serializer
 
-  attributes :source_type, :source_user, :created_at, :notification_type, :seen
+  attributes :id, :source_type, :source_user, :created_at, :notification_type, :seen
 
   def source_user 
     object.source.target.name
