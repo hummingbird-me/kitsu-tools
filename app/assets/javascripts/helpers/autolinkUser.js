3708216b0786bea12ba9e767d0bbb566261650db
root <pchen@4-south.com>
2014-05-04 11:13:37 -0500

    fix for website links

--- app/assets/javascripts/helpers/autolinkUser.js
+++ app/assets/javascripts/helpers/autolinkUser.js
@@ -1,8 +1,23 @@
+var checkUserString = function(userUrl){
+  if( userUrl.indexOf("<") != -1 ||
+       userUrl.indexOf("script") != -1 ||
+       userUrl.indexOf("%") != -1 ||
+       userUrl.indexOf(">") != -1 ||
+       userUrl.indexOf("&") != -1){
+  return "Invalid Website"
+  } else return userUrl;  
+  
+}
+;
+
+
+
 Ember.Handlebars.helper('autolink', function(userUrl) {
   var url = "";
-  if( userUrl)
-    if( userUrl.length)
-      url =  Autolinker.link(userUrl);
-
+  if(userUrl)
+    if(userUrl.length){
+      url = checkUserString(userUrl); 
+      url =  Autolinker.link(url);
+   }
   return new Handlebars.SafeString(url);
 });
