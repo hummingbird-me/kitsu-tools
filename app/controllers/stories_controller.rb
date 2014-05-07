0bcb00010cf0d6e7c85a761d955afddb6f27e0ad
root <pchen@4-south.com>
2014-05-07 15:26:16 -0500

    fixed a n+1 query

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,7 +1,7 @@
 class StoriesController < ApplicationController
   def index
     if params[:user_id]
-      stories = User.find(params[:user_id]).stories.for_user(current_user).order('updated_at DESC').includes(:substories, :user, :target).page(params[:page]).per(20)
+      stories = User.find(params[:user_id]).stories.for_user(current_user).order('updated_at DESC').includes({substories: :user}, :user, :target).page(params[:page]).per(20)
 
       render json: stories, meta: {cursor: 1 + (params[:page] || 1).to_i}
     end
