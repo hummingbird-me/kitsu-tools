5cf28b7dc36884528fc3a24ff903e6431df106ee
Iwakura Taro <taro@mail333.com>
2014-05-11 13:28:13 +0300

    Splitting query in order to fetch more related data in batch.

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,7 +1,10 @@
 class StoriesController < ApplicationController
   def index
     if params[:user_id]
-      stories = User.find(params[:user_id]).stories.for_user(current_user).order('updated_at DESC').includes({substories: :user}, :user, :target).page(params[:page]).per(20)
+      story_ids = User.find(params[:user_id]).stories.for_user(current_user).order('stories.updated_at DESC').page(params[:page]).per(20).pluck(:id)
+      stories = Story.where(id: story_ids).where('target_type <> ?', 'Anime').includes(:user, :target, substories: :user)
+      stories += Story.where(id: story_ids).where(target_type: 'Anime').includes(:user, target: :genres, substories: :user)
+      stories = stories.sort_by(&:updated_at).reverse
 
       render json: stories, meta: {cursor: 1 + (params[:page] || 1).to_i}
     end
