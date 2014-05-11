4db3e44b37de8041b24e643cb4bba485d7c7b62c
Iwakura Taro <taro@mail333.com>
2014-05-11 21:46:22 +0300

    Loading franchises with related data.

--- app/controllers/franchises_controller.rb
+++ app/controllers/franchises_controller.rb
@@ -1,6 +1,6 @@
 class FranchisesController < ApplicationController
   def index
-    franchises = Franchise.where(id: params[:ids])
+    franchises = Franchise.where(id: params[:ids]).includes(anime: :genres)
     render json: franchises
   end
 
