f8a821c21c1fb5047ea6a6bd97c12f17abcdbb1e
Vikhyat Korrapati <vikhyatk@gmail.com>
2014-05-23 11:59:17 +0530

    Add secret_token.rb.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hummingbird::Application.config.secret_token = ENV['SECRET_TOKEN']
+Hummingbird::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
