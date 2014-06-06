64caa92f50a70fe2938cd6f7c64ef636d4d65577
Vikhyat Korrapati <vikhyatk@gmail.com>
2014-06-06 22:12:06 +0530

    Fix missing image.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -13,6 +13,8 @@ Hummingbird::Application.configure do
   config.serve_static_assets = true
   config.static_cache_control = "public, max-age=3600"
 
+  config.assets.image_optim = false
+
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
