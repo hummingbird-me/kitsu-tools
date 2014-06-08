c0815ccd12d80a3316fd24a4b85a863fb1d22e31
Vikhyat Korrapati <vikhyatk@gmail.com>
2014-06-08 17:23:45 +0530

    Simplify unicorn configuration.

--- config/unicorn.conf.rb
+++ config/unicorn.conf.rb
@@ -1,27 +1,19 @@
-worker_processes ENV["UNICORN_PROCESSES"].to_i
+hummingbird_path = File.expand_path(File.expand_path(File.dirname(__FILE__)) + "/../")
 
-if ENV["RAILS_ENV"] == "production"
-  working_directory "/u/apps/hummingbird/current"
-  listen "/tmp/unicorn.sock", backlog: 1024
-  pid "/u/apps/hummingbird/shared/pids/unicorn.pid"
-  stderr_path "/u/apps/hummingbird/shared/log/unicorn.stderr.log"
-  stdout_path "/u/apps/hummingbird/shared/log/unicorn.stdout.log"
+worker_processes ENV["UNICORN_WORKERS"].to_i
+working_directory hummingbird_path
+listen 3000
 
-  # Force the bundler gemfile environment variable to reference the Capistrano
-  # "current" symlink.
-  before_exec do |server|
-    ENV["BUNDLE_GEMFILE"] ="/u/apps/hummingbird/current/Gemfile"
-  end
-else
+# Nuke workers after 30 seconds.
+timeout 30
 
-  listen 3000, tcp_nopush: true
-end
-
-
+# Reduces memory usage in Ruby 2.0+.
 preload_app true
 
-# Nuke workers after 60 seconds.
-timeout 60
+pid "#{hummingbird_path}/tmp/pids/unicorn.pid"
+
+stderr_path "#{hummingbird_path}/log/unicorn.stderr.log"
+stdout_path "#{hummingbird_path}/log/unicorn.stdout.log"
 
 # Enable this flag to have unicorn test client connections by writing the
 # beginning of the HTTP headers before calling the application.  This
@@ -34,39 +26,17 @@ check_client_connection false
 before_fork do |server, worker|
   # the following is highly recomended for Rails + "preload_app true"
   # as there's no need for the master process to hold a connection
-  if defined?(ActiveRecord::Base)
-    ActiveRecord::Base.connection.disconnect!
-  end
-
+  ActiveRecord::Base.connection.disconnect!
   $redis.quit
 
-  # Zero downtime deploys.
-  #
-  old_pid = "#{server.config[:pid]}.oldbin"
-  if File.exists?(old_pid) and server.pid != old_pid
-    begin
-      Process.kill("QUIT", File.read(old_pid).to_i)
-    rescue Errno::ENOENT, Errno::ESRCH
-      # Someone else did our job for us.
-    end
-  end
+  # Throttle master from forking too quickly. Partially prevents identical
+  # repeated signals from being lost when the receiving process is busy.
+  sleep 1
 end
 
 after_fork do |server, worker|
-  # per-process listener ports for debugging/admin/migrations
-  # addr = "127.0.0.1:#{9293 + worker.nr}"
-  # server.listen(addr, :tries => -1, :delay => 5, :tcp_nopush => true)
-
   # the following is *required* for Rails + "preload_app true",
-  if defined?(ActiveRecord::Base)
-    ActiveRecord::Base.establish_connection
-  end
-
-  # if preload_app is true, then you may also want to check and
-  # restart any other shared sockets/descriptors such as Memcached,
-  # and Redis.  TokyoCabinet file handles are safe to reuse
-  # between any number of forked children (assuming your kernel
-  # correctly implements pread()/pwrite() system calls)
+  ActiveRecord::Base.establish_connection
   $redis = Redis.connect(host: ENV['REDIS_HOST'])
 end
 
