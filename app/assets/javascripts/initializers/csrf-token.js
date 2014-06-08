d3f9b769dbb0260a32b77b5aabfda796d39e3a1a
Vikhyat Korrapati <vikhyatk@gmail.com>
2014-06-08 22:41:52 +0530

    Use X-CSRF-Token header.

--- app/assets/javascripts/initializers/csrf-token.js
+++ app/assets/javascripts/initializers/csrf-token.js
@@ -2,12 +2,9 @@ Ember.Application.initializer({
   name: 'csrf-token',
   initialize: function () {
     var csrfToken = $('meta[name="csrf-token"]').attr('content');
-    var csrfParam = $('meta[name="csrf-param"]').attr('content');
     $.ajaxPrefilter(function(options, originalOptions, jqXHR) {
-      if (!options.crossDomain && csrfToken && csrfParam) {
-        var newData = {};
-        newData[csrfParam] = csrfToken;
-        options.data = $.param($.extend(originalOptions.data, newData));
+      if (!options.crossDomain && csrfToken) {
+        return jqXHR.setRequestHeader('X-CSRF-Token', csrfToken);
       }
     });
   }
