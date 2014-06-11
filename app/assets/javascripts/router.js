50c035dd2da7bc244d53851fd4b130d052e4a135
Sven Gehring <marc@cybrox.eu>
2014-06-11 22:15:27 +0200

    Converted main coffee assets to javascript

---
+++ app/assets/javascripts/router.js
@@ -0,0 +1,26 @@
+Hummingbird.Router.reopen({
+  location: 'history'
+});
+
+Hummingbird.Router.map(function() {
+  this.resource('anime', {path: '/anime/:id'}, function() {
+    this.resource('reviews', {path: '/reviews'}, function() {
+      this.route('show', {path: '/:review_id'});
+    });
+  });
+
+  this.resource('manga', {path: '/manga/:id'}, function() {
+    this.route('reviews');
+  });
+
+  this.resource('user', {path: '/users/:id'}, function() {
+    this.route('library');
+    this.route('reviews');
+    this.route('following');
+    this.route('followers');
+  });
+  
+  this.route('dashboard');
+  this.route('notifications');
+  this.route('sign-in');
+});
---
+++ app/assets/javascripts/store.js
@@ -0,0 +1,9 @@
+# http://emberjs.com/guides/models/defining-a-store/
+
+Hummingbird.Store = DS.Store.extend({
+  revision: 13,
+  adapter: '_ams'
+});
+
+Hummingbird.ApplicationAdapter = DS.ActiveModelAdapter.extend({});
+Hummingbird.ApplicationSerializer = DS.ActiveModelSerializer.extend({});
