24580032a9be213bf367c0e13907141bff07e19d
Vikhyat Korrapati <vikhyatk@gmail.com>
2014-06-13 01:37:33 +0530

    Indentation fix.

--- app/serializers/user_info_serializer.rb
+++ app/serializers/user_info_serializer.rb
@@ -2,10 +2,10 @@ class UserInfoSerializer < ActiveModel::Serializer
   attributes :id, :life_spent_on_anime, :anime_watched, :top_genres
 
   def anime_watched
-  	object.watchlists.all.count
+    object.watchlists.all.count
   end
 
   def top_genres
-  	object.top_genres
+    object.top_genres
   end
 end
