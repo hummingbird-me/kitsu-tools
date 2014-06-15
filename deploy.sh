ba463d0fa8ad0c24c9c30fdb3d87c491bf190d5f
Vikhyat Korrapati <vikhyatk@gmail.com>
2014-06-15 19:52:53 +0530

    Fast deploy: first attempt.

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# This script will update the git repository to the latest revision in the
+# origin's master branch. Then it restarts sidekiq using monit and does a zero
+# downtime restart of the unicorn process.
+#
+# It is intended to be run by Travis after a successful build.
+
+cd "$(dirname "$0")"
+
+# Update the git repo.
+git fetch
+git checkout master
+git reset --hard HEAD@{upstream}
+
+# bundle install, precompile assets, migrate database
+bundle install --deployment --without test
+bundle exec rake assets:precompile
+bundle exec rake db:migrate
+
+# restart sidekiq
+monit restart sidekiq
+
+# zero-downtime unicorn restart
+kill -USR2 `cat tmp/pids/unicorn.pid`
