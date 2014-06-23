e96a86bd00c479bda551d6f7a329ca08d12c1599
Peter Lejeck <peter.lejeck@gmail.com>
2014-06-22 23:50:28 -0700

    Fix character and staff methods for MALImport when media=manga

--- lib/mal_import.rb
+++ lib/mal_import.rb
@@ -7,45 +7,68 @@ class MALImport
     @main_noko = Nokogiri::HTML open("http://myanimelist.net/#{media.to_s}/#{id}", 'User-Agent' => 'iMAL-iOS').read
     char_page = Nokogiri::HTML open("http://myanimelist.net/#{media.to_s}/#{id}/a/characters", 'User-Agent' => 'iMAL-iOS').read
     @char_noko = char_page.css('h2:contains("Characters")')[0].parent
+    @sidebar = @main_noko.css('td.borderClass')[0]
   end
   def staff
-    @char_noko.css('a[name="staff"] + h2 + table tr td:nth-child(2)').map do |sm|
-      {
-        external_id: sm.css('a')[0]['href'].scan(/people\/(\d+)\//).flatten[0].to_i,
-        name: sm.css('a').text.split(',').map(&:strip).reverse.join(' '),
-        role: sm.css('small').text
-      }
+    case @media
+    when :anime
+      @char_noko.css('a[name="staff"] + h2 + table tr td:nth-child(2)').map do |sm|
+        {
+          external_id: sm.css('a')[0]['href'].scan(/people\/(\d+)\//).flatten[0].to_i,
+          name: nameflip(sm.css('a').text),
+          role: sm.css('small').text
+        }
+      end
+    when :manga
+      @sidebar.css('span:contains("Authors:") ~ a').map do |author|
+        {
+          external_id: author['href'].scan(/people\/(\d+)\//).flatten[0].to_i,
+          name: nameflip(author.text.strip),
+          role: author.next.text.scan(/\(([a-zA-Z ]+)\)/).flatten[0].strip
+        }
+      end
     end
   end
   def characters
     featured_chars = @main_noko.css('table div.picSurround a[href*="character/"]').map {|x| x['href'].scan(/character\/(\d+)/) }.flatten.map(&:to_i)
 
-    @char_noko.css('h2:contains("Characters") ~ *').take_while { |x|
-      x.name == 'table'
-    }.map do |chara|
-      external_id = chara.css('td:nth-child(2) > a')[0]['href'].scan(/character\/(\d+)\//).flatten[0].to_i
-      {
-        external_id: external_id,
-        name: chara.css('td:nth-child(2) > a').text.split(',').map(&:strip).reverse.join(' '),
-        role: chara.css('td:nth-child(2) small').text,
-        featured: featured_chars.include?(external_id),
-        voice_actors: chara.css('td:nth-child(3) tr > td:nth-child(1)').map do |va|
-          {
-            external_id: va.css('a')[0]['href'].scan(/people\/(\d+)\//).flatten[0].to_i,
-            name: va.css('a').text.split(',').map(&:strip).reverse.join(' '),
-            lang: va.css('small').text
-          } if va.children.length > 0
-        end.compact
-      }
+    case @media
+    when :anime
+      @char_noko.css('h2:contains("Characters") ~ *').take_while { |x|
+        x.name == 'table'
+      }.map do |chara|
+        external_id = chara.css('td:nth-child(2) > a')[0]['href'].scan(/character\/(\d+)\//).flatten[0].to_i
+        {
+          external_id: external_id,
+          name: nameflip(chara.css('td:nth-child(2) > a').text),
+          role: chara.css('td:nth-child(2) small').text,
+          featured: featured_chars.include?(external_id),
+          voice_actors: chara.css('td:nth-child(3) tr > td:nth-child(1)').map do |va|
+            {
+              external_id: va.css('a')[0]['href'].scan(/people\/(\d+)\//).flatten[0].to_i,
+              name: nameflip(va.css('a').text),
+              lang: va.css('small').text
+            } if va.children.length > 0
+          end.compact
+        }
+      end
+    when :manga
+      @char_noko.css('h2:contains("Characters") ~ table').map do |chara|
+        external_id = chara.css('td:nth-child(2) > a')[0]['href'].scan(/character\/(\d+)\//).flatten[0].to_i
+        {
+          external_id: external_id,
+          name: nameflip(chara.css('td:nth-child(2) > a').text),
+          role: chara.css('td:nth-child(2) small').text,
+          featured: featured_chars.include?(external_id)
+        }
+      end
     end
   end
   def metadata
-    sidebar = @main_noko.css('td.borderClass')[0]
-
     meta = {
       external_id: @mal_id,
-      title: @main_noko.css('h1').children[1].text,
-      english_title: begin sidebar.css('div:contains("English:")')[0].text.gsub("English: ", "") rescue nil end,
+      title: @main_noko.css('h1').children[1].text.strip,
+      english_title: begin @sidebar.css('div:contains("English:")')[0].text.gsub("English: ", "") rescue nil end,
       synopsis: begin
         synopsis = @main_noko.css('td td:contains("Synopsis")')[0].text.gsub("Synopsis", '')
         if synopsis.include? "No synopsis has been added"
@@ -55,29 +78,28 @@ class MALImport
         end
       rescue
       end,
-      poster_image: URI(sidebar.css("img")[0]['src']),
-      genres: begin (sidebar.css('span:contains("Genres:") ~ a').map(&:text) rescue []).compact end,
-      type: begin allowed_types.grep(sidebar.css('div:contains("Type:")')[0].text.gsub("Type:", '').strip)[0] rescue nil end,
-      status: begin sidebar.css('div:contains("Status:")')[0].children[1].text.strip.gsub(/\w+/){ |w| w.capitalize } rescue nil end
+      poster_image: URI(@sidebar.css("img")[0]['src']),
+      genres: begin (@sidebar.css('span:contains("Genres:") ~ a').map(&:text) rescue []).compact end,
+      type: begin allowed_types.grep(@sidebar.css('div:contains("Type:")')[0].text.gsub("Type:", '').strip)[0] rescue nil end,
+      status: begin @sidebar.css('div:contains("Status:")')[0].children[1].text.strip.gsub(/\w+/){ |w| w.capitalize } rescue nil end
     }
 
     # Media-specific data
     case @media
     when :manga
       meta.merge!({
-        dates: begin sidebar.css('div:contains("Published:")')[0].text.gsub("Published:", '').split("to").map { |s| parse_maldate(s) } rescue nil end,
-        authors: begin (sidebar.css('span:contains("Authors:") ~ a').map(&:text) rescue []).compact end,
-        volumes: begin sidebar.css('div:contains("Volumes:")')[0].text.gsub("Volumes: ", "") rescue nil end,
-        chapters: begin sidebar.css('div:contains("Volumes:")')[0].text.gsub("Volumes: ", "") rescue nil end,
-        serialization: begin sidebar.css('div:contains("Serialization:") ~ a')[0].text.gsub("Serialization: ", "") rescue nil end,
+        dates: begin @sidebar.css('div:contains("Published:")')[0].text.gsub("Published:", '').split("to").map { |s| parse_maldate(s) } rescue nil end,
+        volumes: begin @sidebar.css('div:contains("Volumes:")')[0].text.gsub("Volumes: ", "").strip.to_i rescue nil end,
+        chapters: begin @sidebar.css('div:contains("Chapters:")')[0].text.gsub("Chapters: ", "").strip.to_i rescue nil end,
+        serialization: begin @sidebar.css('span:contains("Serialization:") ~ a').map(&:text)[0] rescue nil end,
       })
     when :anime
       meta.merge!({
-        dates: begin sidebar.css('div:contains("Aired:")')[0].text.gsub("Aired:", '').split("to").map { |s| parse_maldate(s) } rescue nil end,
-        producers: begin (sidebar.css('span:contains("Producers:") ~ a').map(&:text) rescue []).compact end,
-        age_rating: begin sidebar.css('div:contains("Rating:")')[0].children[1].text.strip rescue nil end,
-        episode_count: begin sidebar.css('div:contains("Episodes:")')[0].children[1].text.strip.to_i rescue nil end,
-        episode_length: parse_duration(begin sidebar.css('div:contains("Duration:")')[0].children[1].text.strip rescue nil end),
+        dates: begin @sidebar.css('div:contains("Aired:")')[0].text.gsub("Aired:", '').split("to").map { |s| parse_maldate(s) } rescue nil end,
+        producers: begin (@sidebar.css('span:contains("Producers:") ~ a').map(&:text) rescue []).compact end,
+        age_rating: begin @sidebar.css('div:contains("Rating:")')[0].children[1].text.strip rescue nil end,
+        episode_count: begin @sidebar.css('div:contains("Episodes:")')[0].children[1].text.strip.to_i rescue nil end,
+        episode_length: parse_duration(begin @sidebar.css('div:contains("Duration:")')[0].children[1].text.strip rescue nil end),
       })
     end
 
@@ -93,6 +115,9 @@ class MALImport
   end
 
   private
+  def nameflip(name)
+    name.split(',').map(&:strip).reverse.join(' ')
+  end
   def allowed_types
     case @media
     when :anime
