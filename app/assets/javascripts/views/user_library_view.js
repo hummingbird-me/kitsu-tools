e6a35802d691787c4f762812bfe8ab4ab273c806
Johannes Thiel <Johannes.Thiel@uni-duesseldorf.de>
2014-06-25 15:45:58 +0200

    Convert views/user_library_view.js.coffee to JS

---
+++ app/assets/javascripts/views/user_library_view.js
@@ -0,0 +1,18 @@
+Hummingbird.UserLibraryView = Ember.View.extend({
+  user: Ember.computed.alias('controller.user'),
+  sections: Ember.computed.alias('controller.sections'),
+  reactComponent: Ember.computed.alias('controller.reactComponent'),
+
+  didInsertElement: function() {
+    this.set('reactComponent', LibrarySectionsReactComponent({
+      content: this.get('sections'),
+      view: this
+    }));
+    return React.renderComponent(this.get('reactComponent'), this.get('element').querySelector('#library-sections'));
+  },
+
+  willClearRender: function() {
+    React.unmountComponentAtNode(this.get('element').querySelector('#library-sections'));
+    return this.set('reactComponent', null);
+  }
+});
