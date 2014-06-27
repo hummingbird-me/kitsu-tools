841e146d7b09484a742a788c2b3299537af2252c
Sven Gehring <marc@cybrox.eu>
2014-06-27 11:14:50 +0200

    Converted Quick Update component to js

---
+++ app/assets/javascripts/components/header-quick-update.js
@@ -0,0 +1,55 @@
+Hummingbird.HeaderQuickUpdateComponent = Ember.Component.extend({
+  tagNames: "li",
+  classNames: "watched-series",
+  classNameBindings: ["active"],
+
+
+
+  active: function() {
+    var firstEntry = this.get('firstEntry');
+    return firstEntry === this.get('item.id');
+  }.property('firstEntry'),
+
+  imageUrl: function() {
+    var item = this.get('item');
+    if (item) return item.get('anime.posterImage');
+  }.property('item.anime'),
+
+  episodesWatchedStats: function() {
+    var epsWatched = this.get('item.episodesWatched'),
+        eps = this.get('item.anime.episodeCount');
+    return (eps) ? Math.ceil((epsWatched / eps) * 100) : "--";
+  }.property('item.anime', 'item'),
+
+  episodeCount: function() {
+    var count = this.get('item.anime.episodeCount');
+    return (count) ? count : "?";
+  }.property('item.anime'),
+
+  incrementLabel: function() {
+    var epsWatched = this.get('item.episodesWatched'),
+        eps = this.get('item.anime.episodeCount'),
+        completed = this.get('seriesIsCompleted');
+    return (!completed) ? (epsWatched + 1) : "";
+  }.property('item.anime', 'item'),
+
+  seriesIsCompleted: function() {
+    var epsWatched = this.get('item.episodesWatched'),
+        eps = this.get('item.anime.episodeCount');
+    return (eps === epsWatched);
+  }.property('item.anime', 'item'),
+
+
+  actions: {
+    incCnt: function() {
+      var item = this.get('item'),
+          epsWatched = item.get('episodesWatched'),
+          eps = this.get('item.anime.episodeCount');
+          
+      if (epsWatched < eps || !eps) {
+        item.set('episodesWatched', epsWatched + 1);
+        return item.save();
+      }
+    }
+  }
+});
