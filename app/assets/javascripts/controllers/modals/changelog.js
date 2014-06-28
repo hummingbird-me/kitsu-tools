308fa2572fac95806db6ff9232127d6a0b5be34d
Vikhyat Korrapati <vikhyatk@gmail.com>
2014-06-28 23:12:53 +0530

    Get rid of changelog from the avatar dropdown.

--- app/assets/javascripts/controllers/modals/changelog.js
+++
@@ -1,8 +0,0 @@
-Hummingbird.ModalsChangelogController = Ember.ObjectController.extend(Hummingbird.ModalControllerMixin, {
-  isLoading: true,
-  commits: function () {
-    return this.store.find('changelog', {
-      order: 'date'
-    });
-  }.property('@each.changelog')
-});
