7065ec5839bf56e107cd9731ba9d784c7c95e3a0
Vikhyat Korrapati <vikhyatk@gmail.com>
2014-06-28 14:46:00 +0530

    Missed a couple of default values.

--- db/migrate/20140628075917_not_null_columns.rb
+++ db/migrate/20140628075917_not_null_columns.rb
@@ -1,5 +1,5 @@
 class NotNullColumns < ActiveRecord::Migration
-  def change
+  def up
     change_column :anime, :bayesian_average, :float, null: false, default: 0
     change_column :anime, :user_count, :float, null: false, default: 0
     change_column :anime, :cover_image_top_offset, :integer, null: false, default: 0
@@ -7,12 +7,15 @@ class NotNullColumns < ActiveRecord::Migration
 
     change_column :manga_library_entries, :user_id, :integer, null: false
     change_column :manga_library_entries, :manga_id, :integer, null: false
-    change_column :manga_library_entries, :chapters_read, :integer, null: false
-    change_column :manga_library_entries, :volumes_read, :integer, null: false
-    change_column :manga_library_entries, :reread_count, :integer, null: false
+    change_column :manga_library_entries, :chapters_read, :integer, null: false, default: 0
+    change_column :manga_library_entries, :volumes_read, :integer, null: false, default: 0
+    change_column :manga_library_entries, :reread_count, :integer, null: false, default: 0
     change_column :manga_library_entries, :status, :string, null: false
     change_column :manga_library_entries, :private, :boolean, null: false, default: false
     change_column :manga_library_entries, :rereading, :boolean, null: false, default: false
 
+  end
+
+  def down
   end
 end
