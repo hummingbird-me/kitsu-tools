f76456688172700741fffdd0444bec1e38e6f969
Vikhyat Korrapati <vikhyatk@gmail.com>
2014-07-06 19:23:04 +0530

    Refactor MessageFormatter.

--- lib/message_formatter.rb
+++ lib/message_formatter.rb
@@ -1,50 +1,81 @@
 require 'timeout'
 
 class MessageFormatter
-  def self.format_message(message)
-    formatted = Rinku.auto_link(ERB::Util.html_escape(message), :all, 'target="_blank"')
-    ytlink = ""
-    response = {}
-    # Replace newlines with <br>.
-    formatted = formatted.strip.gsub(/\n{3,}/, "\n\n")
-    formatted = formatted.gsub(/\n/, "<br>")
+  def initialize(message)
+    @message = message
+    @processed = message
 
-    # Link @usernames.
-    formatted = formatted.gsub(/@[-_A-Za-z0-9]+/) do |x|
-      u = User.find_by_name(x[1..-1])
-      if u
-        "<span class='name'>@<a href='#{Rails.application.routes.url_helpers.user_path(u)}' target='_blank' data-user-name='#{u.name}'>#{u.name}</a></span>"
+    # Apply filters.
+    autolink
+    link_usernames
+    process_newlines
+    embed_image_or_youtube
+  end
+
+  def format
+    @processed
+  end
+
+  def self.format_message(message)
+    MessageFormatter.new(message).format
+  end
+
+  private
+
+  def autolink
+    @processed = Rinku.auto_link(ERB::Util.html_escape(@processed), :all, 'target="_blank"')
+  end
+
+  def link_usernames
+    @processed.gsub!(/@[_A-Za-z0-9]+/) do |x|
+      if user = User.find_by(name: x[1..-1])
+        "<span class='name'>@<a href='//hummingbird.me/users/#{user.name}' target='_blank' data-user-name='#{user.name}'>#{user.name}</a></span>"
       else
         x
       end
     end
+  end
 
-    noko = Nokogiri::HTML.parse formatted
+  def process_newlines
+    @processed.gsub!(/\n{3,}/, "\n\n")
+    @processed.gsub!(/\n/, "<br>")
+  end
+
+  def embed_image_or_youtube
+    noko = Nokogiri::HTML.fragment(@processed)
     links = noko.css('a').map {|link| link['href'] }
-    if links.length > 0
-      link = links[-1]
 
-      # Embed images.
-      if link =~ /\.(gif|jpe?g|png)$/i
-        begin
-          Timeout::timeout(5) do
-            if open(link).size <= 1024*1024*2
-              formatted += "<br><a href='#{link}'><img class='autoembed' src='#{link}' style='max-height: 500px; width: auto;' /></a>"
-            end
-          end
-        rescue
-        end
-      end
+    # Only do autoembedding if there is exactly one link.
+    return if links.length != 1
+    link = links.first
 
-      # Embed YouTube videos.
-      if link =~ /(?:http:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)\/(?:watch\?v=)?(.+)/
-        begin
-          code = link.scan(/v=([A-Za-z0-9\-_]+)/)[0][0]
-          formatted += "<div class='video-embed'><div class='video-wrapper'><iframe width='350' height='240' frameborder='0' class='autoembed' allowfullscreen src='http://youtube.com/embed/#{code}'> </iframe></div></div>"
-        rescue
-        end
-      end
+    if embeddable_image?(link)
+      delete_links
+      @processed += "<br><a href='#{link}'><img class='autoembed' src='#{link}' style='max-height: 500px; width: auto;' /></a>"
+    elsif code = embeddable_video_code(link)
+      delete_links
+      @processed += "<div class='video-embed'><div class='video-wrapper'><iframe width='350' height='240' frameborder='0' class='autoembed' allowfullscreen src='http://youtube.com/embed/#{code}'> </iframe></div></div>"
     end
-    formatted
+  end
+
+  def embeddable_image?(link)
+    link =~ /\.(gif|jpe?g|png)$/i && filesize(link) <= 1024 * 1024 * 2
+  end
+
+  def filesize(link)
+    Timeout::timeout(5) { return open(link).size }
+  rescue
+    return 1.0 / 0
+  end
+
+  def embeddable_video_code(link)
+    return unless link =~ /(?:http:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)\/(?:watch\?v=)?(.+)/
+    link.scan(/v=([A-Za-z0-9\-_]+)/)[0][0]
+  rescue
+    nil
+  end
+
+  def delete_links
+    @processed = Nokogiri::HTML.fragment(@processed).tap {|x| x.css('a').remove }.to_s
   end
 end
---
+++ test/lib/message_formatter_test.rb
@@ -0,0 +1,45 @@
+require 'test_helper'
+require 'fakeweb_helper'
+
+fake({
+  [:get, "http://i.imgur.com/CUjJQap.gif"] => "small_image",
+  [:get, "https://i.minus.com/iE02xvicOlrbF.gif"] => "large_image"
+})
+
+class MessageFormatterTest < ActiveSupport::TestCase
+  def format(message)
+    MessageFormatter.format_message(message)
+  end
+
+  test "links to URLs" do
+    assert_match /href=/, format("http://vikhyat.net/")
+  end
+
+  test "replaces newlines with <br>" do
+    assert_match /a<br\s?\/?>b/, format("a\nb")
+    assert_match /a(<br\s?\/?>){2}b/, format("a\n\n\nb") # max two <br>s
+  end
+
+  test "embeds small images" do
+    assert_match /img/, format("http://i.imgur.com/CUjJQap.gif")
+  end
+
+  test "does not embed large images" do
+    assert_no_match /img/, format("https://i.minus.com/iE02xvicOlrbF.gif")
+  end
+
+  test "embeds youtube videos" do
+    assert_match /iframe/, format("https://www.youtube.com/watch?v=v8YEw6JaM3c")
+  end
+
+  test "removes original embedded link URL" do
+    fmt = format("message http://i.imgur.com/CUjJQap.gif")
+    noko = Nokogiri::HTML.parse fmt
+    assert_match /message/, fmt
+    assert_equal 1, noko.css('a').length
+  end
+
+  test "links usernames" do
+    assert_match /href=/, format("@vikhyat")
+  end
+end
