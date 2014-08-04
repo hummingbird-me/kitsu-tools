5a4b64721c6848b0254c3c4679d8fd5760d57bad
Sven Gehring <svengehring@cybrox.eu>
2014-08-04 20:39:16 +0200

    Cleaned up search page parts

--- app/assets/javascripts/controllers/search_controller.js
+++ app/assets/javascripts/controllers/search_controller.js
@@ -1,18 +1,11 @@
 Hummingbird.SearchController = Ember.Controller.extend({
-  queryParams: ['query', 'filter'],
-  searchResults: [],
-  filter: null,
-  query: "",
   filters: ["Everything", "Anime", "Manga", "User"],
-
-  init: function () {
-    this.set('didSearch', false);
-    this._super();
-    console.log(this.get('query'));
-    if(!Ember.empty(this.get('query'))){
-      this.performSearch();
-    }
-  },
+  queryParams: ['query', 'filter'],
+  performedSearch: false,
+  userTypedSearch: false,
+  searchResults: [],
+  filter: "Everything",
+  query: "",
 
   filteredSearchResults: function(){
     var self = this;
@@ -20,6 +13,26 @@ Hummingbird.SearchController = Ember.Controller.extend({
       return (result.type == self.get('filter').toLowerCase() || "Everything" == self.get('filter'));
     });
   }.property('searchResults', 'filter'),
+
+
+
+  // Devnote: For some reason, the Ember query parameter is not accesible
+  // during init or after init._super(); This hack will trigger at the first
+  // query change unless the seach term is <2 chars long. (Ember beta build)
+  // On the first change, queryCounter actually still is set to zero.
+  timesQueryChanged: 0,
+
+  countQueryChanged: function (){
+    this.incrementProperty('timesQueryChanged')
+  }.observes('query'),
+
+  forcePerformSearch: function(){
+    if(!this.get('performedSearch') && !Ember.empty(this.get('query')))
+      if(this.get('query').length > 2 && this.get('timesQueryChanged') == 0)
+        this.performSearch();
+  }.observes('query'),
+
+
 
   performSearch: function(){
     var self = this;
@@ -29,7 +42,7 @@ Hummingbird.SearchController = Ember.Controller.extend({
     }).then(function(payload) {
       self.setProperties({
         'searchResults': payload.search,
-        'didSearch': true
+        'performedSearch': true
       });
     });
   },
