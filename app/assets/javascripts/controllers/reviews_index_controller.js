7af48d55e65bca622f8c17d7153fef5eadc8a34b
Vikhyat Korrapati <vikhyatk@gmail.com>
2014-08-14 20:16:19 +0530

    Fix review creation.

--- app/assets/javascripts/controllers/reviews_index_controller.js
+++ app/assets/javascripts/controllers/reviews_index_controller.js
@@ -2,5 +2,11 @@ Hummingbird.ReviewsIndexController = Ember.ArrayController.extend({
   needs: "anime",
   anime: Ember.computed.alias('controllers.anime'),
   sortProperties: ['wilsonScore'],
-  sortAscending: false
+  sortAscending: false,
+
+  // Legacy -- remove after Ember transition is complete.
+  newReviewURL: function() {
+    return "/anime/" + this.get('anime.id') + "/reviews/new";
+  }.property('anime.id')
+  // End Legacy
 });
