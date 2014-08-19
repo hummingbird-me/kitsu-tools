7f9bfd5c841425f06d9fd8bca7bf4d62d19409d0
Vikhyat Korrapati <vikhyatk@gmail.com>
2014-08-19 13:34:10 +0530

    Revert "Allow specifying number of substories returned by /stories."
    This reverts commit 3bb54e3f13462eadc79e37b6da4270aa065f1802.

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,6 +1,6 @@
 class StoriesController < ApplicationController
   def index
-    params.permit(:user_id, :news_feed, :page, :substory_limit)
+    params.permit(:user_id, :news_feed, :page)
 
     if params[:user_id]
       stories = User.find(params[:user_id]).stories.for_user(current_user).order('updated_at DESC').includes(:user, :target, substories: :user).page(params[:page]).per(30)
@@ -8,8 +8,7 @@ class StoriesController < ApplicationController
       stories = NewsFeed.new(current_user).fetch(params[:page] || 1)
     end
 
-    render json: stories, meta: {cursor: 1 + (params[:page] || 1).to_i},
-      substory_limit: (params[:substory_limit] || 2).to_i
+    render json: stories, meta: {cursor: 1 + (params[:page] || 1).to_i}
   end
 
   def destroy
