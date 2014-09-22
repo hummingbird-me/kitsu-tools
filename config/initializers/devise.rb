658d2a79e258f12b79c8c0135cd7971de47ebbea
Vikhyat Korrapati <vikhyatk@gmail.com>
2014-09-22 18:31:13 +0530

    Remove unused cookie_auth warden strategy.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -221,10 +221,9 @@ Devise.setup do |config|
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
   #
-  config.warden do |manager|
+  # config.warden do |manager|
   #   manager.intercept_401 = false
-    manager.default_strategies(:scope => :user).unshift :cookie_auth
-  end
+  # end
 
   # ==> Mountable engine configurations
   # When using Devise inside an engine, let's call it `MyEngine`, and this engine
