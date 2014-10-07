96f83223ae451e6bf5ef4df7d688f8b321fb831f
Sven Gehring <svengehring@cybrox.eu>
2014-10-08 01:04:24 +0200

    Added model placeholder for apps page

---
+++ app/assets/javascripts/routes/apps_route.js
@@ -0,0 +1,12 @@
+Hummingbird.AppsRouter = Ember.Route.extend({
+  
+  model: function(){
+
+    // The apps page only lists actively supported, community made apps.
+    // Since this list is mostly static, it will be hardcoded as a model.
+    return [
+    ]
+    
+  }
+
+});
