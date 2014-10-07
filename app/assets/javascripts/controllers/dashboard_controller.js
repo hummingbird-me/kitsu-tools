29b2473743fcc27356384617fe3bc720ab618fd8
Sven Gehring <svengehring@cybrox.eu>
2014-10-07 21:59:25 +0200

    Added tab filtering to dashboard

--- app/assets/javascripts/controllers/dashboard_controller.js
+++ app/assets/javascripts/controllers/dashboard_controller.js
@@ -1,1 +1,35 @@
-Hummingbird.DashboardController = Ember.Controller.extend(Hummingbird.HasCurrentUser);
+//Hummingbird.DashboardController = Ember.Controller.extend(Hummingbird.HasCurrentUser){
+Hummingbird.DashboardController = Ember.Controller.extend({
+
+  currentTab: "dashboard",
+
+  highlightDasboard: function() { return this.get('currentTab') == "dashboard" }.property('currentTab'),
+  highlightPosts: function() { return this.get('currentTab') == "posts" }.property('currentTab'),
+  highlightMedia: function() { return this.get('currentTab') == "media" }.property('currentTab'),
+
+  actualContent: function(){
+    var tab = this.get('currentTab');
+    if(tab == "posts") return this.get('postsOnly');
+    if(tab == "media") return this.get('mediaOnly');
+    return this.get('content');
+  }.property('currentTab', 'content'),
+
+  postsOnly: function(){
+    return this.get('content').filter(function(item, index, enumr){
+      console.log(item);
+      return (item._data.type == "comment" || item._data.type == "followed");
+    });
+  }.property('content'),
+
+  mediaOnly: function(){
+    return this.get('content').filterBy('type', 'media_story');
+  }.property('content'),
+
+
+  actions: {
+    setCurrentTab: function(newTab){
+      this.set('currentTab', newTab);
+    }
+  }
+
+});
