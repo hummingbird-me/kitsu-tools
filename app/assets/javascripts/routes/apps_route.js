47bd481a5c32933c9acfaae1c660625a0c83fcda
Sven Gehring <svengehring@cybrox.eu>
2014-10-08 02:13:22 +0200

    Added basic template and style

--- app/assets/javascripts/routes/apps_route.js
+++ app/assets/javascripts/routes/apps_route.js
@@ -1,12 +1,13 @@
-Hummingbird.AppsRouter = Ember.Route.extend({
+Hummingbird.AppsRoute = Ember.Route.extend({
   
   model: function(){
 
     // The apps page only lists actively supported, community made apps.
     // Since this list is mostly static, it will be hardcoded as a model.
     return [
+      "a", "b"
     ]
-    
+
   }
 
 });
