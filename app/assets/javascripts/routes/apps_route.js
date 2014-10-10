78baf5862b2796fb4c8dc12528cd9e253a86096c
Sven Gehring <svengehring@cybrox.eu>
2014-10-10 17:31:44 +0200

    Added basic cards for most popular apps

--- app/assets/javascripts/routes/apps_route.js
+++ app/assets/javascripts/routes/apps_route.js
@@ -6,18 +6,69 @@ Hummingbird.AppsRoute = Ember.Route.extend({
     // Since this list is mostly static, it will be hardcoded as a model.
     return [
       {
-        "name": "Random Kitten",
-        "type": "Android App",
-        "cover": "http://placekitten.com/g/300/150",
-        "desc": "This is a really cool application that has been around for quite a while. \
-              It does not actually do anything but it has a picture of a kitten in the  \
-              header so it has to be pretty awesome, right? Though, the picture resolution \
-              sucks, it's a pretty neat app. Also, its description is clearly a work of \
-              art, so you should definitely download it. (Run tehkitten.exe)",
+        "name": "Tenpenchii",
+        "type": "Android",
+        "cover": "",
+        "desc": "Tenpenchii is a Hummingbird client for Android devices with \
+              a modern user interface and amazing functionality. It has been \
+              around for over a year now and slowly emerged to the inofficial \
+              Hummingbird client for Android.",
         "links": [
-          { "name": "Website", "link": "#" },
-          { "name": "Source", "link": "#" },
-          { "name": "Thread", "link": "#" },
+          { "name": "Website", "link": "http://fractalemagic.com/" },
+          { "name": "Thread", "link": "https://forums.hummingbird.me/t/android-tenpenchii/582" },
+        ]
+      },
+      {
+        "name": "Taiga",
+        "type": "Windows",
+        "cover": "",
+        "desc": "Taiga is an open source, lightweight, anime tracker for Windows. \
+              It can automatically detect the episodes you're watching on your \
+              computer and synchronize your progress with Hummingbird. It helps \
+              you manage your list, discover new series, share watched episodes \
+              and download new ones.",
+        "links": [
+          { "name": "Website", "link": "http://taiga.erengy.com" },
+          { "name": "Source", "link": "http://taiga.erengy.com"},
+          { "name": "Thread", "link": "http://forums.hummingbird.me/t/windows-taiga/10565" },
+        ]
+      },
+      {
+        "name": "HAPU",
+        "type": "Mac",
+        "cover": "",
+        "desc": "HAPU is a menubar app that allows you to interact with Hummingbird \
+              and automatically update your anime progress, this also includes posting \
+              to your acticity feed, getting notifications on comment replies, serch for \
+              anime directly withing the app and share what you're watching directly via \
+              facebook or twitter.",
+        "links": [
+          { "name": "Website", "link": "http://mahdi.jp/apps/hapu" },
+          { "name": "Thread", "link": "https://forums.hummingbird.me/t/mac-hapu-scrobbler-app/56" },
+        ]
+      },
+      {
+        "name": "Anitro",
+        "type": "Windows /- Phone",
+        "cover": "",
+        "desc": "Anitro is an Anime management application for Windows Phone 8.1 and Windows \
+            8.1 which utilizes the Hummingbird service, and will allow you to View and Manage \
+            your entire profile.",
+        "links": [
+          { "name": "Windows Phone Store", "link": "http://www.windowsphone.com/en-us/store/app/anitro/6377b104-7df2-4d29-8ebb-88527728e673" },
+          { "name": "Windows Store", "link": "http://apps.microsoft.com/windows/en-us/app/anitro/665e06bf-b4de-4cde-8ae3-c20c1a98b12b" },
+          { "name": "Thread", "link": "https://forums.hummingbird.me/t/anitro-windows-8-1-windows-phone-8-1/3824" },
+        ]
+      },
+      {
+        "name": "Zenbu",
+        "type": "Cross Platform",
+        "cover": "",
+        "desc": "Zenbu is a cross-platform, multi functional, anime and manga desktop \
+              management application with exceptional features.",
+        "links": [
+          { "name": "Info and Source", "link": "https://bitbucket.org/Ippytraxx/zenbu" },
+          { "name": "Thread", "link": "https://forums.hummingbird.me/t/zenbu-development-log-new-installer-in-development/1583" },
         ]
       }
     ]
