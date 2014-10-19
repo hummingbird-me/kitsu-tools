204f86e48aab76e2eb14589ac94d9c409f7d19cb
Vikhyat Korrapati <vikhyatk@gmail.com>
2014-10-19 08:58:24 +0530

    Fix localjump error.

--- config/initializers/message_bus.rb
+++ config/initializers/message_bus.rb
@@ -7,6 +7,9 @@ MessageBus.redis_config = {
 
 MessageBus.user_id_lookup do |env|
   auth_token = Rack::Request.new(env).cookies["auth_token"]
-  return nil unless auth_token
-  User.find_by(authentication_token: auth_token).try(:id)
+  if auth_token
+    User.find_by(authentication_token: auth_token).try(:id)
+  else
+    nil
+  end
 end
