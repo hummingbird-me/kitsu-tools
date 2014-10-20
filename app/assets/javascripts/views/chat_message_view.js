5865db43a51149612fb92268a45392dd183a226f
Peter Lejeck <peter.lejeck@gmail.com>
2014-10-19 23:44:11 -0700

    Hook into load events for images and rescroll

--- app/assets/javascripts/views/chat_message_view.js
+++ app/assets/javascripts/views/chat_message_view.js
@@ -3,6 +3,20 @@ Hummingbird.ChatMessageView = Ember.View.extend({
     var self = this;
     if (this.get('content.formattedMessage')) {
       this.$('.spoiler').spoilerAlert();
+      this.$('img').load(function() {
+        self.get('parentView').send('rescroll');
+      });
     }
   },
+  // didInsertElement is post-DOM, messageObserver is pre-DOM
+  // So they're separated like this
+  didInsertElement: function() {
+    this.messageHooks();
+  },
+  messageObserver: function() {
+    var self = this;
+    Em.run.next(function() {
+      self.messageHooks();
+    });
+  }.observes('content.formattedMessage')
 });
--- app/assets/javascripts/views/chat_view.js
+++ app/assets/javascripts/views/chat_view.js
@@ -1,10 +1,22 @@
 Hummingbird.ChatView = Ember.View.extend({
   scrollToBottom: function() {
-    var chatItems = this.$('.chat-items');
-    if (chatItems && (chatItems.prop('scrollTop') > (chatItems.prop('scrollHeight') - chatItems.innerHeight() * 1.5)) ) {
-      Em.run.next(function() {
+    var self = this;
+    Em.run.next(function() {
+      self.send('rescroll');
+    });
+  }.observes('controller.content.@each'),
+  actions: {
+    rescroll: function() {
+      var chatItems = this.$('.chat-items');
+      if (!chatItems) return;
+
+      // Distance between bottom of visible part and the bottom of the scrollable pane
+      var stickyOffset = chatItems.prop('scrollHeight') - chatItems.innerHeight() * 0.5;
+      var scrollBottom = chatItems.prop('scrollTop') + chatItems.innerHeight();
+
+      if (scrollBottom > stickyOffset) {
         chatItems.scrollTop(chatItems.prop('scrollHeight'));
-      });
+      }
     }
-  }.observes('controller.content.@each')
+  }
 });
