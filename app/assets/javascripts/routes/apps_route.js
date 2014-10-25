bf963ac0533163c6005ac37a9efcfd5c5e7a1ac7
Josh Fabian <contact@joshfabian.com>
2014-10-25 14:53:41 -0500

    90% there with new apps page

--- app/assets/javascripts/routes/apps_route.js
+++ app/assets/javascripts/routes/apps_route.js
@@ -10,54 +10,52 @@ HB.AppsRoute = Ember.Route.extend({
         "type": "Android",
         "cover": "",
         "desc": "Tenpenchii is a Hummingbird client for Android devices with \
-              a modern user interface and amazing functionality. It has been \
-              around for over a year now and slowly emerged to the inofficial \
-              Hummingbird client for Android.",
+              a modern user interface and amazing functionality.",
         "links": [
-          { "name": "Website", "link": "http://fractalemagic.com/" },
-          { "name": "Thread", "link": "https://forums.hummingbird.me/t/android-tenpenchii/582" },
+          { "name": "Discuss", "link": "https://forums.hummingbird.me/t/android-tenpenchii/582" },
+          { "name": "Download", "link": "https://play.google.com/store/apps/details?id=com.fractalemagic" },
         ]
       },
       {
         "name": "Taiga",
         "type": "Windows",
         "cover": "",
-        "desc": "Taiga is an open source, lightweight, anime tracker for Windows. \
-              It can automatically detect the episodes you're watching on your \
-              computer and synchronize your progress with Hummingbird. It helps \
-              you manage your list, discover new series, share watched episodes \
+        "desc": "Taiga helps you manage your list, discover new series, share watched episodes \
               and download new ones.",
         "links": [
-          { "name": "Website", "link": "http://taiga.erengy.com" },
-          { "name": "Source", "link": "http://taiga.erengy.com"},
-          { "name": "Thread", "link": "http://forums.hummingbird.me/t/windows-taiga/10565" },
+          { "name": "Discuss", "link": "http://forums.hummingbird.me/t/windows-taiga/10565" },
+          { "name": "Download", "link": "http://taiga.erengy.com/download.php" },
         ]
       },
       {
         "name": "HAPU",
         "type": "Mac",
         "cover": "",
-        "desc": "HAPU is a menubar app that allows you to interact with Hummingbird \
-              and automatically update your anime progress, this also includes posting \
-              to your acticity feed, getting notifications on comment replies, serch for \
-              anime directly withing the app and share what you're watching directly via \
-              facebook or twitter.",
+        "desc": "HAPU is a mac menubar app that allows you to interact with Hummingbird \
+              and automatically update your anime progress.",
         "links": [
-          { "name": "Website", "link": "http://mahdi.jp/apps/hapu" },
-          { "name": "Thread", "link": "https://forums.hummingbird.me/t/mac-hapu-scrobbler-app/56" },
+          { "name": "Discuss", "link": "https://forums.hummingbird.me/t/mac-hapu-scrobbler-app/56" },
+          { "name": "Download", "link": "https://dl.dropboxusercontent.com/u/2439981/HAPU/Sparkle/HAPU-latest.zip" },
         ]
       },
       {
-        "name": "Anitro",
-        "type": "Windows /- Phone",
+        "name": "Anitro Mobile",
+        "type": "Windows Phone",
         "cover": "",
-        "desc": "Anitro is an Anime management application for Windows Phone 8.1 and Windows \
-            8.1 which utilizes the Hummingbird service, and will allow you to View and Manage \
-            your entire profile.",
+        "desc": "Anitro is an application for Windows Phone 8.1 that allows you to View and Manage your entire profile.",
         "links": [
-          { "name": "Windows Phone Store", "link": "http://www.windowsphone.com/en-us/store/app/anitro/6377b104-7df2-4d29-8ebb-88527728e673" },
-          { "name": "Windows Store", "link": "http://apps.microsoft.com/windows/en-us/app/anitro/665e06bf-b4de-4cde-8ae3-c20c1a98b12b" },
-          { "name": "Thread", "link": "https://forums.hummingbird.me/t/anitro-windows-8-1-windows-phone-8-1/3824" },
+          { "name": "Discuss", "link": "https://forums.hummingbird.me/t/anitro-windows-8-1-windows-phone-8-1/3824" },
+          { "name": "Download", "link": "http://www.windowsphone.com/en-us/store/app/anitro/6377b104-7df2-4d29-8ebb-88527728e673" },
+        ]
+      },
+       {
+        "name": "Anitro",
+        "type": "Windows ",
+        "cover": "",
+        "desc": "Anitro is an application for Windows 8.1 that allows you to View and Manage your entire profile.",
+        "links": [
+          { "name": "Discuss", "link": "https://forums.hummingbird.me/t/anitro-windows-8-1-windows-phone-8-1/3824" },
+          { "name": "Download", "link": "http://apps.microsoft.com/windows/en-us/app/anitro/665e06bf-b4de-4cde-8ae3-c20c1a98b12b" },
         ]
       },
       {
@@ -67,8 +65,8 @@ HB.AppsRoute = Ember.Route.extend({
         "desc": "Zenbu is a cross-platform, multi functional, anime and manga desktop \
               management application with exceptional features.",
         "links": [
-          { "name": "Info and Source", "link": "https://bitbucket.org/Ippytraxx/zenbu" },
-          { "name": "Thread", "link": "https://forums.hummingbird.me/t/zenbu-development-log-new-installer-in-development/1583" },
+          { "name": "Discuss", "link": "https://forums.hummingbird.me/t/zenbu-development-log-new-installer-in-development/1583" },
+          { "name": "Download", "link": "https://bitbucket.org/Ippytraxx/zenbu/downloads" },
         ]
       }
     ]
