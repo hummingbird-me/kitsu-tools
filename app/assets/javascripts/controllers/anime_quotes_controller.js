977e2332c7864d5ca11733ceab09ace1edff4e06
Sven Gehring <svengehring@cybrox.eu>
2014-10-29 09:42:53 +0100

    Added quote create and fixed containers

--- app/assets/javascripts/controllers/anime_quotes_controller.js
+++ app/assets/javascripts/controllers/anime_quotes_controller.js
@@ -1,10 +1,43 @@
-HB.AnimeQuotesController = Ember.ObjectController.extend({
+HB.AnimeQuotesController = Ember.ObjectController.extend(HB.HasCurrentUser, {
+
+  showCreate: false,
+  quoteText: "",
+  quoteChar: "",
 
   animeQuotes: function(){
     var self = this;
     return this.store.filter('quote', function(quote){
       return (self.content == quote.anime_id);
     });
-  }.property('@each.quote')
+  }.property('@each.quote'),
+
+
+  actions: {
+    toggleQuoteCreate: function(){
+      this.toggleProperty('showCreate');
+    },
+
+    toggleQuoteFavorite: function(quote) {
+      quote.set('isFavorite', !quote.get('isFavorite'));
+      return quote.save().then(Ember.K, function() {
+        return quote.rollback();
+      });
+    },
+    
+    submitQuote: function(){
+      var self = this,
+          quote = this.store.createRecord('quote', {
+            anime_id: self.content,
+            characterName: self.get('quoteChar'),
+            content: self.get('quoteText'),
+            username: self.get('currentUser.username'),
+            favoriteCount: 0,
+            isFavorite: false
+          });
+
+      quote.save();
+      this.set('showCreate', false);
+    }
+  }
   
 });
