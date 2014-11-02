0b25df94aa0260e36a7aaadfb0fb365c78874373
Peter Lejeck <peter.lejeck@gmail.com>
2014-11-02 15:11:53 -0800

    Update HB.Session.signIn to modern standards
     * Update signIn to use ic.ajax (and leave redirecting as an exercise for the reader)
     * Remove getAuthToken, signInWithOptionalRedirect (lol) and signIn aliases
     * Change url for signIn to /sign-in to match the signOut path

--- app/assets/javascripts/lib/session.js
+++ app/assets/javascripts/lib/session.js
@@ -1,42 +1,13 @@
 HB.Session = {
-  getAuthToken: function() {
-    var parts = document.cookie.split("auth_token=");
-    if (parts.length === 2) {
-      return parts.pop().split(';').shift();
-    }
-  },
-  signInWithOptionalRedirect: function(email, password, redirect) {
-    return $.ajax("/api/v2/sign-in", {
+  signIn: function(email, password) {
+    return ic.ajax({
+      url: "/sign-in",
       type: "POST",
       data: {
         email: email,
         password: password
-      },
-      success: function(data) {
-        // Set cookie.
-        var cookieString = "auth_token=" + data["auth_token"];
-        if (window.location.host !== "localhost:3000") {
-          cookieString += ";domain=." + window.location.host;
-        }
-        cookieString += ";max-age=" + 60 * 60 * 60 * 24 * 365;
-        cookieString += ";path=/";
-        document.cookie = cookieString;
-        // Redirect to previous URL.
-        if (redirect) {
-          window.location.href = window.lastVisitedURL;
-        }
-      },
-      error: function(jqXHR, textStatus, errorThrown) {
-        // TODO handle error.
-        return alert(errorThrown);
       }
     });
-  },
-  signIn: function(email, password) {
-    return HB.Session.signInWithOptionalRedirect(email, password, true);
-  },
-  signInWithoutRedirect: function(email, password) {
-    return HB.Session.signInWithOptionalRedirect(email, password, false);
   },
   signOut: function() {
     return ic.ajax({
