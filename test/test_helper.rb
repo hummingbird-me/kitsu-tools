a4cfb47a1d00434e916c9ed50ee8f746133a6fd3
Vikhyat Korrapati <vikhyatk@gmail.com>
2014-11-12 04:23:00 +0530

    Revert "Fix tests."
    This reverts commit 755ba96bfe0f8883eb41655f7effe67961e64fb9.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -25,8 +25,3 @@ class ActionController::TestCase
     assert JSON.parse(assigns["preload"].to_json).map {|x| x.keys }.flatten.include?(key), "#{key} should be preloaded"
   end
 end
-
-# Don't want SSL redirects in the test suite
-module ActionController::ForceSSL::ClassMethods
-  def force_ssl(options={}); end
-end
