402d73eb4ddb0086166e9962e1e9cafb3662865a
Sven Gehring <svengehring@cybrox.eu>
2014-11-12 12:57:46 +0100

    Add library entries to onboard-lib route

--- app/assets/javascripts/routes/onboarding_library_route.js
+++ app/assets/javascripts/routes/onboarding_library_route.js
@@ -4,6 +4,7 @@ HB.OnboardingLibraryRoute = Ember.Route.extend({
     return Ember.RSVP.hash({
       anime: this.store.find('anime', { 'sort_by': 'user_count' }),
       manga: this.store.find('manga', { 'sort_by': 'created_at' }),
+      library_entry: this.store.find('library_entry', { 'user_id': this.get('currentuser.id') })
     });
   }
 
