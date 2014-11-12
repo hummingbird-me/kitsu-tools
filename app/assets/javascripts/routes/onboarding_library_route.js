473b577ad4f1e0a14382f86ff141b7b02bdec4bf
Sven Gehring <svengehring@cybrox.eu>
2014-11-12 13:12:49 +0100

    Fixed onboarding library route

--- app/assets/javascripts/routes/onboarding_library_route.js
+++ app/assets/javascripts/routes/onboarding_library_route.js
@@ -2,9 +2,10 @@ HB.OnboardingLibraryRoute = Ember.Route.extend({
   
   model: function() {
     return Ember.RSVP.hash({
-      anime: this.store.find('anime', { 'sort_by': 'user_count' }),
-      manga: this.store.find('manga', { 'sort_by': 'created_at' }),
-      library_entry: this.store.find('library_entry', { 'user_id': this.get('currentuser.id') })
+      anime: this.store.find('anime', { 'sort_by': 'user_count', 'sort_reverse': true }),
+      manga: this.store.find('manga', { 'sort_by': 'created_at', 'sort_reverse': true }),
+      library_entry: this.store.find('library_entry', { 'user_id': this.get('currentUser.id') }),
+      manga_library_entry: this.store.find('manga_library_entry', { 'user_id': this.get('currentUser.id') })
     });
   }
 
