f309c05a45d1ba8dbaecd46424d3c77e43802b8c
Sven Gehring <svengehring@cybrox.eu>
2014-11-12 14:24:09 +0100

    Fix onboarding library route model keys

--- app/assets/javascripts/routes/onboarding_library_route.js
+++ app/assets/javascripts/routes/onboarding_library_route.js
@@ -4,8 +4,8 @@ HB.OnboardingLibraryRoute = Ember.Route.extend({
     return Ember.RSVP.hash({
       anime: this.store.find('anime', { 'sort_by': 'user_count', 'sort_reverse': true }),
       manga: this.store.find('manga', { 'sort_by': 'created_at', 'sort_reverse': true }),
-      library_entry: this.store.find('library_entry', { 'user_id': this.get('currentUser.id') }),
-      manga_library_entry: this.store.find('manga_library_entry', { 'user_id': this.get('currentUser.id') })
+      libraryEntries: this.store.find('library_entry', { 'user_id': this.get('currentUser.id') }),
+      mangaLibraryEntries: this.store.find('manga_library_entry', { 'user_id': this.get('currentUser.id') })
     });
   }
 
