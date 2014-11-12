608c3ad1369ff5bcb8f4eef6749a9846836aac67
Vikhyat Korrapati <vikhyatk@gmail.com>
2014-11-12 12:48:39 +0530

    Type is a reserved field in JSON-API.

--- app/serializers/api/v2/anime_serializer.rb
+++ app/serializers/api/v2/anime_serializer.rb
@@ -4,12 +4,11 @@ module Api::V2
 
     fields :id, :slug, :canonical_title, :synopsis, :started_airing_date,
       :finished_airing_date, :youtube_video_id, :age_rating, :episode_count,
-      :episode_length
+      :episode_length, :show_type
 
     field(:english_title) {|a| a.alt_title }
     field(:romaji_title) {|a| a.title }
     field(:poster_image) {|a| a.poster_image_thumb }
-    field(:type) {|a| a.show_type }
     field(:community_rating) {|a| a.bayesian_average }
     field(:genres) {|a| a.genres.map {|x| x.name } }
     field(:screencaps) {|a| a.gallery_images.map {|x| x.image.url(:thumb) } }
