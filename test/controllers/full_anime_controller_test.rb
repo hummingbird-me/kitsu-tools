3d7d31cf446ffbb850f1db221e9e19b3262ceb0a
Thomas McNiven <hello@vevix.net>
2014-11-13 11:53:57 +1000

    add model test, add test for creating an edit

--- test/controllers/full_anime_controller_test.rb
+++ test/controllers/full_anime_controller_test.rb
@@ -6,4 +6,22 @@ class FullAnimeControllerTest < ActionController::TestCase
     assert_response 200
     assert_equal FullAnimeSerializer.new(anime(:sword_art_online)).to_json, @response.body
   end
+
+  # TODO: needs updating when editing is available to all users
+  test "can edit an anime as staff" do
+    sign_in users(:josh)
+
+    data = {
+      synopsis: "Hello, World!",
+      episode_count: 32
+    }
+
+    put :update, format: :json, id: 'sword-art-online', full_anime: data
+    assert_response 200
+    assert_equal 4, Version.count # 3 fixtures + new
+
+    version = Version.last
+    assert_equal data[:synopsis], version.object["synopsis"]
+    assert_equal data[:episode_count], version.object["episode_count"]
+  end
 end
---
+++ test/models/version_test.rb
@@ -0,0 +1,21 @@
+# == Schema Information
+#
+# Table name: versions
+#
+#  id             :integer          not null, primary key
+#  item_id        :integer          not null
+#  item_type      :string(255)      not null
+#  user_id        :integer
+#  object         :json             not null
+#  object_changes :json             not null
+#  state          :integer          default(0)
+#  created_at     :datetime
+#  updated_at     :datetime
+#
+
+class VersionTest < ActiveSupport::TestCase
+  should validate_presence_of(:item)
+  should validate_presence_of(:user)
+  should validate_presence_of(:object)
+  should validate_presence_of(:object_changes)
+end
