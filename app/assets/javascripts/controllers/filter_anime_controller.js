da2c60dc4f7c7cc356844d42fcdaefe67eda862b
Sven Gehring <svengehring@cybrox.eu>
2014-11-14 11:24:22 +0100

    First, basic refactoring of the anime filter stuff

--- app/assets/javascripts/controllers/filter_anime_controller.js
+++ app/assets/javascripts/controllers/filter_anime_controller.js
@@ -1,53 +1,27 @@
-HB.FilterAnimeController = Ember.ObjectController.extend({
+HB.FilterAnimeController = Ember.Controller.extend({
   queryParams: ['query'],
 
-  filterSorts: [
-    {name: "Highest Rated", value: "rating"},
-    {name: "Most Popular", value: "popular"},
-    {name: "Newest", value: "newest"},
-    {name: "Oldest", value: "oldest"}
-  ],
-
-  filterTimes: [
-    "Upcoming",
-    "2010s",
-    "2000s",
-    "1990s",
-    "1980s",
-    "1970s",
-    "Older"
-  ],
-
-  filterGenres: function(){
-    return this.store.all('genre');
-  }.property('@each.genres'),
-  
   query: "",
   showPage: 1,
   isLoading: false,
   animeList: [],
-  selectSort: "rating",
   selectTime: {},
   selectGenre: {},
   hasManuallySet: false,
 
+  selectedSort: "rating",
+  selectedItems: [],
+
 
   encodeQuery: function(){
     var query  = "?page=" + this.get('showPage');
-        query += "&sort=" + this.get('selectSort');
+        query += "&sort=" + this.get('selectedSort');
 
-    $.each(this.get('selectTime'), function(key, val){
-      if(val) query += "&y[]=" + key;
+    this.get('selectedItems').forEach(function(item){
+      query += "&" + item.fkey + "[]=" + item.fval;
     });
 
-    $.each(this.get('selectGenre'), function(key, val){
-      if(val) query += "&g[]=" + key;
-    });
-
-    this.setProperties({
-      'query': query,
-      'hasManuallySet': true
-    });
+    this.set('query', query);
   },
 
   decodeQuery: function(){
@@ -74,17 +48,36 @@ HB.FilterAnimeController = Ember.ObjectController.extend({
 
       this.setProperties({
         'showPage': parseInt(/\?page\=([0-9]{1,4})/.exec(query)[1]),
-        'selectSort': /\&sort\=([a-z]{6,7})/.exec(query)[1],
+        'selectedSort': /\&sort\=([a-z]{6,7})/.exec(query)[1],
         'selectGenre': tempg,
         'selectTime': tempy
       });
     }
   }.observes('query'),
 
-  updateItem: function(item, state){
-    if(state) $('#'+item).addClass('active');
-    else  $('#'+item).removeClass('active');
-  },
+  applyFilter: function(){
+    this.encodeQuery();
+
+    console.log(this.get('query'));
+    /*
+
+    var self = this,
+        requrl = decodeURIComponent(this.get('query'));
+        requrl = requrl.replace(/\[/,'%5B');
+        requrl = requrl.replace(/\]/,'%5D');
+        requrl += "&new_filter=true" // TEMPORARY!!
+        // ^ Hacky solution to allow us to store
+        //   query as an ember query param
+
+    this.set('isLoading', true);
+    $.getJSON('/anime/filter.json' + requrl, function(payload){
+      self.set('isLoading', false);
+      self.store.pushPayload(payload);
+      self.set('animeList', payload.anime);
+    });
+    */
+  }.observes('selectedItems.length'),
+
 
   actions: {
     switchPage: function(mod){
@@ -96,33 +89,12 @@ HB.FilterAnimeController = Ember.ObjectController.extend({
       this.send('applyFilter');
     },
 
-    toggleFilterTime: function(item){
-      this.set('selectTime.'+item, !this.get('selectTime.'+item));
-      this.updateItem(item, this.get('selectTime.'+item));
-    },
+    selectItem: function(item){
+      var currentState = item.get('selected');
+      item.set('selected', !currentState);
 
-    toggleFilterGenre: function(item){
-      this.set('selectGenre.'+item, !this.get('selectGenre.'+item));
-      this.updateItem(item, this.get('selectGenre.'+item));
-    },
-
-    applyFilter: function(){
-      this.encodeQuery();
-
-      var self = this,
-          requrl = decodeURIComponent(this.get('query'));
-          requrl = requrl.replace(/\[/,'%5B');
-          requrl = requrl.replace(/\]/,'%5D');
-          requrl += "&new_filter=true" // TEMPORARY!!
-          // ^ Hacky solution to allow us to store
-          //   query as an ember query param
-
-      this.set('isLoading', true);
-      $.getJSON('/anime/filter.json' + requrl, function(payload){
-        self.set('isLoading', false);
-        self.store.pushPayload(payload);
-        self.set('animeList', payload.anime);
-      });
+      if(currentState) this.get('selectedItems').removeObject(item);
+      else this.get('selectedItems').pushObject(item);
     }
   }
 });
---
+++ app/assets/javascripts/routes/filter_anime_route.js
@@ -0,0 +1,45 @@
+HB.FilterAnimeRoute = Ember.Route.extend({
+
+  setupController: function(controller){
+
+    // Every object will be mapped to contain a value for fkey (the query key)
+    // and fval (the query value). These values will be sent to the server to
+    // request the respective search results.
+
+    this.store.find('genre').then(function(genres){
+      controller.set('filterGenres', genres.map(function(genre) {
+        return Ember.Object.create({
+          model: genre,
+          selected: false,
+          fkey: "g",
+          fval: genre.get('id')
+        });
+      }));
+    });
+
+    controller.setProperties({
+      filterTimes: [
+        Ember.Object.create({ selected: false, fkey: "y", fval: "Upcoming" }),
+        Ember.Object.create({ selected: false, fkey: "y", fval: "2010s" }),
+        Ember.Object.create({ selected: false, fkey: "y", fval: "2000s" }),
+        Ember.Object.create({ selected: false, fkey: "y", fval: "1990s" }),
+        Ember.Object.create({ selected: false, fkey: "y", fval: "1980s" }),
+        Ember.Object.create({ selected: false, fkey: "y", fval: "1970s" }),
+        Ember.Object.create({ selected: false, fkey: "y", fval: "Older" })
+      ],
+
+      filterUserCount: [],
+      filterStudios: [],
+      filterRating: [],
+
+      filterSorts: [
+        { name: "Highest Rated", value: "rating" },
+        { name: "Most Popular", value: "popular" },
+        { name: "Newest", value: "newest" },
+        { name: "Oldest", value: "oldest" }
+      ]
+    });
+
+  }
+
+});
