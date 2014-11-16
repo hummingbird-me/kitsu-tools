e0ad64676e6cd5c895f0c97774cc2dfab490a42a
Vikhyat Korrapati <vikhyatk@gmail.com>
2014-11-17 02:49:11 +0530

    Fix character, person image sizes.

--- app/models/character.rb
+++ app/models/character.rb
@@ -30,7 +30,7 @@ class Character < ActiveRecord::Base
   has_many :castings, dependent: :destroy
 
   has_attached_file :image,
-    styles: {thumb_small: "30x39#"},
+    styles: {thumb_small: "60x60#"},
     default_url: "/assets/default-avatar.jpg",
     convert_options: {all: "-unsharp 2x0.5+1+0"}
 
