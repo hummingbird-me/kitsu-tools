4900272a05ecd510373b3efc6f728b012c512a71
Vikhyat Korrapati <vikhyatk@gmail.com>
2014-11-16 13:56:38 +0530

    Sort by bayesian_average for onboarding.

--- app/assets/javascripts/routes/onboarding_library_route.js
+++ app/assets/javascripts/routes/onboarding_library_route.js
@@ -4,7 +4,7 @@ HB.OnboardingLibraryRoute = Ember.Route.extend({
     controller.set('loading', true);
 
     var promises = {
-      anime: this.store.find('anime', { 'sort_by': 'user_count', 'sort_reverse': true }),
+      anime: this.store.find('anime', { 'sort_by': 'bayesian_average', 'sort_reverse': true }),
       manga: this.store.find('manga', { 'sort_by': 'created_at', 'sort_reverse': true }),
       libraryEntries: this.store.find('library_entry', { 'user_id': this.get('currentUser.id') }),
       mangaLibraryEntries: this.store.find('manga_library_entry', { 'user_id': this.get('currentUser.id') })
