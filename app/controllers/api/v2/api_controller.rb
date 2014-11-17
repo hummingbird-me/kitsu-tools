ae43940280af5335a4ecef9eb93390124dae56ff
Peter Lejeck <peter.lejeck@gmail.com>
2014-11-17 02:43:41 -0800

    Require a client id header for all APIv2 requests

--- app/controllers/api/v2/api_controller.rb
+++ app/controllers/api/v2/api_controller.rb
@@ -1,5 +1,20 @@
 module Api::V2
   class ApiController < ActionController::Base
     force_ssl if Rails.env.production?
+
+    def error!(info, status)
+      info = { title: info } if info.is_a?(String)
+
+      render json: {errors: [info.merge({
+        status: Rack::Utils::HTTP_STATUS_CODES[status],
+      })]}, status: status
+    end
+
+    def require_client_id
+      unless request.headers['X-Client-Id'] && App.exists?(key: request.headers['X-Client-Id'])
+        error! "Missing or invalid API key; please pass the X-Client-Id header", 403
+      end
+    end
+    before_filter :require_client_id
   end
 end
