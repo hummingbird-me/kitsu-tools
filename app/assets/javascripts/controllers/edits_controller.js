70f09ca0442c1dd92400cc96e5fe86b3a13bed13
Thomas McNiven <hello@vevix.net>
2014-11-17 19:17:18 +1000

    remove unused field [skip ci]

--- app/assets/javascripts/controllers/edits_controller.js
+++ app/assets/javascripts/controllers/edits_controller.js
@@ -1,6 +1,4 @@
 HB.EditsController = Ember.ArrayController.extend({
-  isDiffShown: false,
-
   pendingAnime: function() {
     return this.get('content').filterBy('objectType', 'anime');
   }.property('@each'),
