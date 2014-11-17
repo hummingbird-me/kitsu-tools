36d13f1c27e2f71d2800acdb12b1297ca4c98f64
Peter Lejeck <peter.lejeck@gmail.com>
2014-11-17 02:56:08 -0800

    The smallest commit ever made (to fix the test still)

--- test/controllers/api/v2/anime_controller_test.rb
+++ test/controllers/api/v2/anime_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class Api::V2::AnimeControllerTest < ActionController::TestCase
   test "can get anime" do
-    controller.class.skip_before_filter :require_client_id
+    @controller.class.skip_before_filter :require_client_id
     get :show, id: 'sword-art-online'
     assert_response 200
 
