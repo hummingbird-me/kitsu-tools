53850768cc6ce57a92f4f34ec431217e2ef58264
Sven Gehring <svengehring@cybrox.eu>
2014-11-18 08:14:39 +0100

    Fix notification loading (dirty)

--- app/assets/javascripts/controllers/notifications_controller.js
+++ app/assets/javascripts/controllers/notifications_controller.js
@@ -1,4 +1,11 @@
 HB.NotificationsController = Ember.ArrayController.extend({
+
+  init: function(){
+    var self = this;
+    this.set('content', self.store.find('notification'));
+    this._super();
+  },
+
   sortProperties: ['createdAt'],
   sortAscending: false
 });
