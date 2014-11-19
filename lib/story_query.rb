f0c064a2c7b5a7e3fd2b1fe333d20fab70889791
Vikhyat Korrapati <vikhyatk@gmail.com>
2014-11-20 01:36:38 +0530

    Add StoryQuery helper class.

--- lib/story_query.rb
+++ lib/story_query.rb
@@ -2,19 +2,28 @@ class StoryQuery
 
   # Returns a list of stories having the given IDs.
   def self.find_by_ids(story_ids, current_user)
-    # Side-load genres for stories where the target is anime.
-    stories = Story.where(id: story_ids, :target_type => 'Anime').for_user(current_user).includes(:user, :substories, target: :genres, substories: :user)
-    # Don't side-load genres for regular stories.
-    if story_ids.length > stories.length
-      stories += Story.where(id: story_ids).where('target_type <> ?', 'Anime').for_user(current_user).includes(:user, :substories, :target, substories: :user)
+    stories = Story.where(id: story_ids).for_user(current_user)
+                   .includes(:user, :substories, :target, substories: :user)
+
+    # Preload genres for anime stories.
+    anime_stories = stories.select {|s| s.target_type == "Anime" }.map(&:target)
+                   .index_by(&:id)
+    Anime.where(id: anime_stories.values.map(&:id)).includes(:genres).each do |a|
+      assoc = anime_stories[a.id].association(:genres)
+      assoc.loaded!
+      assoc.target.concat(a.genres)
+      a.genres.each {|genre| assoc.set_inverse_instance(genre) }
     end
-    # Return in the same order as the IDs passed in.
+
     stories.sort_by {|s| story_ids.find_index s.id }
   end
 
   # Return `limit` stories for the given user.
   def self.find_for_user(user, current_user, page, per_page)
-    story_ids = user.stories.for_user(current_user).order('stories.updated_at DESC').select(:id).page(page).per(per_page).map {|x| x.id }
+    story_ids = user.stories.for_user(current_user)
+                    .order('stories.updated_at DESC').select(:id)
+                    .page(page).per(per_page).map(&:id)
+
     StoryQuery.find_by_ids(story_ids, current_user)
   end
 
