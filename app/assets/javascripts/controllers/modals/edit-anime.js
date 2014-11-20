b23fb9af867e3a2338dc2b414a931f516479afc4
Thomas McNiven <hello@vevix.net>
2014-11-21 03:20:03 +1000

    fix bug with escaping urls

--- app/assets/javascripts/controllers/modals/edit-anime.js
+++ app/assets/javascripts/controllers/modals/edit-anime.js
@@ -1,16 +1,8 @@
 HB.ModalsEditAnimeController = Ember.ObjectController.extend(HB.ModalControllerMixin, {
   canSave: Ember.computed.not('isDirty'),
 
-  escapeURIs: function() {
-    this.get('content').setProperties({
-      'posterImage': encodeURI(this.get('content.posterImage')),
-      'coverImage': encodeURI(this.get('content.coverImage'))
-    });
-  },
-
   actions: {
     save: function () {
-      this.escapeURIs();
       Messenger().expectPromise(function() {
         return this.get('content').save();
       }.bind(this), {
