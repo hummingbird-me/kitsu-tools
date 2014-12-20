bfa08a9a5ee6eade3999e7992e254c8cb700a2b6
Josh Fabian <contact@joshfabian.com>
2014-12-20 14:02:03 -0500

    Lower pro pricing by one dollar

--- app/models/pro_membership_plan.rb
+++ app/models/pro_membership_plan.rb
@@ -9,11 +9,11 @@ class ProMembershipPlan
               :recurring # is it recurring?
 
   PLANS = [
-    {id: 1, name: "monthly - $4.99", amount: BigDecimal.new("4.99"), duration: 1, recurring: true},
-    {id: 2, name: "yearly - $39.99 (~$3.33/month)", amount: BigDecimal.new("39.99"), duration: 12, recurring: true},
-    {id: 3, name: "1 month: $4.99", amount: BigDecimal.new("4.99"), duration: 1, recurring: false},
+    {id: 1, name: "monthly - $3.99", amount: BigDecimal.new("3.99"), duration: 1, recurring: true},
+    {id: 2, name: "yearly - $29.99 ($2.50/month)", amount: BigDecimal.new("29.99"), duration: 12, recurring: true},
+    {id: 3, name: "1 month: $3.99", amount: BigDecimal.new("3.99"), duration: 1, recurring: false},
     {id: 4, name: "3 months: $11.97", amount: BigDecimal.new("11.97"), duration: 3, recurring: false},
-    {id: 5, name: "12 months (special price!): $39.99", amount: BigDecimal.new("39.99"), duration: 12, recurring: false},
+    {id: 5, name: "12 months (special price!): $29.99", amount: BigDecimal.new("29.99"), duration: 12, recurring: false},
     {id: 6, name: "24 months (special price!): $59.98", amount: BigDecimal.new("59.98"), duration: 24, recurring: false},
     {id: 7, name: "36 months (special price!): $89.97", amount: BigDecimal.new("89.97"), duration: 36, recurring: false}
   ]
