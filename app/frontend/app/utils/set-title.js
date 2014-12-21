645990c2ab270d430cd1dc041f7494089864e1f7
Vikhyat Korrapati <vikhyatk@gmail.com>
2014-12-21 12:43:28 +0530

    accidentally broken set-title.

--- app/frontend/app/utils/set-title.js
+++ app/frontend/app/utils/set-title.js
@@ -3,12 +3,16 @@ import Ember from "ember";
 export default function setTitle(title) {
   var actualTitle = 'Hummingbird';
 
-  if (typeof title === 'string' && title.length > 0 && title.indexOf('Hummingbird') < 0) {
-    actualTitle = title + " | Hummingbird";
+  if (typeof title === 'string' && title.length > 0) {
+    if (title.indexOf('Hummingbird') < 0) {
+      actualTitle = title + " | Hummingbird";
+    } else {
+      actualTitle = title;
+    }
   }
 
   document.title = actualTitle;
-  Ember.$("meta[name='og:title']").attr('content', title);
+  Ember.$("meta[name='og:title']").attr('content', actualTitle);
 
   return true;
 }
