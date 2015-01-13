40df047b5be78ee6a6178c89426b0decdcb30207
Sven Gehring <svengehring@cybrox.eu>
2015-01-13 22:19:37 +0100

    Add favRank to favorite model

--- app/frontend/app/models/favorite.js
+++ app/frontend/app/models/favorite.js
@@ -1,6 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  favRank: DS.attr('number'),
+
   user: DS.belongsTo('user', { async: true }),
   item: DS.belongsTo('media', { polymorphic: true, async: true }),
 });
