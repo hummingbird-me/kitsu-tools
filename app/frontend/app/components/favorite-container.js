cdf0d4a40d9b432a5a7a4d86a9c5f8456474b4a7
Sven Gehring <svengehring@cybrox.eu>
2015-01-13 22:19:51 +0100

    First, really basic sort list implementation

--- app/frontend/app/components/favorite-container.js
+++ app/frontend/app/components/favorite-container.js
@@ -10,5 +10,33 @@ export default Ember.Component.extend({
 
   disabled: function() {
     return !this.get('isEditing');
-  }.property('isEditing')
+  }.property('isEditing'),
+
+  editable: function(){
+    var self = this;
+
+    if(this.get('isEditing')){
+      this.$().dragsort({
+        dragEnd: function(){
+          self.$().children('li').each(function(i, fav){
+            var id = $(fav).attr('data-id');
+            self.get('targetObject.store').find('favorite', id).then(function(item){
+              item.set('favRank', i);
+            });
+          })
+        }
+      });
+    } else {
+      this.$().dragsort('destroy');
+    }
+  }.observes('isEditing'),
+
+
+  actions: {
+    deleteFavorite: function(fav){
+      this.get('targetObject.store').find('favorite', fav.id).then(function(item){
+        item.deleteRecord();
+      });
+    }
+  }
 });
