24994e2827504f01b7cbf482b382cde3be58b042
Sven Gehring <svengehring@cybrox.eu>
2015-01-14 14:07:36 +0100

    Add user to_follow to public data

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,7 +2,8 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :cover_image_url, :avatar_template, :rating_type, :mini_bio,
     :about, :is_followed, :title_language_preference, :location, :website,
     :waifu, :waifu_or_husbando, :waifu_slug, :waifu_char_id, :last_sign_in_at,
-    :current_sign_in_at, :is_admin, :following_count, :follower_count, :is_pro
+    :current_sign_in_at, :is_admin, :following_count, :follower_count, :is_pro,
+    :to_follow
 
   def id
     object.name
