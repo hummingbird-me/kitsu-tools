e6097b8531b1c86301a4e584a1871be1863c443b
Sven Gehring <svengehring@cybrox.eu>
2015-01-14 12:59:36 +0100

    Workaround for ember-data issue 2385

--- app/frontend/app/models/favorite.js
+++ app/frontend/app/models/favorite.js
@@ -4,5 +4,11 @@ export default DS.Model.extend({
   favRank: DS.attr('number'),
 
   user: DS.belongsTo('user', { async: true }),
-  item: DS.belongsTo('media', { polymorphic: true, async: true }),
+  item: DS.belongsTo('media', { polymorphic: true }),
+  
+  // item: DS.belongsTo('media', { polymorphic: true, async: true }),
+  // FIXME: The item relation should actually be an async relation.
+  //   In our case, this will currently work, even if we comment it
+  //   out, since the FavoriteSerializer includes the item information.
+  //   once Ember Data issue #2385 is fixed, we can re-implement this.
 });
