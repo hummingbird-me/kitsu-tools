482076bcd7762f5553197183583f41ebec0945d2
Thomas McNiven <hello@vevix.net>
2015-01-19 12:19:18 +1000

    fix cover image on group page, add manage template

--- app/frontend/app/controllers/group.js
+++ app/frontend/app/controllers/group.js
@@ -2,6 +2,10 @@ import Ember from 'ember';
 import HasCurrentUser from '../mixins/has-current-user';
 
 export default Ember.Controller.extend(HasCurrentUser, {
+  coverImageStyle: function() {
+    return "background-image: url(" + this.get('model.coverImage') + ")";
+  }.property('model.coverImage'),
+
   currentMember: function() {
     return this.get('model.members').findBy('user.id', this.get('currentUser.id'));
   }.property('model.members'),
