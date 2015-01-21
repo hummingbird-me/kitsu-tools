841e6daa0f75c10a07a9094a09c0222f327d6559
Peter Lejeck <peter.lejeck@gmail.com>
2015-01-20 21:03:50 -0800

    pro_memership_id -> pro_memebership_plan_id

--- app/workers/pro_renew_retry_worker.rb
+++ app/workers/pro_renew_retry_worker.rb
@@ -7,11 +7,11 @@ class ProRenewRetryWorker
   def perform
     recurring_plans = ProMembershipPlan.recurring_plans.map(&:id)
     tries = Array.new(3)
-    tries[0] = User.where(pro_membership_id: recurring_plans,
+    tries[0] = User.where(pro_membership_plan_id: recurring_plans,
                           pro_expires_at: (2.days.ago)..(1.day.ago))
-    tries[1] = User.where(pro_membership_id: recurring_plans,
+    tries[1] = User.where(pro_membership_plan_id: recurring_plans,
                           pro_expires_at: (4.days.ago)..(3.day.ago))
-    tries[2] = User.where(pro_membership_id: recurring_plans,
+    tries[2] = User.where(pro_membership_plan_id: recurring_plans,
                           pro_expires_at: (6.days.ago)..(5.day.ago))
 
     tries.each.with_index do |users, index|
