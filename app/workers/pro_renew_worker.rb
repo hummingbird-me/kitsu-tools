9e10bfcff5ed6384cbc24d187a7879814e8e5825
Peter Lejeck <peter.lejeck@gmail.com>
2015-01-21 18:26:14 -0800

    Make everything use current or last instead of relative to now

--- app/workers/pro_renew_worker.rb
+++ app/workers/pro_renew_worker.rb
@@ -5,9 +5,10 @@ class ProRenewWorker
   recurrence { hourly }
 
   def perform(last, current)
+    recurring_plans = ProMembershipPlan.recurring_plans.map(&:id)
     # Anything which wasn't caught in the last run, up until 1 hour from now
-    users = User.where(pro_membership_plan_id: ProMembershipPlan.recurring_plans.map(&:id),
-                       pro_expires_at: (last + 1.hour)..(1.hour.from_now.to_i))
+    users = User.where(pro_membership_plan_id: recurring_plans,
+                       pro_expires_at: (last + 1.hour)..(current + 1.hour))
 
     users.all.each do |user|
       manager = ProMembershipManager.new(user)
