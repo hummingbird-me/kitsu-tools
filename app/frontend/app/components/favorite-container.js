f04346d7a3da929f2f699a8c33b43835e160ff40
Sven Gehring <svengehring@cybrox.eu>
2015-01-22 10:25:35 +0100

    Move all the favorite stuff into its own component

--- app/frontend/app/components/favorite-container.js
+++ app/frontend/app/components/favorite-container.js
@@ -1,19 +1,37 @@
 import Ember from 'ember';
+import ajax from 'ic-ajax';
 
-// TODO HB.SortableMixin
+var FAVS_PER_PAGE = 6;
+
 export default Ember.Component.extend({
-  tagName: 'ul',
-  classNames: 'media-grid',
-  connectWith: '.grid-thumb',
-  tolerance: 'pointer',
-  containment: '.favorite-anime',
+  store: Ember.computed.alias('targetObject.store'),
 
   isAnimeContainer: Ember.computed.equal('containerType', 'anime'),
   isMangaContainer: Ember.computed.equal('containerType', 'manga'),
 
+  isEditing: false,
+  isLoading: true,
+  showPages: 1,
+
+
   favoriteList: function() {
+    this.set('isLoading', false);
     return this.get('favorites').sortBy('favRank');
   }.property('favorites.@each' ,'isEditing'),
+
+  favoriteListCapped: function(){
+    if(this.get('isEditing')) { return this.get('favoriteList'); }
+
+    var favs = this.get('favoriteList'),
+        page = this.get('showPages');
+
+    return favs.slice(0, page * FAVS_PER_PAGE);
+  }.property('favorites.@each', 'showPages', 'isEditing'),
+
+  canLoadMoreFavorites: function () {
+    var page = this.get('showPages');
+    return (page * FAVS_PER_PAGE + 1 <= this.get('favoriteList.length'));
+  }.property('favoriteList.@each', 'showPages'),
 
   disabled: function() {
     return !this.get('isEditing');
@@ -23,10 +41,10 @@ export default Ember.Component.extend({
     var self = this;
 
     if(this.get('isEditing')) {
-      this.$().dragsort({
+      this.$('.media-grid-dragsort').dragsort({
         dragSelector: ".grid-draggable",
         dragEnd: function() {
-          self.$().children('li').each(function(i, fav) {
+          self.$('.media-grid-dragsort').children('li').each(function(i, fav) {
             var id = self.$(fav).attr('data-id');
             self.get('targetObject.store').find('favorite', id).then(function(item) {
               item.set('favRank', i);
@@ -37,17 +55,60 @@ export default Ember.Component.extend({
     } else {
       // Manual workaround for broken part in vendor script
       // this.$().dragsort('destroy');
-      this.$().unbind("mousedown")
+      this.$('.media-grid-dragsort').unbind("mousedown")
               .children().unbind("mousedown")
                          .unbind("dragsort-uninit");
     }
   }.observes('isEditing'),
+
+  viewingSelf: function(){
+    return (this.get('user.id') === this.get('currentUser.id'));
+  }.property('user.id', 'currentUser.id'),
 
 
   actions: {
     deleteFavorite: function(fav) {
       this.get('targetObject.store').find('favorite', fav.id).then(function(item) {
         item.destroyRecord();
+      });
+    },
+
+    loadMoreFavorites: function () {
+      var page = this.get('showPages');
+      if (page * FAVS_PER_PAGE + 1 <= this.get('favoriteList.length')) {
+        this.set('showPages', (page + 1));
+      }
+    },
+
+    editFavorites: function(){
+      this.set('isEditing', true);
+    },
+
+    saveFavoritePositions: function () {
+      this.set('isEditing', false);
+      this.get('targetObject.store').filter('favorite', function(fav) {
+        return fav.get('isDirty') === true;
+      }).then(function(updatedFavs) {
+        if(updatedFavs.get('length') === 0) {
+          return;
+        }
+
+        var postData = updatedFavs.map(function(fav) {
+          return {
+            id: fav.get('id'),
+            rank: fav.get('favRank')
+          };
+        });
+
+        return ajax({
+          type: 'POST',
+          url: "/favorites/update_all",
+          data: { favorites: JSON.stringify(postData) },
+          dataType: 'json'
+        }).then(Ember.K, function() {
+          alert("Something went wrong.");
+        });
+
       });
     },
 
