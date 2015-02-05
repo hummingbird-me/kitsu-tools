5d73ab29787cde16ea39f0c3b9eee577534bf880
Peter Lejeck <peter.lejeck@gmail.com>
2015-02-05 13:00:51 -0800

    Show pendingness on settings page

--- frontend/app/controllers/settings.js
+++ frontend/app/controllers/settings.js
@@ -84,6 +84,34 @@ export default Ember.Controller.extend({
            !this.get('usernameValid');
   }.property('currentUser.isDirty', 'passwordValid', 'usernameValid'),
 
+  importList: function(service, file) {
+    var data = new FormData();
+    data.append('list', file);
+    return ajax('/settings/import/' + service, {
+      data: data,
+      cache: false,
+      contentType: false,
+      processData: false,
+      type: 'POST'
+    }).then(() => {
+      this.store.update('currentUser', {
+        id: this.get('currentUser.id'),
+        importStatus: 'queued',
+        importingFrom: service
+      });
+    }, (err) => {
+      try {
+        this.store.update('currentUser', {
+          id: this.get('currentUser.id'),
+          importStatus: 'error',
+          importError: err.jqXHR.responseJSON.error
+        });
+      } catch (e) {
+        alert('An unknown error occurred while attempting to import your list');
+      }
+    });
+  },
+
   actions: {
     save: function() {
       var self = this;
@@ -125,11 +153,11 @@ export default Ember.Controller.extend({
     pickHbFile: function() {
       Ember.$('#hb-file').click();
     },
-    restoreBackup: function() {
-      Ember.$('#backuprestore').submit();
+    importHummingbird: function(file) {
+      this.importList('hummingbird', file);
     },
-    importMal: function() {
-      Ember.$('#malimport').submit();
+    importMal: function(file) {
+      this.importList('myanimelist', file);
     }
   }
 });
--- frontend/app/models/current-user.js
+++ frontend/app/models/current-user.js
@@ -9,10 +9,19 @@ export default User.extend({
   sfwFilter: DS.attr('boolean'),
   lastBackup: DS.attr('date'),
   hasDropbox: DS.attr('boolean'),
+  importStatus: DS.attr('string'),
+  importFrom: DS.attr('string'),
+  importError: DS.attr('string'),
   hasFacebook: DS.attr('boolean'),
   confirmed: DS.attr('boolean'),
   proExpiresAt: DS.attr('date'),
   proMembershipPlan: DS.belongsTo('pro-membership-plan'),
+
+  isImportOngoing: function() {
+    var status = this.get('importStatus');
+    return status === 'queued' || status === 'running';
+  }.property('importStatus'),
+  isImportErrored: Ember.computed.equal('importStatus', 'error'),
 
   // Temporary solution for beta indicator
   betaAccess: Ember.computed.or('isAdmin', 'isPro')
