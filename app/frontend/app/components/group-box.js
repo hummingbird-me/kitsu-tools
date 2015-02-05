c20d25b68462aaaabd965aa09869848a2d578ed0
Sven Gehring <svengehring@cybrox.eu>
2015-02-05 21:56:32 +0100

    Hide join group button if user already is member

--- app/frontend/app/components/group-box.js
+++ app/frontend/app/components/group-box.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+  userIsMemberOfGroup: function(){
+    var users = this.get('group.members').map(function(member){
+      return member.get('user.id');
+    });
+
+    return users.contains(this.get('currentUser.id'));
+  }.property('currentUser', 'group')
+
 });
