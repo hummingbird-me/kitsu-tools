shiranwa/hummingbird

implement feed posting on groups

app/controllers/stories_controller.rb
@@ -30,15 +30,26 @@ class StoriesController < ApplicationController
 
   def create
     authenticate_user!
-    params.require(:story).permit(:user_id, :comment)
+    params.require(:story).permit(:user_id, :group_id, :comment)
 
-    user = User.find(params[:story][:user_id])
-    story = Action.broadcast(
-      action_type: "created_profile_comment",
-      user: user,
-      poster: current_user,
-      comment: params[:story][:comment]
-    )
+    if params[:story][:group_id].present?
+      group = Group.find(params[:story][:group_id])
+      story = Action.broadcast(
+        action_type: "created_group_comment",
+        group: group,
+        user: current_user,
+        poster: current_user,
+        comment: params[:story][:comment]
+      )
+    else
+      user = User.find(params[:story][:user_id])
+      story = Action.broadcast(
+        action_type: "created_profile_comment",
+        user: user,
+        poster: current_user,
+        comment: params[:story][:comment]
+      )
+    end
 
     render json: StoryQuery.find_by_id(story.id, current_user)
   end

hummingbird-me/kitsu-tools

app/controllers/stories_controller.rb
@@ -30,16 +30,28 @@ class StoriesController < ApplicationController
 
   def create
     authenticate_user!
-    params.require(:story).permit(:user_id, :comment)
+    params.require(:story).permit(:user_id, :group_id, :comment)
 
-    user = User.find(params[:story][:user_id])
-    story = Action.broadcast(
-      action_type: "created_profile_comment",
-      user: user,
-      poster: current_user,
-      comment: params[:story][:comment],
-      adult: params[:story][:adult]
-    )
+    if params[:story][:group_id].present?
+      group = Group.find(params[:story][:group_id])
+      story = Action.broadcast(
+        action_type: "created_group_comment",
+        group: group,
+        user: current_user,
+        poster: current_user,
+        comment: params[:story][:comment],
+        adult: params[:story][:adult]
+      )
+    else
+      user = User.find(params[:story][:user_id])
+      story = Action.broadcast(
+        action_type: "created_profile_comment",
+        user: user,
+        poster: current_user,
+        comment: params[:story][:comment],
+        adult: params[:story][:adult]
+      )
+    end
 
     render json: StoryQuery.find_by_id(story.id, current_user)
   end