e1e0fab38d89365c528ddf4d89ffdf98b723046e
Thomas McNiven <hello@vevix.net>
2015-02-14 04:17:59 +1000

    don't serialize group on members

--- app/serializers/group_member_serializer.rb
+++ app/serializers/group_member_serializer.rb
@@ -2,7 +2,6 @@ class GroupMemberSerializer < ActiveModel::Serializer
   embed :ids, include: true
 
   attributes :id, :rank, :pending, :group_id
-  has_one :group, embed_key: :slug
   has_one :user, embed_key: :name
 
   def group_id
