b59fb0269f06f9b2df8dc7da4decc2b04dca6b07
Thomas McNiven <hello@vevix.net>
2015-02-17 05:39:11 +1000

    get rid of unused devise controllers/routes, redirect to new routes. fixes #387

--- app/controllers/users/sessions_controller.rb
+++
@@ -1,8 +0,0 @@
-class Users::SessionsController < Devise::SessionsController
-  def new
-    session["user_return_to"] = params[:return_to] || request.referer
-    self.resource = resource_class.new(sign_in_params)
-    clean_up_passwords(resource)
-    respond_with(resource, serialize_options(resource))
-  end
-end
