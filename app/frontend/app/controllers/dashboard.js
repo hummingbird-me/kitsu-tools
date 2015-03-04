0aa6daf30639f023ad19d15015a3202ea47fe871
Thomas McNiven <hello@vevix.net>
2015-03-05 09:25:54 +1000

    add a groups filter to dashboard timeline

--- app/frontend/app/controllers/dashboard.js
+++ app/frontend/app/controllers/dashboard.js
@@ -6,18 +6,21 @@ export default Ember.ArrayController.extend(HasCurrentUser, {
 
   highlightDasboard: function() { return this.get('currentTab') === "dashboard"; }.property('currentTab'),
   highlightPosts: function() { return this.get('currentTab') === "posts"; }.property('currentTab'),
+  highlightGroups: function() { return this.get('currentTab') === "groups"; }.property('currentTab'),
   highlightMedia: function() { return this.get('currentTab') === "media"; }.property('currentTab'),
 
   newStories: [],
 
-  postsOnly: Ember.computed.filter('content', function(item){
-    return item.get('type') === "comment";
+  postsOnly: Ember.computed.filter('content', function(item) {
+    return item.get('type') === "comment" && item.get('group') === null;
   }),
+  groupsOnly: Ember.computed.filterBy('content', 'group'),
   mediaOnly: Ember.computed.filterBy('content', 'type', "media_story"),
 
   arrangedContent: function() {
     var tab = this.get('currentTab');
     if (tab === "posts") { return this.get('postsOnly'); }
+    if (tab === "groups") { return this.get('groupsOnly'); }
     if (tab === "media") { return this.get('mediaOnly'); }
     return this.get('content');
   }.property('currentTab', 'content'),
