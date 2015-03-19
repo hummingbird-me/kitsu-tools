e2230426d04e9cd9f07ebdfde8a28f2547324817
Peter Lejeck <peter.lejeck@gmail.com>
2015-03-18 18:07:53 -0700

    Switch from minutely(5) to hourly.minute_of_hour
    Workaround for tobiassvn/sidetiq#31

--- app/workers/trending_groups_refresh_worker.rb
+++ app/workers/trending_groups_refresh_worker.rb
@@ -2,7 +2,10 @@ class TrendingGroupsRefreshWorker
   include Sidekiq::Worker
   include Sidetiq::Schedulable
 
-  recurrence { minutely(5) }  
+  # Because minutely(int) is slow as balls
+  # See tobiassvn/sidetiq#31
+  recurrence { hourly.minute_of_hour(00, 05, 10, 15, 20, 25,
+                                     30, 35, 40, 45, 50, 55) }
 
   def perform
     TrendingGroup.connection.execute('REFRESH MATERIALIZED VIEW trending_groups')
