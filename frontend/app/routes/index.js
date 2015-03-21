ab961bafe287d2689cde46a4f28c468821da28b2
Sven Gehring <svengehring@cybrox.eu>
2015-03-21 23:33:24 +0100

    Final masonry implementation

--- frontend/app/routes/index.js
+++ frontend/app/routes/index.js
@@ -1,5 +1,3 @@
-/* global Masonry */
-
 import Ember from 'ember';
 import jQuery from 'jquery';
 
@@ -10,21 +8,22 @@ export default Ember.Route.extend({
     });
   },
 
-  setupController: function(controller, model) {
+ setupController: function(controller, model) {
     this._super(controller, model);
     Ember.run.scheduleOnce('afterRender', () => {
       jQuery.mark.jump({
         offset: -120
       });
-      var container = document.querySelector('.recent-stories');
-      var msnry = new Masonry( container, {
-        // options
+
+      var container = Ember.$('.recent-stories');
+      container.masonry({
         "itemSelector": '.recent-story',
         "gutter": 10
       });
+
       // layout Masonry again after all images have loaded
       window.imagesLoaded(container, function() {
-        msnry.layout();
+        container.masonry();
       });
     });
   }
