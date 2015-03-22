a830f1a3ba88155d08f988436553554cf64ea478
Sven Gehring <svengehring@cybrox.eu>
2015-03-22 17:48:27 +0100

    Added googleadservice tracking to landing page

--- frontend/app/controllers/index.js
+++ frontend/app/controllers/index.js
@@ -18,5 +18,37 @@ export default Ember.Controller.extend({
 
   hideOnMobile: function() {
     return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-  }.property()
+  }.property(),
+
+  googReportConversion: function(url) {
+    window.google_conversion_format = "3";
+    window.google_is_call = true;
+    var opt = {
+      onload_callback: function() {
+        if (typeof(url) !== 'undefined') {
+          window.location = url;
+        }
+      }
+    };
+    var conv_handler = window['google_trackConversion'];
+    if (typeof(conv_handler) === 'function') {
+      conv_handler(opt);
+    }
+  },
+
+  actions: {
+    emailSignupClick: function() {
+      window.google_conversion_id = 992672164;
+      window.google_conversion_label = "FrnFCPSZy1oQpPOr2QM";
+      window.google_remarketing_only = false;
+      this.googReportConversion("/sign-up");
+    },
+
+    facebookSignupClick: function() {
+      window.google_conversion_id = 992672164;
+      window.google_conversion_label = "s9hgCKiYy1oQpPOr2QM";
+      window.google_remarketing_only = false;
+      this.googReportConversion("/users/auth/facebook");
+    }
+  }
 });
