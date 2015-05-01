d5147fef5d100c525e5b4fa50efbf665f9b32e86
Thomas McNiven <hello@vevix.net>
2015-05-01 16:42:28 +1000

    :arrow_up: ember 1.11.3

--- frontend/config/environment.js
+++ frontend/config/environment.js
@@ -25,6 +25,7 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     ENV.APP.LOG_VIEW_LOOKUPS = true;
+    ENV.exportApplicationGlobal = true;
   }
 
   if (environment === 'test') {
