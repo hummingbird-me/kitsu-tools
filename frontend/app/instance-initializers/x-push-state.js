848a68fb2f33ac2da76a7dce8afdc0d42ad5d5b2
Thomas McNiven <hello@vevix.net>
2015-05-16 11:11:39 +1000

    :memo: instance initializers

--- frontend/app/instance-initializers/x-push-state.js
+++ frontend/app/instance-initializers/x-push-state.js
@@ -1,9 +1,9 @@
 import Ember from 'ember';
 
-var XContentReady = new CustomEvent('XContentReady');
+let XContentReady = new CustomEvent('XContentReady');
 
-export function initialize(container) {
-  let router = container.lookup('router:main');
+export function initialize(instance) {
+  let router = instance.container.lookup('router:main');
   document.addEventListener('XPushState', (e) => {
     Ember.run(() => {
       router.replaceWith(e.url).then((route) => {
