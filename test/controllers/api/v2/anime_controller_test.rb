739960b59eb18b0246dfc31f948ddbdc95ffd488
Leonardo Prado <leonardoprado@gmail.com>
2015-05-27 03:17:34 -0300

    Add tests for Anime search endpoint on API V2

--- test/controllers/api/v2/anime_controller_test.rb
+++ test/controllers/api/v2/anime_controller_test.rb
@@ -3,6 +3,96 @@ require 'test_helper'
 class Api::V2::AnimeControllerTest < ActionController::TestCase
   setup do
     @controller.class.skip_before_filter :require_client_id
+    # Mind the fixture data generated on anime.yml until we get rid of it
+    create(:anime, title: 'Anime name1', show_type: 'OVA', age_rating: 'PG',
+                   episode_count: '3', started_airing_date: '2011-06-01',
+                   finished_airing_date: '2011-09-01')
+    create(:anime, title: 'Anime name2', started_airing_date: '2011-05-01')
+  end
+
+  #TODO test json structure
+  test "can list anime" do
+    get :index
+    assert_response 200
+    assert JSON.parse(@response.body)['anime'].is_a? Array
+  end
+
+  test "can search anime by name" do
+    get :index, filter: { name: 'by name' }
+    anime = get_anime_from_body
+    assert_equal 2, anime.length
+    assert_equal 'Anime name1', anime.first['canonical_title']
+    assert_equal 'Anime name2', anime.last['canonical_title']
+  end
+
+  test "can search anime by id" do
+    get :index, filter: { id: 'anime-name1' }
+    anime = get_anime_from_body
+    assert_equal 1, anime.length
+    assert_equal 'anime-name1', anime.first['id']
+  end
+
+  test "can search anime by show_type" do
+    get :index, filter: { show_type: 'OVA' }
+    anime = get_anime_from_body
+    assert_equal 1, anime.length
+    assert_equal 'OVA', anime.first['show_type']
+  end
+
+  test "can search anime by age rating" do
+    get :index, filter: { age_rating: 'PG' }
+    anime = get_anime_from_body
+    assert_equal 1, anime.length
+    assert_equal 'PG', anime.first['age_rating']
+  end
+
+  test "can search anime by episode count" do
+    get :index, filter: { episode_count: '3' }
+    anime = get_anime_from_body
+    assert_equal 1, anime.length
+    assert_equal 3, anime.first['episode_count']
+  end
+
+  test "can search anime by started airing" do
+    get :index, filter: { started_airing: '2012-05-01' }
+    anime = get_anime_from_body
+    assert_equal 1, anime.length
+    assert_equal '2012-07-08', anime.first['started_airing']
+  end
+
+  test "can search anime by finished airing" do
+    get :index, filter: { finished_airing: '2011-10-01' }
+    anime = get_anime_from_body
+    assert_equal 3, anime.length
+    assert_equal '2011-09-01', anime.first['finished_airing']
+  end
+
+  # test "can search anime by genre" do
+  #   get :index, filter: { genres: 'Test' }
+  #   anime = get_anime_from_body
+  #   assert_equal 1, anime.length
+  #   assert_equal 'Test', anime.genres.first['name']
+  # end
+
+  test "can search anime by season" do
+    get :index, filter: { season: 'summer' }
+    anime = get_anime_from_body
+    assert_equal 2, anime.length
+    assert_equal '2011-06-01', anime.first['started_airing']
+  end
+
+  test "can search anime by year" do
+    get :index, filter: { year: '2011' }
+    anime = get_anime_from_body
+    assert_equal 3, anime.length
+    assert_equal '2011-06-01', anime.first['started_airing']
+  end
+
+  test "can search anime by multiple filters" do
+    get :index, filter: { year: '2011', season: 'spring' }
+    anime = get_anime_from_body
+    assert_equal 2, anime.length
+    assert_equal '2011-05-01', anime.first['started_airing']
   end
 
   test "can get anime" do
@@ -22,5 +112,11 @@ class Api::V2::AnimeControllerTest < ActionController::TestCase
     assert_response 200
     assert JSON.parse(@response.body)["anime"].is_a? Array
     assert_equal 1, JSON.parse(@response.body)["anime"].length
+  end
+
+  def get_anime_from_body
+    anime = JSON.parse(@response.body)['anime']
+    assert anime.is_a? Array
+    anime
   end
 end
