800a38d24626c764826b23e4ca7cd79d6122f98e
Peter Lejeck <peter.lejeck@gmail.com>
2015-06-12 22:35:47 -0700

    Change conversion tracking to use segment

--- frontend/app/controllers/index.js
+++ frontend/app/controllers/index.js
@@ -20,35 +20,26 @@ export default Ember.Controller.extend({
     return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
   }.property(),
 
-  googReportConversion: function(url) {
-    window.google_conversion_format = "3";
-    window.google_is_call = true;
-    var opt = {
-      onload_callback: function() {
-        if (typeof(url) !== 'undefined') {
-          window.location = url;
-        }
+  reportConversion: function(via) {
+    try {
+      if (window.analytics) {
+        window.analytics.track('Conversion', { via });
       }
-    };
-    var conv_handler = window['google_trackConversion'];
-    if (typeof(conv_handler) === 'function') {
-      conv_handler(opt);
+    } catch (e) {
+      // prevent issues in case Segment fails
+      console.error('Analytics Error: ' + e.message);
     }
   },
 
   actions: {
     emailSignupClick: function() {
-      window.google_conversion_id = 992672164;
-      window.google_conversion_label = "FrnFCPSZy1oQpPOr2QM";
-      window.google_remarketing_only = false;
-      this.googReportConversion("/sign-up");
+      this.reportConversion('email');
+      this.transitionToRoute('sign-up');
     },
 
     facebookSignupClick: function() {
-      window.google_conversion_id = 992672164;
-      window.google_conversion_label = "s9hgCKiYy1oQpPOr2QM";
-      window.google_remarketing_only = false;
-      this.googReportConversion("/users/auth/facebook");
+      this.reportConversion('facebook');
+      window.location = '/users/auth/facebook';
     }
   }
 });
