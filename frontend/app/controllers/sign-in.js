d9b83a435d835daa6566b5534b1eded73f5143d0
Peter Lejeck <peter.lejeck@gmail.com>
2015-06-14 13:19:11 -0700

    Add redirectTo parameter to sign in and sign up pages

--- frontend/app/controllers/sign-in.js
+++ frontend/app/controllers/sign-in.js
@@ -2,21 +2,25 @@ import Ember from 'ember';
 import Session from '../utils/session';
 
 export default Ember.Controller.extend({
+  queryParams: [{ redirectTo: 'redirect_to'}],
   email: '',
   password: '',
-
+  redirectTo: '',
   errorMessage: '',
 
   actions: {
     signIn: function() {
-      var self = this;
-      Session.signIn(this.get('email'), this.get('password')).then(function() {
-        window.location.href = window.lastVisitedURL;
-      }, function(err) {
+      Session.signIn(this.get('email'), this.get('password')).then(() => {
+        if (this.get('redirectTo')) {
+          window.location.href = this.get('redirectTo');
+        } else {
+          window.location.href = window.lastVisitedURL;
+        }
+      }, (err) => {
         try {
-          self.set('errorMessage', err.jqXHR.responseJSON.error);
+          this.set('errorMessage', err.jqXHR.responseJSON.error);
         } catch (e) {
-          self.set('errorMessage', 'An unknown error occurred');
+          this.set('errorMessage', 'An unknown error occurred');
         }
       });
     }
