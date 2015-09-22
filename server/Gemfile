2bf51c8b930ee672a8adb734f6ffc5a9d1f26e7b
Peter Lejeck <peter.lejeck@gmail.com>
2015-09-21 22:19:51 -0700

    Initialize Rails app

---
+++ server/Gemfile
@@ -0,0 +1,28 @@
+source 'https://rubygems.org'
+
+
+gem 'rails', '4.2.1'
+
+gem 'rails-api'
+
+gem 'spring', :group => :development
+
+
+gem 'pg'
+
+
+
+# To use ActiveModel has_secure_password
+# gem 'bcrypt', '~> 3.1.7'
+
+# To use Jbuilder templates for JSON
+# gem 'jbuilder'
+
+# Use unicorn as the app server
+# gem 'unicorn'
+
+# Deploy with Capistrano
+# gem 'capistrano', :group => :development
+
+# To use debugger
+# gem 'ruby-debug19', :require => 'ruby-debug'
---
+++ server/config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+Hummingbird::Application.config.secret_token = 'e314ec5d45ac0f0268b3d156afd6ebfa894b94816e54308fe37a5f083207d14d244897cdd124652a7e19113a953833d678f95c7b924bc33ed5a2cfc20f566d75'
