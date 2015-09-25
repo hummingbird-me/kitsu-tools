d8c1b1fd2bdcd89308d50bb52959105994dd8687
Thomas McNiven <hello@vevix.net>
2015-09-26 01:53:50 +1000

    client: switch to pods

--- client/app/pods/application/route.js
+++ client/app/pods/application/route.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
-import config from '../config/environment';
+import config from 'client/config/environment';
 
 export default Ember.Route.extend({
   title(tokens) {
--- client/config/environment.js
+++ client/config/environment.js
@@ -3,6 +3,7 @@
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'client',
+    podModulePrefix: 'client/pods',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
