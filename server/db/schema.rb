f62f4042ac3300f876ea0d3fdf90638c34dfe820
Peter Lejeck <peter.lejeck@gmail.com>
2015-09-26 17:23:02 -0700

    Add owner to applications

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150926085842) do
+ActiveRecord::Schema.define(version: 20150927002118) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -372,8 +372,11 @@ ActiveRecord::Schema.define(version: 20150926085842) do
     t.string   "scopes",       default: "", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "owner_id"
+    t.string   "owner_type"
   end
 
+  add_index "oauth_applications", ["owner_id", "owner_type"], name: "index_oauth_applications_on_owner_id_and_owner_type", using: :btree
   add_index "oauth_applications", ["uid"], name: "index_oauth_applications_on_uid", unique: true, using: :btree
 
   create_table "partner_codes", force: :cascade do |t|
