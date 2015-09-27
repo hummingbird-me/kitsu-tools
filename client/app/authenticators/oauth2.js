3470c2734f2cb380af788f406a8981a0d093b824
Thomas McNiven <hello@vevix.net>
2015-09-28 05:38:32 +1000

    client: don't need computed for hosts

--- client/app/authenticators/oauth2.js
+++ client/app/authenticators/oauth2.js
@@ -1,17 +1,8 @@
-import Ember from 'ember';
 import OAuth2PasswordGrant from 'ember-simple-auth/authenticators/oauth2-password-grant';
 import config from 'client/config/environment';
 
-const { computed } = Ember;
-
 export default OAuth2PasswordGrant.extend({
   refreshAccessTokens: true,
-
-  serverTokenEndpoint: computed(function() {
-    return `${config.host}/oauth/token`;
-  }),
-
-  serverTokenRevocationEndpoint: computed(function() {
-    return `${config.host}/oauth/revoke`;
-  })
+  serverTokenEndpoint: `${config.host}/oauth/token`,
+  serverTokenRevocationEndpoint: `${config.host}/oauth/revoke`
 });
