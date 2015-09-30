d5827764017d0a0563238ef82c2ad02efb020391
Peter Lejeck <peter.lejeck@gmail.com>
2015-09-29 22:13:29 -0700

    Enable force_ssl in production and create error! method

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::API
+  force_ssl if Rails.env.production?
+
+  def error!(status, message = 'Something went wrong')
+    if status.respond_to?(:errors) # error!(model)
+      # TODO: include 'source' field for JSON-API
+      render json: {
+        errors: status.errors.map { |e| {title: e} }
+      }, status: 400
+    else # error!(status, message)
+      render json: {
+        errors: [{ title: title }],
+      }, status: errors
+    end
+  end
 end
