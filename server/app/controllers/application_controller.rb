3bda83139187007b8f7fd192fac6ecca3085ca28
Peter Lejeck <peter.lejeck@gmail.com>
2015-09-29 22:31:34 -0700

    Add save_or_error!, validate_id methods and document ApplicationController helpers

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -6,6 +6,13 @@ class ApplicationController < ActionController::API
 
   force_ssl if Rails.env.production?
 
+  ##
+  # Renders an error message or a model's error messages
+  #
+  # = Examples
+  #
+  # `error! 500, 'Server is a dog'`
+  # `error! user`
   def error!(status, message = 'Something went wrong')
     if status.respond_to?(:errors) # error!(model)
       # TODO: include 'source' field for JSON-API
@@ -16,6 +23,32 @@ class ApplicationController < ActionController::API
       render json: {
         errors: [{ title: title }],
       }, status: errors
+    end
+  end
+
+  ##
+  # Attempts to save a model instance.  If it fails, renders the error info. If
+  # it succeeds, renders the JSON representation of it.
+  def save_or_error!(model)
+    if model.save
+      render json: model
+    else
+      error! model
+    end
+  end
+
+  ##
+  # Check that params[:id] matches a given instance's id field
+  #
+  # = Examples
+  #
+  # `check_id! user`
+  # `check_id! comment, :comment_id`
+  def validate_id(model, param = :id)
+    if model.id == params[param]
+      true
+    else
+      error! 400, 'ID mismatch'
     end
   end
 end
