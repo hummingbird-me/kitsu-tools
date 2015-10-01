9b2211e5ff23be3b81ac6dd4c10d011e86ace069
Peter Lejeck <peter.lejeck@gmail.com>
2015-09-30 22:36:34 -0700

    Require deserializer automatically for threadsafe goodness

--- server/config/initializers/deserializer.rb
+++ server/config/initializers/deserializer.rb
@@ -1,3 +1,5 @@
+require 'deserializer'
+
 deserializers_path = "#{Rails.root}/app/deserializers"
 unless ActiveSupport::Dependencies.autoload_paths.include?(deserializers_path)
   ActiveSupport::Dependencies.autoload_paths << deserializers_path
