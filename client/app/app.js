f718ca297ea0ffe66e48225632249f86c9c658a1
Thomas McNiven <hello@vevix.net>
2015-10-02 18:13:56 +1000

    client: extract resolver and use it for tests as well

--- client/app/app.js
+++ client/app/app.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
-import Resolver from 'ember-resolver';
+import Resolver from './resolver';
 import loadInitializers from 'ember/load-initializers';
 import config from './config/environment';
 
@@ -8,38 +8,7 @@ Ember.MODEL_FACTORY_INJECTIONS = true;
 const App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
-  Resolver: Resolver.extend({
-    moduleNameLookupPatterns: Ember.computed(function() {
-      const defaultLookup = this._super();
-      return [
-        this.modelResolver,
-        this.componentResolver
-      ].concat(defaultLookup);
-    }),
-
-    /*
-      app/models/<name>/model
-      app/models/<name>/serializer
-      app/models/<name>/adapter
-    */
-    modelResolver(parsedName) {
-      const { fullNameWithoutType, type } = parsedName;
-      if (type === 'model' || type === 'serializer' || type === 'adapter') {
-        return `${this.prefix(parsedName)}/models/${fullNameWithoutType}/${type}`;
-      }
-    },
-
-    // app/components/<name>/component
-    // app/components/<name>/template
-    componentResolver(parsedName) {
-      const { fullNameWithoutType, type } = parsedName;
-      if (type === 'component') {
-        return `${this.prefix(parsedName)}/components/${fullNameWithoutType}/${type}`;
-      } else if (type === 'template') {
-        return `${this.prefix(parsedName)}/${fullNameWithoutType}/${type}`;
-      }
-    }
-  })
+  Resolver
 });
 
 loadInitializers(App, config.modulePrefix);
---
+++ client/app/resolver.js
@@ -0,0 +1,37 @@
+import Ember from 'ember';
+import Resolver from 'ember-resolver';
+
+const { computed } = Ember;
+
+export default Resolver.extend({
+  moduleNameLookupPatterns: computed(function() {
+    const defaultLookup = this._super();
+    return [
+      this.modelResolver,
+      this.componentResolver
+    ].concat(defaultLookup);
+  }),
+
+  /*
+    app/models/<name>/model
+    app/models/<name>/serializer
+    app/models/<name>/adapter
+  */
+  modelResolver(parsedName) {
+    const { fullNameWithoutType, type } = parsedName;
+    if (type === 'model' || type === 'serializer' || type === 'adapter') {
+      return `${this.prefix(parsedName)}/models/${fullNameWithoutType}/${type}`;
+    }
+  },
+
+  // app/components/<name>/component
+  // app/components/<name>/template
+  componentResolver(parsedName) {
+    const { fullNameWithoutType, type } = parsedName;
+    if (type === 'component') {
+      return `${this.prefix(parsedName)}/components/${fullNameWithoutType}/${type}`;
+    } else if (type === 'template') {
+      return `${this.prefix(parsedName)}/${fullNameWithoutType}/${type}`;
+    }
+  }
+});
