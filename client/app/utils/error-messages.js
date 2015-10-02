85642a847f2338bd4af5b31d6bcd616310ca3c27
Thomas McNiven <hello@vevix.net>
2015-10-02 11:01:36 +1000

    client: update error-messages util to show the property name as well

--- client/app/utils/error-messages.js
+++ client/app/utils/error-messages.js
@@ -6,13 +6,22 @@ const lookupTable = {
   'invalid_grant': 'The provided credentials are not valid.'
 };
 
+function formatErrors(errors) {
+  errors = errors.map((error) => {
+    const param = get(error, 'source.parameter');
+    const message = param === undefined ? get(error, 'title') : `${param} ${get(error, 'title')}`;
+    return message;
+  });
+  return errors.compact().join('\n');
+}
+
 // @Cleanup: Might be worth only showing a single error, rather than all. -vevix
 export default function errorMessages(obj) {
   let reason = 'An unknown error occurred';
   let errors = get(obj, 'errors') || get(obj, 'error');
   errors = errors === undefined ? get(obj, 'jqXHR.responseJSON.errors') : errors;
   if (isPresent(errors)) {
-    reason = isArray(errors) ? errors.mapBy('title').compact().join('\n') : get(lookupTable, errors);
+    reason = isArray(errors) ? formatErrors(errors) : get(lookupTable, errors);
   }
   return reason;
 }
