6fb39c861decbe8fe522d883d60b9f5a6f142a2d
Peter Lejeck <peter.lejeck@gmail.com>
2015-10-06 23:25:38 -0700

    server: allow nil average_rating

--- server/app/models/concerns/media.rb
+++ server/app/models/concerns/media.rb
@@ -13,7 +13,7 @@ module Media
     validates :average_rating, numericality: {
       less_than_or_equal_to: 5,
       greater_than: 0
-    }
+    }, allow_nil: true
     validates_attachment :cover_image, content_type: {
       content_type: %w[image/jpg image/jpeg image/png]
     }
