9e29e283f8089af6abf75b699e4e8702510c09eb
Peter Lejeck <peter.lejeck@gmail.com>
2015-10-06 23:25:18 -0700

    server: annotate role model

--- server/app/models/role.rb
+++ server/app/models/role.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: roles
+#
+#  id            :integer          not null, primary key
+#  name          :string
+#  resource_id   :integer
+#  resource_type :string
+#  created_at    :datetime
+#  updated_at    :datetime
+#
+
 class Role < ActiveRecord::Base
   has_and_belongs_to_many :users, join_table: :users_roles
   belongs_to :resource, polymorphic: true
