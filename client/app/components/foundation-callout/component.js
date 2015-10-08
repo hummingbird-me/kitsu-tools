88094c439e17edb1e25638eb16b85301a1467f7c
Thomas McNiven <hello@vevix.net>
2015-10-08 11:36:49 +1000

    client: add foundation meta-tags to html. alert is now callout

--- client/app/components/foundation-callout/component.js
+++ client/app/components/foundation-callout/component.js
@@ -7,7 +7,7 @@ const {
   get
 } = Ember;
 
-// Let Foundation handle closing the alert box due to additonal animations.
+// Let Foundation handle closing the callout due to additonal animations.
 // Cleanup the component after the fact.
 export default Component.extend({
   type: null,
@@ -16,15 +16,14 @@ export default Component.extend({
   _initializeFoundation: on('didInsertElement', function() {
     if (get(this, 'closable') === true) {
       run.scheduleOnce('afterRender', this, () => {
-        this.$().foundation('alert', 'reflow');
-        this.$().on('close.fndtn.alert', () => this.destroy());
+        this.$().on('closed.zf', () => this.destroy());
       });
     }
   }),
 
   _cleanupFoundation: on('willDestroyElement', function() {
     if (get(this, 'closable') === true) {
-      this.$().off('close.fndtn.alert');
+      this.$().off('closed.zf');
     }
   })
 });
--- client/tests/integration/components/foundation-callout/component-test.js
+++ client/tests/integration/components/foundation-callout/component-test.js
@@ -1,20 +1,20 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('foundation-alert-box', 'Integration | Component | foundation alert box', {
+moduleForComponent('foundation-callout', 'Integration | Component | foundation callout', {
   integration: true
 });
 
 test('it renders', function(assert) {
   assert.expect(3);
-  this.render(hbs`{{foundation-alert-box type="success" closable=true}}`);
+  this.render(hbs`{{foundation-callout type="success" closable=true}}`);
 
   // type is set
-  const $alert = this.$('.alert-box.success');
+  const $alert = this.$('.callout.success');
   assert.equal($alert.length, 1);
 
   // button is created
-  const $button = this.$('.alert-box button');
+  const $button = this.$('.callout button');
   assert.equal($button.length, 1);
   assert.equal($button.text().trim(), '×');
 });
@@ -22,12 +22,12 @@ test('it renders', function(assert) {
 test('block renders', function(assert) {
   assert.expect(1);
   this.render(hbs`
-    {{#foundation-alert-box type="success" closable=false}}
+    {{#foundation-callout type="success" closable=false}}
       Hello, World!
-    {{/foundation-alert-box}}
+    {{/foundation-callout}}
   `);
 
   // text is displayed
-  const $alert = this.$('.alert-box.success');
+  const $alert = this.$('.callout.success');
   assert.equal($alert.text().trim(), 'Hello, World!');
 });
