cf7e3c7aa9e82e7358c6497eff75121e390ed914
Thomas McNiven <hello@vevix.net>
2015-10-08 10:04:17 +1000

    client: remove ember-cli-foundation-sass & move to fs6

--- client/ember-cli-build.js
+++ client/ember-cli-build.js
@@ -4,10 +4,8 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     storeConfigInMeta: false,
-    'ember-cli-foundation-sass': {
-      'modernizr': false,
-      'fastclick': true,
-      'foundationJs': 'all'
+    sassOptions: {
+      includePaths: ['bower_components/foundation-sites/scss']
     }
   });
 
@@ -23,7 +21,7 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
-  app.import('bower_components/foundation/js/vendor/modernizr.js');
+  app.import('bower_components/foundation-sites/dist/foundation.js');
   app.import('bower_components/PACE/pace.js');
   app.import('bower_components/PACE/themes/orange/pace-theme-minimal.css');
 
