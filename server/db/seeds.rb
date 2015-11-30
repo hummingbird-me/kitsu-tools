ab0b4e4b6e9f33427854a89ab3c89f14ad7136e6
Peter Lejeck <peter.lejeck@gmail.com>
2015-11-29 20:20:49 -0800

    server: load seed data from YAML files

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Load seeds from YAML files in db/seeds/*
+
+MAX_ID = 2 ** 30 - 1
+SEED_DIR = 'db/seeds'
+
+Dir[Rails.root.join("#{SEED_DIR}/*.yml")].each do |f|
+  basename = File.basename(f, File.extname(f))
+  yaml = YAML::load_file(f)
+  model = basename.classify.constantize
+  puts "==> Seeding: #{model.name}"
+
+  yaml.each do |key, values|
+    puts key
+    model.where(id: values['id']).first_or_create do |record|
+      record.assign_attributes(values)
+    end
+  end
+end
