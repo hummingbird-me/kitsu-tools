cb8d3f911678bc2e20eff56240f7403996ebeb79
Thomas McNiven <hello@vevix.net>
2016-05-18 06:48:50 +1000

    client: Setup ember-i18n addon
    * Dependency updates as well

---
+++ client/app/locales/en/translations.js
@@ -0,0 +1,25 @@
+/**
+ * Translation file for English.
+ */
+export default {
+  'auth': {
+    'register': 'Sign Up',
+    'login': 'Sign In',
+    'logout': 'Logout'
+  },
+  'header': {
+    'shows': 'Shows',
+    'anime': 'Anime',
+    'manga': 'Manga',
+    'drama': 'Drama',
+    'forums': 'Forums',
+    'downloads': 'Downloads',
+    'user': {
+      'profile': 'View Profile'
+    }
+  },
+  'footer': {
+    'header': 'Find an anime, manga, or drama',
+    'search-placeholder': 'Search by title...'
+  }
+};
