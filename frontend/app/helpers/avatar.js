a8d2df3e172bb903f6c903b21c3de95b3419ec27
Thomas McNiven <hello@vevix.net>
2016-06-11 22:44:33 +1000

    Fix Facebook avatar links on frontend

--- frontend/app/helpers/avatar.js
+++ frontend/app/helpers/avatar.js
@@ -3,9 +3,15 @@ import Ember from 'ember';
 export function avatar(user, size) {
   var avatarUrl = user.get("avatarTemplate").replace('{size}', size);
   if (size !== "thumb") {
-    avatarUrl = avatarUrl.replace(/\.[a-zA-Z]+\?/, '.jpg?');
+    const pattern = /\.[a-zA-Z]+\?/;
+    // handle Facebook/weird case
+    if (avatarUrl.match(pattern) !== null) {
+      avatarUrl = avatarUrl.replace(pattern, '.jpg?');
+    } else {
+      avatarUrl = avatarUrl.split('?');
+      avatarUrl = `${avatarUrl[0]}.jpg?${avatarUrl[1]}`;
+    }
   }
-
   return new Ember.Handlebars.SafeString('<img class="responsive-image" src="' + avatarUrl + '" alt="' + user.get("username") + '">');
 }
 
