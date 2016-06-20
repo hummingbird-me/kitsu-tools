923f3d772084d0d38527e23350b68c4bf9373294
Peter Lejeck <peter.lejeck@gmail.com>
2016-06-19 23:46:22 -0700

    server: add fixture helper

---
+++ server/spec/support/fixture_helper.rb
@@ -0,0 +1,36 @@
+class Fixture
+  attr_accessor :name, :filename, :content
+  @@cache = Hash.new { |h,k| h[k] = {} }
+
+  def initialize(name, opts = {})
+    @name = name
+    @opts = opts
+  end
+
+  def to_s
+    @opts[:erb] ? compiled : content
+  end
+
+  private
+
+  def compiled
+    @@cache[:compiled][name] ||= ERB.new(content).tap do |erb|
+      erb.filename = filename
+    end
+    @@cache[:compiled][name]
+  end
+
+  def content
+    @@cache[:content][name] ||= open(filename).read
+    @@cache[:content][name]
+  end
+
+  def filename
+    File.realpath(File.join('spec/fixtures/', name), Rails.root)
+  end
+end
+
+# Global helper method for easy, cached access to fixtures
+def fixture(name, opts = {})
+  Fixture.new(name, opts).to_s
+end
