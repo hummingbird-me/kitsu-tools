41495bb46d72db8c4b04691a7823fee772715eef
Peter Lejeck <peter.lejeck@gmail.com>
2016-06-25 14:20:57 -0700

    server: add Anime#season spec

--- server/spec/models/anime_spec.rb
+++ server/spec/models/anime_spec.rb
@@ -39,4 +39,42 @@ RSpec.describe Anime, type: :model do
   include_examples 'media'
   include_examples 'episodic'
   include_examples 'age_ratings'
+
+  describe '#season' do
+    it 'should return winter for shows starting in December through February' do
+      dec_anime = build(:anime, start_date: Date.new(2015, 12))
+      jan_anime = build(:anime, start_date: Date.new(2016, 1))
+      feb_anime = build(:anime, start_date: Date.new(2016, 2))
+      expect(dec_anime.season).to eq(:winter)
+      expect(jan_anime.season).to eq(:winter)
+      expect(feb_anime.season).to eq(:winter)
+    end
+
+    it 'should return spring for shows starting in March through May' do
+      mar_anime = build(:anime, start_date: Date.new(2016, 3))
+      apr_anime = build(:anime, start_date: Date.new(2016, 4))
+      may_anime = build(:anime, start_date: Date.new(2016, 5))
+      expect(mar_anime.season).to eq(:spring)
+      expect(apr_anime.season).to eq(:spring)
+      expect(may_anime.season).to eq(:spring)
+    end
+
+    it 'should return summer for shows starting in June through August' do
+      jun_anime = build(:anime, start_date: Date.new(2016, 6))
+      jul_anime = build(:anime, start_date: Date.new(2016, 7))
+      aug_anime = build(:anime, start_date: Date.new(2016, 8))
+      expect(jun_anime.season).to eq(:summer)
+      expect(jul_anime.season).to eq(:summer)
+      expect(aug_anime.season).to eq(:summer)
+    end
+
+    it 'should return fall for shows starting in September through November' do
+      sep_anime = build(:anime, start_date: Date.new(2016, 9))
+      oct_anime = build(:anime, start_date: Date.new(2016, 10))
+      nov_anime = build(:anime, start_date: Date.new(2016, 11))
+      expect(sep_anime.season).to eq(:fall)
+      expect(oct_anime.season).to eq(:fall)
+      expect(nov_anime.season).to eq(:fall)
+    end
+  end
 end
