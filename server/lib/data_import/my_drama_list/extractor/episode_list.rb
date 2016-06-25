baa3e12d3140308b5151822b38817b2335654a13
Peter Lejeck <peter.lejeck@gmail.com>
2016-06-25 02:04:24 -0700

    server: extract data from MDL episode list

---
+++ server/lib/data_import/my_drama_list/extractor/episode_list.rb
@@ -0,0 +1,27 @@
+class DataImport::MyDramaList
+  module Extractor
+    class EpisodeList
+      include Helpers
+      include Enumerable
+
+      attr_reader :dom
+      def initialize(html)
+        @dom = Nokogiri::HTML(html)
+      end
+
+      def episodes
+        dom.css('li.episode').map do |row|
+          thumb = row.at_css('.cover > .mask')['style'].match(/\((.*)\)/)[1]
+          {
+            image: original_for(thumb),
+            title: row.at_css('.title').content,
+            air_date: row.at_css('.air_date').try(:content),
+            summary: row.at_css('.summary').at_xpath('text()').try(:content),
+            number: row.at_css('.title a')['href'].match(/episode\/(\d+)/)[1]
+          }
+        end
+      end
+      delegate :each, to: :episodes
+    end
+  end
+end
---
+++ server/spec/lib/data_import/my_drama_list/extractor/episode_list_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe DataImport::MyDramaList::Extractor::EpisodeList do
+  let(:html) { fixture('my_drama_list/signal-episodes.html') }
+  subject { described_class.new(html) }
+
+  it 'should allow iteration over episodes' do
+    expect(subject.count).to eq(16)
+    expect(subject).to include({
+      image: 'http://i.mdldb.net/cache/mWb/w/z1dVbjn3_f80ad7_f.jpg',
+      title: 'Signal Episode 1',
+      air_date: 'Jan 22, 2016',
+      number: '1',
+      summary: <<-EOF.strip_heredoc.gsub(/\s+/, ' ').strip,
+        By a rainy afternoon, the young Park Hae Yeong see a woman pick up a
+        girl from school. But soon, he finds out that the girl is missing and
+        the suspect is a man! Years later, the case…
+      EOF
+    })
+  end
+end
