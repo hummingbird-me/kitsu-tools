a626c7271ca95e6f217ee66601602b939681c7c5
Peter Lejeck <peter.lejeck@gmail.com>
2016-06-25 21:42:43 -0700

    server: DataImport::MyDramaList::Extractor::Details should return a hash to mass-assign

--- server/lib/data_import/my_drama_list/extractor/details.rb
+++ server/lib/data_import/my_drama_list/extractor/details.rb
@@ -1,12 +1,99 @@
 class DataImport::MyDramaList
   module Extractor
     class Details
+      COUNTRY_CODES = {
+        'South Korea' => 'sk',
+        'Japan' => 'jp',
+        'China' => 'cn',
+        'Taiwan' => 'tw',
+        'Hong Kong' => 'hk',
+        'Thailand' => 'th'
+      }
+      LANGUAGE_CODES = {
+        'South Korea' => 'ko',
+        'Japan' => 'ja',
+        'China' => 'zh',
+        'Taiwan' => 'zh',
+        'Hong Kong' => 'zh',
+        'Thailand' => 'th'
+      }
+      SHOW_TYPES = {
+        'Drama' => :drama,
+        'Movie' => :movie,
+        'Drama Special' => :special
+      }
+
       include Helpers
 
       attr_reader :dom
       def initialize(html)
         @dom = Nokogiri::HTML(html)
       end
+
+      def titles
+        main_title = dom.at_css('h1').content.
+                         gsub("(#{start_date.year})", '').strip
+        titles = {}
+        titles["en_#{country}"] = main_title
+        titles["#{language}_#{country}"] = details['Native title']
+        titles
+      end
+
+      def canonical_title
+        "en_#{country}"
+      end
+
+      def abbreviated_titles
+        (details['Also Known as'] || '').split(';').map(&:strip)
+      end
+
+      def synopsis
+        dom.css('.show-synopsis > p').map(&:content).join('\n\n')
+      end
+
+      def episode_count
+        (details['Episodes'] || 1).to_i
+      end
+
+      def episode_length
+        str = details['Duration']
+        parts = str.match(/(?:(?<hr>\d+) hr.)? (?<min>\d+) min./)
+        (parts['hr'].to_i * 60) + parts['min'].to_i
+      end
+
+      def show_type
+        SHOW_TYPES[details['Type']]
+      end
+
+      def poster_image
+        original_for dom.at_css('.cover img')['src']
+      end
+
+      def start_date
+        dates[0]
+      end
+
+      def end_date
+        dates[1]
+      end
+
+      def country
+        COUNTRY_CODES[details['Country']]
+      end
+
+      def to_h
+        %i[titles canonical_title abbreviated_titles synopsis episode_count
+           episode_length show_type poster_image start_date end_date country
+        ].map do |k|
+          [k, send(k)]
+        end.to_h
+      end
+
+      def genres
+        dom.css('.show-genres > a').map(&:content).map(&:strip)
+      end
+
+      private
 
       def details
         @details ||= dom.css('.show-details .txt-block').map do |row|
@@ -23,25 +110,18 @@ class DataImport::MyDramaList
         end.to_h
       end
 
-      def [](key)
-        details[key]
+      def dates
+        if details.include? 'Release Date'
+          [Date.parse(details['Release Date'])]
+        elsif details.include? 'Aired'
+          details['Aired'].split('to').map { |d| Date.parse(d) }
+        else
+          []
+        end
       end
 
-      def title
-        dom.at_css('h1').content.strip
-      end
-
-      def synopsis
-        dom.css('.show-synopsis > p').map(&:content).join('\n\n')
-      end
-
-      def genres
-        dom.css('.show-genres > a').map(&:content).map(&:strip)
-      end
-
-      def poster_image
-        # Extract the image and go fullsize
-        original_for dom.at_css('.cover img')['src']
+      def language
+        LANGUAGE_CODES[details['Country']]
       end
     end
   end
