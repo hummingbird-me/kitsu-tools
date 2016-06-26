bfd83da8284961ee3bd103fd787f565d081f0bea
Peter Lejeck <peter.lejeck@gmail.com>
2016-06-26 00:38:56 -0700

    server: reorganize Media concern, add index update trigger

--- server/app/models/concerns/media.rb
+++ server/app/models/concerns/media.rb
@@ -6,12 +6,6 @@ module Media
     include Titleable
     include Rateable
 
-    has_many :library_entries, as: 'media', dependent: :destroy
-    has_many :mappings, as: 'media', dependent: :destroy
-
-    friendly_id :slug_candidates, use: %i[slugged finders history]
-    resourcify
-
     # HACK: we need to return a relation but want to handle historical slugs
     scope :by_slug, -> (slug) {
       record = where(slug: slug)
@@ -19,13 +13,19 @@ module Media
       record
     }
 
+    friendly_id :slug_candidates, use: %i[slugged finders history]
+    resourcify
     has_attached_file :cover_image
     has_attached_file :poster_image
+    update_index("media##{name.tableize}") { self }
 
     has_and_belongs_to_many :genres
     has_many :castings, as: 'media'
     has_many :installments, as: 'media'
     has_many :franchises, through: :installments
+    has_many :library_entries, as: 'media', dependent: :destroy
+    has_many :mappings, as: 'media', dependent: :destroy
+    delegate :year, to: :start_date, allow_nil: true
 
     validates_attachment :cover_image, content_type: {
       content_type: %w[image/jpg image/jpeg image/png]
@@ -41,6 +41,4 @@ module Media
       -> { titles[:ja_en] }
     ]
   end
-
-  delegate :year, to: :start_date, allow_nil: true
 end
