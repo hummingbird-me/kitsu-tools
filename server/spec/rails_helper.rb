fc6fa641dc0afe64af7f5f60443de7dbfba6b651
Peter Lejeck <peter.lejeck@gmail.com>
2016-07-07 19:25:50 -0700

    server: move Chewy stuff to environments/test instead of spec/support

--- server/spec/rails_helper.rb
+++ server/spec/rails_helper.rb
@@ -4,8 +4,6 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort('The Rails environment is in production mode!') if Rails.env.production?
 
-require_relative './support/chewy' # Load chewy stuff early
-
 require 'spec_helper'
 require 'rspec/rails'
 # Rails is now loaded
