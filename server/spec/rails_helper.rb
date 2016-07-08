0e62303821deebcf5db2fc6af014376611488a89
Peter Lejeck <peter.lejeck@gmail.com>
2016-07-07 17:14:59 -0700

    server: move spec stuff into support/chewy

--- server/spec/rails_helper.rb
+++ server/spec/rails_helper.rb
@@ -4,8 +4,7 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort('The Rails environment is in production mode!') if Rails.env.production?
 
-Chewy.use_after_commit_callbacks = false
-Chewy.strategy(:urgent)
+require_relative './support/chewy' # Load chewy stuff early
 
 require 'spec_helper'
 require 'rspec/rails'
--- server/spec/support/chewy.rb
+++ server/spec/support/chewy.rb
@@ -1,5 +1,12 @@
 require 'chewy/rspec'
 
+# Index immediately, since commits don't happen in testing
+Chewy.use_after_commit_callbacks = false
+
+# Process indexing requests immediately
+Chewy.strategy(:urgent)
+
+# Reset all the indices before we start
 Chewy::Index.descendants.each do |index|
   puts "Resetting #{index.name}"
   index.reset!
