1879e45b289818e8062ffd2a3800a974874b4b14
Peter Lejeck <peter.lejeck@gmail.com>
2016-07-17 11:36:36 -0700

    server: update IndexTranslatable to comply with BCP47 and allow passing opts into field

--- server/app/chewy/concerns/index_translatable.rb
+++ server/app/chewy/concerns/index_translatable.rb
@@ -2,16 +2,15 @@ module IndexTranslatable
   extend ActiveSupport::Concern
 
   TRANSLATION_MAPPINGS = {
-    '*_jp' => 'cjk',
+    'ja_*' => 'cjk',
     'zh_*' => 'cjk',
     'ko_*' => 'cjk',
     'en_*' => 'english'
   }.freeze
 
   class_methods do
-    def translatable_field(name)
-      # TODO: fix romaji in the db
-      field name, type: 'object'
+    def translatable_field(name, opts = {})
+      field name, { type: 'object' }.merge(opts)
       TRANSLATION_MAPPINGS.each do |mask, analyzer|
         template "#{name}.#{mask}", type: 'string', analyzer: analyzer
       end
