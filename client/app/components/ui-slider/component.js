30b1bf0e0f5a3ba5559963e431e0b911697f3dd1
Thomas McNiven <hello@vevix.net>
2016-08-01 20:03:09 +1000

    client: rename foundation-slider to ui-slider

--- client/app/components/ui-slider/component.js
+++ client/app/components/ui-slider/component.js
@@ -3,34 +3,17 @@ import get from 'ember-metal/get';
 /* global noUiSlider */
 
 export default Component.extend({
+  classNames: ['slider'],
   start: 0,
   end: 100,
   step: 1,
   initialStart: 0,
   initialEnd: 100,
-  clickSelect: true,
-  vertical: false,
-  draggable: true,
-  disabled: false,
   doubleSided: false,
   decimal: 2,
-  moveTime: 200,
-  disabledClass: 'disabled',
-  classNames: 'slider',
 
   didInsertElement() {
     this._super(...arguments);
-    /* TO BE ENABLED AFTER zurb/foundation-sites#7730 IS FIXED!
-    new Foundation.Slider(this.$(), getProperties(this, [
-      'start', 'end', 'initialStart', 'initialEnd', 'clickSelect', 'vertical',
-      'draggable', 'disabled', 'doubleSided', 'decimal', 'moveTime',
-      'disabledClass'
-    ]));
-    this.$().on('moved.zf.slider', () => {
-      const values = this.$('input').map(function () { return this.value });
-      this.sendAction('update', values.toArray());
-    });
-    */
     const [$el] = this.$();
     const selected = [get(this, 'initialStart')];
     if (get(this, 'doubleSided') === true) {
--- client/tests/integration/components/ui-slider/component-test.js
+++ client/tests/integration/components/ui-slider/component-test.js
@@ -1,30 +1,30 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('foundation-slider', 'Integration | Component | foundation slider', {
+moduleForComponent('ui-slider', 'Integration | Component | ui slider', {
   integration: true
 });
 
 test('it renders a single handled slider', function(assert) {
-  this.render(hbs`{{foundation-slider doubleSided=false}}`);
+  this.render(hbs`{{ui-slider doubleSided=false}}`);
   const slider = this.$('.noUi-handle');
   assert.equal(slider.length, 1);
 });
 
 test('it renders a two handled slider', function(assert) {
-  this.render(hbs`{{foundation-slider doubleSided=true}}`);
+  this.render(hbs`{{ui-slider doubleSided=true}}`);
   const slider = this.$('.noUi-handle');
   assert.equal(slider.length, 2);
 });
 
-test('decimals are respected and actions are called', function(assert) {
+test('decimal setting is respected', function(assert) {
   // update is called 3 times by both sets, and the mouse click
   // release is called once by the simulated mouse click
-  assert.expect(6);
+  assert.expect(4);
   this.set('update', () => assert.ok(true));
   this.set('release', () => assert.ok(true));
   this.render(hbs`
-    {{foundation-slider
+    {{ui-slider
         initialStart=50 initialEnd=90 doubleSided=true
         decimal=2 step=0.01
         onUpdate=(action update)
@@ -36,6 +36,17 @@ test('decimals are respected and actions are called', function(assert) {
   assert.deepEqual($el.noUiSlider.get(), [50, 90]);
   $el.noUiSlider.set([25.125, null]);
   assert.deepEqual($el.noUiSlider.get(), [25.13, 90]);
+});
+
+test('actions are called', function(assert) {
+  assert.expect(2);
+  this.set('update', () => assert.ok(true));
+  this.set('release', () => assert.ok(true));
+  this.render(hbs`{{ui-slider
+    onUpdate=(action update)
+    onRelease=(action release)
+  }}`);
+
 
   // trigger an actual click to proc the 'slide' event
   const offset = (el) => {
@@ -45,6 +56,7 @@ test('decimals are respected and actions are called', function(assert) {
       y: rect.top + this.$()[0].scrollTop
     };
   };
+  const [$el] = this.$('.noUi-target');
   const event = document.createEvent('MouseEvents');
   event.initMouseEvent('mousedown', true, true, window, null, 0, 0,
     offset($el).left + 100, offset($el).top + 8, '',
