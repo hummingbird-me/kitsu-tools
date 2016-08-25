fcb801dd0d9dc42e486aba20548edc942e6cfdd3
Peter Lejeck <peter.lejeck@gmail.com>
2016-08-24 19:17:30 -0700

    Switch to NULLS LAST sorting for manga
    Also lock down sort_by values to prevent injection attacks

--- app/controllers/manga_controller.rb
+++ app/controllers/manga_controller.rb
@@ -21,7 +21,13 @@ class MangaController < ApplicationController
       format.json do
         manga = Manga
         manga = Manga.where(slug: params[:ids]) if params[:ids]
-        manga = Manga.order(params[:sort_by] + (params[:sort_reverse].nil? ? "" : " DESC")) if params[:sort_by]
+        
+        sort_options = %w(bayesian_rating)
+        if params[:sort_by] && params[:sort_by].in?(sort_options)
+          reverse = (params[:sort_reverse].nil?) ? '' : ' DESC NULLS LAST'
+          manga = Manga.order(params[:sort_by] + reverse)
+        end
+        
         render json: manga.includes(:genres).page(params[:page]).per(40)
       end
     end
